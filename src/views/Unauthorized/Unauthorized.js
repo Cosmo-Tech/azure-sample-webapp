ecb7ecb7ad55dfaaf7dabf18e26239d9d81f3858
Tristan Huet <tristan.huet@cosmotech.com>
2020-12-07 13:38:54 +0100

    Change application name on Unauthorized page

--- src/views/Unauthorized/Unauthorized.js
+++ src/views/Unauthorized/Unauthorized.js
@@ -135,7 +135,7 @@ const Unauthorized = props => {
           <div className={classes.quote}>
             <div className={classes.quoteInner}>
               <Typography className={classes.quoteText} variant="h1">
-                Airspace Network Management
+                Azure Sample Web Application
               </Typography>
               <div className={classes.person}>
                 <Typography className={classes.name} variant="body2">
