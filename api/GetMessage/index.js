6f152284931e0d9e608f22bfb43b357a024c623a
Vincent Carluer <vcarluer@gmail.com>
2021-01-15 17:28:30 +0100

    Answer caller in message api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,10 +1,16 @@
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
-
+  var caller = 'No caller'
+  const header = req.headers["x-ms-client-principal"]
+  if (header) {
+    const encoded = Buffer.from(header, "base64")
+    caller  = encoded.toString("ascii")
+  }
   const name = (req.query.name || (req.body && req.body.name))
-  const responseMessage = name
+  var responseMessage = name
     ? 'Hello, ' + name + ' from phoenix. This HTTP triggered function executed successfully.'
     : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+  responseMessage = responseMessage + ' API caller: ' + caller
 
   context.res = {
     // status: 200, /* Defaults to 200 */
