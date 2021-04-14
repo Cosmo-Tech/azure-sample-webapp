3d160e30e981b44ef05947bc96a213acd621d9d0
jreynard <jeremy.reynard@cosmotech.com>
2021-04-14 12:20:07 +0200

    Add GetScenariosTree azure function

---
+++ api/GetScenariosTree/index.js
@@ -0,0 +1,9 @@
+const responseTemplate = require('./GetScenariosTree.json')
+
+// TODO remove the mock response and use the ScenarioAPI when it's ready
+module.exports = async function (context, req) {
+  context.log('GetScenariosTree HTTP trigger function.')
+  context.res = {
+    body: responseTemplate
+  }
+}
