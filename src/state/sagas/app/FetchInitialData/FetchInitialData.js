fcaf854c78980ac2e00b26bee52ce56567bc496e
jreynard <jeremy.reynard@cosmotech.com>
2021-04-23 14:16:58 +0200

    Add FetchInitialData saga
    Define Saga process
    Add test for this saga
    Add this saga into the rootSaga.js

---
+++ src/state/sagas/app/FetchInitialData/FetchInitialData.js
@@ -0,0 +1,27 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { put, takeEvery, call } from 'redux-saga/effects'
+import { APPLICATION_ACTIONS_KEY, APPLICATION_STATUS } from '../../../commons/ApplicationConstants'
+import { getAllScenariosData } from '../../scenario/FindAllScenarios/FindAllScenariosData'
+import { fetchScenarioTreeData } from '../../scenario/GetScenariosTree/GetScenariosTreeData'
+
+// generators function
+export function * fetchAllInitialData () {
+  try {
+    // Fetch all scenarios
+    yield call(getAllScenariosData)
+    yield call(fetchScenarioTreeData)
+    yield put({ type: APPLICATION_ACTIONS_KEY.SET_APPLICATION_STATUS, status: APPLICATION_STATUS.READY })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// generators function
+// Here is a watcher that take EVERY action dispatched named GET_SCENARIO_LIST and bind getAllScenariosData saga to it
+function * getAllInitialData () {
+  yield takeEvery(APPLICATION_ACTIONS_KEY.GET_ALL_INITIAL_DATA, fetchAllInitialData)
+}
+
+export default getAllInitialData
---
+++ src/state/sagas/app/index.js
@@ -0,0 +1,11 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { all, fork } from 'redux-saga/effects'
+import { getAllInitialData } from './FetchInitialData'
+
+export default function * appSaga () {
+  yield all([
+    fork(getAllInitialData)
+  ])
+}
