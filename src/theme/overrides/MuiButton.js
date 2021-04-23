21a859202cfb075866f101b865486c5f60596fb2
COSMO-VMI <valentin.michelet@cosmotech.com>
2021-04-23 10:33:36 +0200

    Remove wrong style override in MuiButton
    Overriding button style prevented from having Contained, Outlined and Text buttons.

--- src/theme/overrides/MuiButton.js
+++ src/theme/overrides/MuiButton.js
@@ -2,12 +2,6 @@
 // Licensed under the MIT license.
 
 export default {
-  root: {
-    background: '#FFAD38'
-  },
-  label: {
-    color: '#000000'
-  },
   contained: {
     boxShadow:
       '0 1px 1px 0 rgba(0,0,0,0.14), 0 2px 1px -1px rgba(0,0,0,0.12), 0 1px 3px 0 rgba(0,0,0,0.20)'
