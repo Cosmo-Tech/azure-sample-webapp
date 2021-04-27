6a1f66feabea4822c961b057db57effe06c7e952
COSMO-VMI <valentin.michelet@cosmotech.com>
2021-04-27 12:00:01 +0200

    Temp fix for "Warning: findDOMNode is deprecated in StrictMode."
    Use unsafe_CreateMuiStrictModeTheme instead of createMuiTheme in order to avoid a warning.
    The warning is triggered from react-transition-group, and has been fixed in v4.4.0 however, MaterialUI v4.11.1 seems not to be using it yet.
    Therefore, there are two ways of getting rid of it: either using an unsafe mui, or Material v5.0 which is not stable yet.
    I opted for unsafe mui fix, and only using it in dev, not in prod, for the StrictMode warning is only visible in dev, not in prod.
    This fix will hopefully be removed when we upgrade to MaterialUI v5.0
    
    See https://github.com/mui-org/material-ui/issues/13394 for more info on
    Warning: findDOMNode is deprecated in StrictMode.

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,13 +1,26 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import { createMuiTheme } from '@material-ui/core'
+import {
+  createMuiTheme,
+  // REMOVE THIS WHEN USING MATERIAL UI v5.0
+  // see https://github.com/mui-org/material-ui/issues/13394 for more info on
+  // Warning: findDOMNode is deprecated in StrictMode.
+  // eslint-disable-next-line camelcase
+  unstable_createMuiStrictModeTheme
+} from '@material-ui/core'
 
 import palette from './palette'
 import typography from './typography'
 import overrides from './overrides'
 
-const theme = createMuiTheme({
+// REMOVE THIS WHEN USING MATERIAL UI v5.0
+// see https://github.com/mui-org/material-ui/issues/13394 for more info on
+// Warning: findDOMNode is deprecated in StrictMode.
+// eslint-disable-next-line camelcase
+const createTheme = process.env.NODE_ENV === 'production' ? createMuiTheme : unstable_createMuiStrictModeTheme
+
+const theme = createTheme({
   palette,
   typography,
   overrides,
