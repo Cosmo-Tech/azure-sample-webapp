fd895cc3366145d750fcf87b338cc69e5e6015fd
jreynard <jeremy.reynard@cosmotech.com>
2021-05-03 14:37:33 +0200

    Fix eslint errors

--- api/AddNewScenario/index.js
+++ api/AddNewScenario/index.js
@@ -2,25 +2,15 @@
 // Licensed under the MIT license.
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-    const scenario = (req.query.scenario || (req.body && req.body.scenario));
-    
-    let body = 'Scenario ' + scenario + ' saved';
-    
-    //////////////////////////////////////////////////////////////////////////
-    // Remove after API implementation
-    const fs = require('fs');
-    fs.writeFile('AddNewScenario/' + scenario + '.json', body, function(err) {
-      if (err) {
-          console.log(err);
-      }
-    });
-    //////////////////////////////////////////////////////////////////////////
+  const scenario = (req.query.scenario || (req.body && req.body.scenario));
 
-    console.log(scenario);
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: body
-    };
-}
+  const body = 'Scenario ' + scenario + ' saved';
+
+  console.log(scenario);
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: body
+  };
+};
--- api/FindScenarioById/index.js
+++ api/FindScenarioById/index.js
@@ -6,24 +6,24 @@ const scenario2 = require('./Scenario2.json');
 const scenario3 = require('./Scenario3.json');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-    const id = (req.query.id || (req.body && req.body.id));
-    
-    let body
-    switch (id) {
-        case '1':
-            body = scenario1;
-            break;
-        case '2':
-            body = scenario2;
-            break;
-        case '3':
-            body = scenario3;
-    }
-    console.log(id);
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: body
-    };
-}
+  const id = (req.query.id || (req.body && req.body.id));
+
+  let body;
+  switch (id) {
+    case '1':
+      body = scenario1;
+      break;
+    case '2':
+      body = scenario2;
+      break;
+    case '3':
+      body = scenario3;
+  }
+  console.log(id);
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: body
+  };
+};
