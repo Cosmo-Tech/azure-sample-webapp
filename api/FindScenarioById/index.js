23b799de67c4c3caff7bba19255bdcd4bcd79053
jreynard <jeremy.reynard@cosmotech.com>
2021-05-06 21:10:36 +0200

    Remove useless console.log

--- api/FindScenarioById/index.js
+++ api/FindScenarioById/index.js
@@ -6,10 +6,9 @@ const scenario2 = require('./Scenario2.json');
 const scenario3 = require('./Scenario3.json');
 
 module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
   const id = (req.query.id || (req.body && req.body.id));
 
+  context.log('FindScenarioById HTTP trigger function processed a request. ' + id);
   let body;
   switch (id) {
     case '1':
@@ -21,7 +20,7 @@ module.exports = async function (context, req) {
     case '3':
       body = scenario3;
   }
-  console.log(id);
+
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: body
