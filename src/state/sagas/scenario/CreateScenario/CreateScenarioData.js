0bc56c46226ffd84745324b71819e4c7dc5605e1
jreynard <jeremy.reynard@cosmotech.com>
2021-05-11 14:39:57 +0200

    Fetch and update scenarioList and scenarioTree when create scenario is successful

--- src/state/sagas/scenario/CreateScenario/CreateScenarioData.js
+++ src/state/sagas/scenario/CreateScenario/CreateScenarioData.js
@@ -6,15 +6,19 @@ import { SCENARIO_ACTIONS_KEY } from '../../../commons/ScenarioConstants';
 import { ORGANISATION_ID } from '../../../../configs/App.config';
 import ScenarioService from '../../../../services/scenario/ScenarioService';
 import { STATUSES } from '../../../commons/Constants';
+import { getAllScenariosData } from '../FindAllScenarios/FindAllScenariosData';
+import { fetchScenarioTreeData } from '../GetScenariosTree/GetScenariosTreeData';
 
 // generators function
 export function * createScenario (action) {
   // yield keyword is here to milestone and save the action
-  const { error, data } = yield call(ScenarioService.createScenario, ORGANISATION_ID, action.workspaceId, action.scenario);
+  const workspaceId = action.workspaceId;
+  const { error, data } = yield call(ScenarioService.createScenario, ORGANISATION_ID, workspaceId, action.scenario);
   if (error) {
     // TODO handle error management
   } else {
-    // TODO Update scenarioList and getScenarioTree
+    yield call(getAllScenariosData, workspaceId);
+    yield call(fetchScenarioTreeData, workspaceId);
     // Here is an effect named put that indicate to the middleware that it can dispatch a SET_CURRENT_SCENARIO action with list as payload
     yield put({ type: SCENARIO_ACTIONS_KEY.SET_CURRENT_SCENARIO, data: { status: STATUSES.SUCCESS, scenario: data } });
   }
