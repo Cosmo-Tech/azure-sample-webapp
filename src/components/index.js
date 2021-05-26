9beab0141918e6560ded77b71a66fc826ea3864c
jreynard <jeremy.reynard@cosmotech.com>
2021-05-26 14:25:41 +0200

    Remove useless component: IframeScenarioResults

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@
 // Licensed under the MIT license.
 
 export { default as ScenarioParameters } from './ScenarioParameters';
-export { default as IframeScenarioResults } from './IframeScenarioResults';
 export { default as PublicRoute } from './PublicRoute';
 export { default as PrivateRoute } from './PrivateRoute';
