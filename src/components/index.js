ca20b82a61124f6d03aea6c09bca0e356ec34ac7
jreynard <jeremy.reynard@cosmotech.com>
2021-05-26 12:16:05 +0200

    Remove useless components: ButtonRunProtocol, ButtonRunSimulation, CardProtocolParameters, CardSimulationParameters

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,6 @@
 // Licensed under the MIT license.
 
 export { default as ScenarioParameters } from './ScenarioParameters';
-export { default as ButtonRunSimulation } from './ButtonRunSimulation';
-export { default as ButtonRunProtocol } from './ButtonRunProtocol';
-export { default as CardSimulationParameters } from './CardSimulationParameters';
-export { default as CardProtocolParameters } from './CardProtocolParameters';
 export { default as IframeScenarioResults } from './IframeScenarioResults';
 export { default as PublicRoute } from './PublicRoute';
 export { default as PrivateRoute } from './PrivateRoute';
