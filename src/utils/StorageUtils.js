d8d77bf244dfc8a706505aa5979979b534683c82
Tristan Huet <tristan.huet@cosmotech.com>
2021-05-31 18:07:16 +0200

    Add local storage utils

---
+++ src/utils/StorageUtils.js
@@ -0,0 +1,18 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+// Functions to read & write from storage.
+// Notes : local storage works on Chromium but not on Firefox if "Delete
+// cookies and site data when Firefox is closed" is selected (for more
+// details, see https://bugzilla.mozilla.org/show_bug.cgi?id=1453699)
+export function writeToStorage (key, value) {
+  localStorage.setItem(key, value);
+}
+
+export function readFromStorage (key) {
+  return localStorage.getItem(key);
+}
+
+export function clearFromStorage (key) {
+  localStorage.removeItem(key);
+}
