9345e9110016336bb6c8f2d6e4c4421344f8ce4a
FMO-Cosmo <81355469+FMO-Cosmo@users.noreply.github.com>
2021-06-07 14:52:43 +0200

    Update DashboardsList.config.js
    Use Brewery-based reports, connected to ADX. Filter is based on SimulationName for testing.

--- src/configs/DashboardsList.config.js
+++ src/configs/DashboardsList.config.js
@@ -5,17 +5,22 @@
 export const DASHBOARDS_LIST_CONFIG = [
   {
     id: 0,
-    title: 'Dashboard 1',
-    url: 'https://app.powerbi.com/reportEmbed?reportId=4f064d6a-2238-4e8f-8fee-8340f5a4800f&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=Demands/Scenario_x0020_Name%20eq%20%27<ScenarioName>%27'
+    title: 'Digital Twin Structure',
+    url: 'https://app.powerbi.com/reportEmbed?reportId=42997cf5-6538-4ca8-bbee-d3194b9976a6&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection6a4194e49b60a76095e8'
   },
   {
     id: 1,
-    title: 'Dashboard 2',
-    url: ''
+    title: 'Stocks Follow-up',
+    url: 'https://app.powerbi.com/reportEmbed?reportId=07d946b2-0125-4e37-9711-9031a6fd615b&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
   },
   {
     id: 2,
-    title: 'Dashboard 3',
-    url: ''
+    title: 'Detailed Costs',
+    url: 'https://app.powerbi.com/reportEmbed?reportId=31fedbca-38b7-4c9c-8a60-064db24a619c&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
+  },
+  {
+    id: 3,
+    title: 'Transport Focus',
+    url: 'https://app.powerbi.com/reportEmbed?reportId=fcfa1797-c6f6-4aca-abdd-0ed2f7f2442a&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
   }
 ];
