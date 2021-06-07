fb7b56a7831edef63f3f0e81afd06a79a3112a4b
FMO-Cosmo <81355469+FMO-Cosmo@users.noreply.github.com>
2021-06-07 14:59:02 +0200

    Use Brewery-based report for scenario view

--- src/configs/ScenarioDashboard.config.js
+++ src/configs/ScenarioDashboard.config.js
@@ -4,5 +4,5 @@
 // Tabs configuration
 export const SCENARIO_DASHBOARD_CONFIG =
 {
-  url: 'https://app.powerbi.com/reportEmbed?reportId=4f064d6a-2238-4e8f-8fee-8340f5a4800f&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=Demands/Scenario_x0020_Name%20eq%20%27<ScenarioName>%27'
+  url: 'https://app.powerbi.com/reportEmbed?reportId=974d4b48-c53c-4bb8-bbd6-ee339f6ae752&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
 };
