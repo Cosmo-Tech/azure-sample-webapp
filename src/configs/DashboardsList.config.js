8e12f5ab9114219f1af4e2ab03a49c59b305ff31
Tristan Huet <tristan.huet@cosmotech.com>
2021-06-09 17:25:29 +0200

    Update dasboard template URL in scenario view

--- src/configs/DashboardsList.config.js
+++ src/configs/DashboardsList.config.js
@@ -11,16 +11,16 @@ export const DASHBOARDS_LIST_CONFIG = [
   {
     id: 1,
     title: 'Stocks Follow-up',
-    url: 'https://app.powerbi.com/reportEmbed?reportId=07d946b2-0125-4e37-9711-9031a6fd615b&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
+    url: 'https://app.powerbi.com/reportEmbed?reportId=07d946b2-0125-4e37-9711-9031a6fd615b&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSection&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27&$filter=StockProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27'
   },
   {
     id: 2,
     title: 'Detailed Costs',
-    url: 'https://app.powerbi.com/reportEmbed?reportId=31fedbca-38b7-4c9c-8a60-064db24a619c&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
+    url: 'https://app.powerbi.com/reportEmbed?reportId=31fedbca-38b7-4c9c-8a60-064db24a619c&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27&$filter=StockProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27'
   },
   {
     id: 3,
     title: 'Transport Focus',
-    url: 'https://app.powerbi.com/reportEmbed?reportId=fcfa1797-c6f6-4aca-abdd-0ed2f7f2442a&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27'
+    url: 'https://app.powerbi.com/reportEmbed?reportId=fcfa1797-c6f6-4aca-abdd-0ed2f7f2442a&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionb0dd90d4b4de14e644a1&$filter=StockProbe/SimulationName%20eq%20%27<ScenarioName>%27&$filter=StockProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27'
   }
 ];
