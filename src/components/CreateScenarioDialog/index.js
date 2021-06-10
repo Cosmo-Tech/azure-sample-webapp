be9bf3ffe9ca08efa2eae9f553766bd33ccddd2a
Tristan Huet <tristan.huet@cosmotech.com>
2021-06-10 11:37:26 +0200

    refactor: remove components that have been moved to @cosmotech/ui

--- src/components/CreateScenarioDialog/index.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Cosmo Tech.
-// Licensed under the MIT license.
-
-export { default as CreateScenarioButton } from './CreateScenarioButton';
-export { default as CreateScenarioDialog } from './CreateScenarioDialog';
