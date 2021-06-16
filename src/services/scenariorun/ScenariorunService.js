8db3030d3d01493625c9a7497e7db013aa616521
Nicolas Borde <nicolas.borde@cosmotech.com>
2021-06-16 20:36:49 +0200

    Add scenariorun logs function in scenariorun service

--- src/services/scenariorun/ScenariorunService.js
+++ src/services/scenariorun/ScenariorunService.js
@@ -2,6 +2,9 @@
 // Licensed under the MIT license.
 
 import { CosmotechApiService } from '../../configs/Api.config';
+import fileDownload from 'js-file-download';
+import { LOG_TYPES } from './ScenariorunConstants.js';
+import { ORGANISATION_ID } from '../../configs/App.config';
 
 const ScenariorunApi = new CosmotechApiService.ScenariorunApi();
 
@@ -13,8 +16,67 @@ function runScenario (organizationId, workspaceId, scenarioId) {
   });
 }
 
+function getScenarioRuns (organizationId, workspaceId, scenarioId) {
+  return new Promise((resolve) => {
+    ScenariorunApi.getScenarioRuns(organizationId, workspaceId, scenarioId, (error, data, response) => {
+      resolve({ error, data, response });
+    });
+  });
+}
+
+function getScenarioRunCumulatedLogs (organizationId, scenarioRunId) {
+  return new Promise((resolve) => {
+    ScenariorunApi.getScenarioRunCumulatedLogs(organizationId, scenarioRunId, (error, data, response) => {
+      resolve({ error, data, response });
+    });
+  });
+}
+
+function getScenarioRunLogs (organizationId, scenarioRunId) {
+  return new Promise((resolve) => {
+    ScenariorunApi.getScenarioRunLogs(organizationId, scenarioRunId, (error, data, response) => {
+      resolve({ error, data, response });
+    });
+  });
+}
+
+async function downloadCumulatedLogsFile (lastRun) {
+  const { error, data } = await getScenarioRunCumulatedLogs(ORGANISATION_ID, lastRun.scenarioRunId);
+  if (error) {
+    console.error(error);
+  } else {
+    const blob = new Blob([data]);
+    fileDownload(blob, lastRun.scenarioRunId + '_cumulated_logs.txt');
+  }
+}
+
+async function downloadLogsSimpleFile (lastRun) {
+  const { error, data } = await getScenarioRunLogs(ORGANISATION_ID, lastRun.scenarioRunId);
+  if (error) {
+    console.error(error);
+  } else {
+    const blob = new Blob([data]);
+    fileDownload(blob, lastRun.scenarioRunId + '_simple_logs.json');
+  }
+}
+
+function downloadLogsFile (lastRun, logType) {
+  switch (logType) {
+    case LOG_TYPES.SIMPLE_LOGS:
+      downloadLogsSimpleFile(lastRun);
+      break;
+    case LOG_TYPES.CUMULATED_LOGS:
+      downloadCumulatedLogsFile(lastRun);
+      break;
+  }
+}
+
 const ScenariorunService = {
-  runScenario
+  runScenario,
+  getScenarioRuns,
+  getScenarioRunCumulatedLogs,
+  getScenarioRunLogs,
+  downloadLogsFile
 };
 
 export default ScenariorunService;
