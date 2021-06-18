0eb47dffcceaa01e9d399e989aa350681fedc80f
jreynard <jeremy.reynard@cosmotech.com>
2021-06-18 15:08:56 +0200

    fix: add json extension to accepted file type for initial stock parameter

--- src/components/ScenarioParameters/UploadFileConfig.js
+++ src/components/ScenarioParameters/UploadFileConfig.js
@@ -9,6 +9,6 @@ export const INITIAL_STOCK_PARAM_ID = 'initial_stock_dataset';
 export const INITIAL_STOCK_PARAM_CONNECTOR_ID = 'C-XPv4LBVGAL';
 
 export const INITIAL_STOCK_PARAM_ACCEPT_FILE_TYPE =
-    '.xlsx,.xls,.csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    '.xlsx,.xls,.csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,.json';
 
 export const MIME_TYPE_FILE_UPLOAD = 'application/json';
