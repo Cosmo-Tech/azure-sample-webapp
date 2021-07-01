c394c114538ec9841fe1d90dcc7664429d9623ba
FMO-Cosmo <81355469+FMO-Cosmo@users.noreply.github.com>
2021-07-01 20:11:34 +0200

    Keep filter pane for the example

--- src/configs/DashboardsList.config.js
+++ src/configs/DashboardsList.config.js
@@ -6,7 +6,7 @@ export const DASHBOARDS_LIST_CONFIG = [
   {
     id: 0,
     title: 'Digital Twin Structure',
-    url: 'https://app.powerbi.com/reportEmbed?reportId=608b7bef-f5e3-4aae-b8db-19bbb38325d5&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionf3ef30b8ad34c9c2e8c4&$filter=StockProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27 and Bar/simulationrun%20eq%20%27<CsmSimulationRun>%27 and contains_Customer/simulationrun%20eq%20%27<CsmSimulationRun>%27 and arc_to_Customer/simulationrun%20eq%20%27<CsmSimulationRun>%27 and parameters/simulationrun%20eq%20%27<CsmSimulationRun>%27 and CustomerSatisfactionProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27&navContentPaneEnabled=false&filterPaneEnabled=false'
+    url: 'https://app.powerbi.com/reportEmbed?reportId=608b7bef-f5e3-4aae-b8db-19bbb38325d5&autoAuth=true&ctid=e413b834-8be8-4822-a370-be619545cb49&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D&pageName=ReportSectionf3ef30b8ad34c9c2e8c4&$filter=StockProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27 and Bar/simulationrun%20eq%20%27<CsmSimulationRun>%27 and contains_Customer/simulationrun%20eq%20%27<CsmSimulationRun>%27 and arc_to_Customer/simulationrun%20eq%20%27<CsmSimulationRun>%27 and parameters/simulationrun%20eq%20%27<CsmSimulationRun>%27 and CustomerSatisfactionProbe/SimulationRun%20eq%20%27<CsmSimulationRun>%27&navContentPaneEnabled=false'
   },
   {
     id: 1,
