100daeec8a35223fffd564690fb2b4ed6dec7960
Tristan Huet <tristan.huet@cosmotech.com>
2021-07-07 14:14:13 +0200

    chore: clean & update dependencies
     - add peer dependency of a date picker component in @cosmotech/ui
     - make i18next-parser a dev dependency
     - remove unused dependencies: history, react-loading
     - remove webVitals that will be replaced by Azure Application Insights
     - move react-scripts to dev dependencies
     - update dependencies to last minor update available
     - update dependencies to last major update available

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider, connect } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import i18n from './configs/i18next.config';
 import applicationStore from './state/Store.config';
 import { BrowserRouter } from 'react-router-dom';
@@ -41,8 +40,3 @@ ReactDOM.render(
     </Suspense>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
