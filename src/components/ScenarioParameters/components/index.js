03bfdb2fdee0c0c3499c3139a98462e897a6deeb
jreynard <jeremy.reynard@cosmotech.com>
2021-07-15 17:01:15 +0200

    feat:  Remove EditModeButton and NormalModeButton components

--- src/components/ScenarioParameters/components/index.js
+++ src/components/ScenarioParameters/components/index.js
@@ -1,6 +1,4 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-export { default as EditModeButton } from './EditModeButton';
-export { default as NormalModeButton } from './NormalModeButton';
 export { default as ScenarioParametersTabs } from './ScenarioParametersTabs';
