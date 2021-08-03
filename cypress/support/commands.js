b2288bcfac4a06473dbfe3b3ef59448fb58b22d7
Nicolas Borde <nicolas.borde@cosmotech.com>
2021-08-03 14:20:23 +0200

    test: add login command for Cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,22 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
+import { SELECTORS } from '../constants/IdConstants';
+import {
+  PAGE_NAME,
+  URL_ROOT
+} from '../constants/TestConstants.js';
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+const urlRegexWithAnySuffix = new RegExp(`^${URL_ROOT}/.*${PAGE_NAME.SCENARIOS}/.*`);
+
+// For this moment, login is handle by clicking on Microsoft Login button
+Cypress.Commands.add('login', () => {
+  cy.intercept('GET', urlRegexWithAnySuffix).as('requestUpdateCurrentScenario1');
+
+  cy.get(SELECTORS.login.microsoftLoginButton).click();
+
+  // TODO : Remove timeout due to loading page
+  cy.wait('@requestUpdateCurrentScenario1', { timeout: 10_000 });
+
+  cy.get(SELECTORS.scenario.parameters.tabs).should('be.visible');
+  cy.get(SELECTORS.scenario.view).should('be.visible');
+});
