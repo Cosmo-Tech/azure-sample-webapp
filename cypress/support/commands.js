f2a9a82d48ed710f81b79fd5dbfd61a679957b8f
Nicolas Borde <nicolas.borde@cosmotech.com>
2021-09-03 20:45:41 +0200

    chore: create utils file and do some code refacto

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,21 +1,18 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import { SELECTORS } from '../constants/IdConstants';
+import { SELECTORS } from '../commons/IdConstants.js';
 import {
-  PAGE_NAME,
-  URL_ROOT
-} from '../constants/TestConstants.js';
-
-const urlRegexWithAnySuffix = new RegExp(`^${URL_ROOT}/.*${PAGE_NAME.SCENARIOS}/.*`);
+  URL_REGEX
+} from '../commons/TestConstants.js';
 
 // For this moment, login is handle by clicking on Microsoft Login button
 Cypress.Commands.add('login', () => {
-  cy.intercept('GET', urlRegexWithAnySuffix).as('requestUpdateCurrentScenario1');
+  cy.intercept('GET', URL_REGEX.WITH_UNKNOWN_ID_SCENARIO_SUFFIX).as('requestUpdateCurrentScenario');
 
   cy.get(SELECTORS.login.microsoftLoginButton).click();
 
   // TODO : Remove timeout due to loading page
-  cy.wait('@requestUpdateCurrentScenario1', { timeout: 10_000 });
+  cy.wait('@requestUpdateCurrentScenario', { timeout: 10_000 });
 
   cy.get(SELECTORS.scenario.parameters.tabs).should('be.visible');
   cy.get(SELECTORS.scenario.view).should('be.visible');
