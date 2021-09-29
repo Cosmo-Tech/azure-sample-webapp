a93eec984418552c5834ca10e35b7294789cd7a7
jreynard <jeremy.reynard@cosmotech.com>
2021-09-29 14:37:45 +0200

    refactor: Modify props pattern of CreateScenarioButton to match with ui package

--- src/views/Scenario/Scenario.js
+++ src/views/Scenario/Scenario.js
@@ -110,6 +110,36 @@ const Scenario = (props) => {
     : t('views.scenario.dropdown.scenario.label', Scenario);
   const showBackdrop = currentScenario.status === STATUSES.LOADING;
 
+  const createScenarioDialogLabels = {
+    button: {
+      title: t('commoncomponents.button.create.scenario.label', 'Create Alternative Scenario'),
+      tooltip: createScenarioButtonToolType
+    },
+    dialog: {
+      title: t('commoncomponents.dialog.create.scenario.text.title', 'Create alternative scenario'),
+      scenarioName: t('commoncomponents.dialog.create.scenario.input.scenarioname.label'),
+      scenarioMaster: t('commoncomponents.dialog.create.scenario.checkbox.scenarioMaster.label',
+        'Master'),
+      scenarioParent: t('commoncomponents.dialog.create.scenario.dropdown.parentscenario.label',
+        'Parent scenario'),
+      datasetPlaceholder: t('commoncomponents.dialog.create.scenario.dropdown.dataset.placeholder',
+        'Dataset'),
+      dataset: t('commoncomponents.dialog.create.scenario.dropdown.dataset.label',
+        'Select a dataset'),
+      scenarioTypePlaceholder:
+              t('commoncomponents.dialog.create.scenario.dropdown.scenariotype.placeholder', 'Scenario'),
+      scenarioType: t('commoncomponents.dialog.create.scenario.dropdown.scenariotype.label',
+        'Scenario Type'),
+      cancel: t('commoncomponents.dialog.create.scenario.button.cancel', 'Cancel'),
+      create: t('commoncomponents.dialog.create.scenario.button.create', 'Create')
+    },
+    errors: {
+      emptyScenarioName: t('commoncomponents.dialog.create.scenario.input.scenarioname.error.empty'),
+      existingScenarioName: t('commoncomponents.dialog.create.scenario.input.scenarioname.error.existing'),
+      forbiddenCharsInScenarioName: t('commoncomponents.dialog.create.scenario.input.scenarioname.error.forbiddenchars')
+    }
+  };
+
   return (
     <>
       <Backdrop className={classes.backdrop} open={showBackdrop}>
@@ -152,8 +182,8 @@ const Scenario = (props) => {
                     scenarios={scenarioList.data}
                     user={user}
                     disabled={editMode}
-                    buttonTooltip={createScenarioButtonToolType}
                     nameValidator={NAME_VALIDATOR}
+                    labels={createScenarioDialogLabels}
                   />
                 </Grid>
               </Grid>
