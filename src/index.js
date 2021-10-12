646367d2683024eb6733c746dcfcc4569fa3204c
Tristan Huet <tristan.huet@cosmotech.com>
2021-10-12 18:25:37 +0200

    refactor: move I18N_NAMESPACE constant to i18next config file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider, connect } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import i18n from './services/config/i18next';
+import { i18next } from './services/config/i18next';
 import applicationStore from './state/Store.config';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
@@ -29,7 +29,7 @@ ReactDOM.render(
       <React.StrictMode>
           <BrowserRouter>
               <Provider store={applicationStore}>
-                  <I18nextProvider i18n={i18n}>
+                  <I18nextProvider i18n={i18next}>
                       <ThemeProvider theme={theme}>
                         <CssBaseline />
                         <ConnectedApp />
