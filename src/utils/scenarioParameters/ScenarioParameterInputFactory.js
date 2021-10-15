DorianMB/Azure-Web-App

feat: add tabs generation & automatic build of params data for update

src/utils/scenarioParameters/ScenarioParameterInputFactory.js
@@ -1,0 +1,36 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import {
+  BasicDateInputFactory,
+  BasicEnumTypesFactory,
+  BasicNumberInputFactory,
+  BasicTextInputFactory,
+  BasicToggleInputFactory
+} from './inputComponents';
+
+const VAR_TYPE_FACTORY_MAPPING = {
+  bool: BasicToggleInputFactory,
+  date: BasicDateInputFactory,
+  enum: BasicEnumTypesFactory,
+  int: BasicNumberInputFactory,
+  number: BasicNumberInputFactory,
+  string: BasicTextInputFactory,
+  '%DATASETID%': null
+};
+
+const create = (t, parameterData, parametersState, setParametersState, editMode) => {
+  const varTypeFactory = VAR_TYPE_FACTORY_MAPPING[parameterData.varType];
+  if (varTypeFactory === undefined) {
+    console.warn('No factory defined for varType ' + parameterData.varType);
+    return null;
+  }
+  if (varTypeFactory === null) {
+    return null;
+  }
+  return varTypeFactory.create(t, parameterData, parametersState, setParametersState, editMode);
+};
+
+export const ScenarioParameterInputFactory = {
+  create
+};

src/utils/scenarioParameters/inputComponents/index.js
@@ -1,0 +1,8 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+export { BasicDateInputFactory } from './BasicDateInputFactory';
+export { BasicEnumTypesFactory } from './BasicEnumTypesFactory';
+export { BasicNumberInputFactory } from './BasicNumberInputFactory';
+export { BasicTextInputFactory } from './BasicTextInputFactory';
+export { BasicToggleInputFactory } from './BasicToggleInputFactory';

Cosmo-Tech/azure-sample-webapp

src/utils/scenarioParameters/ScenarioParameterInputFactory.js
@@ -1,0 +1,36 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import {
+  BasicDateInputFactory,
+  BasicEnumInputFactory,
+  BasicNumberInputFactory,
+  BasicTextInputFactory,
+  BasicToggleInputFactory
+} from './inputComponents';
+
+const VAR_TYPE_FACTORY_MAPPING = {
+  bool: BasicToggleInputFactory,
+  date: BasicDateInputFactory,
+  enum: BasicEnumInputFactory,
+  int: BasicNumberInputFactory,
+  number: BasicNumberInputFactory,
+  string: BasicTextInputFactory,
+  '%DATASETID%': null
+};
+
+const create = (t, parameterData, parametersState, setParametersState, editMode) => {
+  const varTypeFactory = VAR_TYPE_FACTORY_MAPPING[parameterData.varType];
+  if (varTypeFactory === undefined) {
+    console.warn('No factory defined for varType ' + parameterData.varType);
+    return null;
+  }
+  if (varTypeFactory === null) {
+    return null;
+  }
+  return varTypeFactory.create(t, parameterData, parametersState, setParametersState, editMode);
+};
+
+export const ScenarioParameterInputFactory = {
+  create
+};

src/utils/scenarioParameters/inputComponents/index.js
@@ -1,0 +1,8 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+export { BasicDateInputFactory } from './BasicDateInputFactory';
+export { BasicEnumInputFactory } from './BasicEnumInputFactory';
+export { BasicNumberInputFactory } from './BasicNumberInputFactory';
+export { BasicTextInputFactory } from './BasicTextInputFactory';
+export { BasicToggleInputFactory } from './BasicToggleInputFactory';