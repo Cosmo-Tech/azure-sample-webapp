4f42bf6ca09333f0a8688bd31f6c277f4c62905c
Tristan Huet <tristan.huet@cosmotech.com>
2021-10-15 15:09:52 +0200

    test: remove testing-library dependency

--- rtl.setup.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cosmo Tech.
-// Licensed under the MIT license.
-
-// See https://github.com/kentcdodds/react-testing-library#global-config
-import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
