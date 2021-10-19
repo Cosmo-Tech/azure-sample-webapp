3da7e4b4d3b0b39413e5db5bcc0f0fe17a0fad2f
Tristan Huet <tristan.huet@cosmotech.com>
2021-10-19 15:06:12 +0200

    chore: create release scripts

---
+++ scripts/release/release.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# Copyright (c) Cosmo Tech.
+# Licensed under the MIT license.
+
+
+import os
+import sys
+import argparse
+from tools_common.git import (
+    check_head,
+    check_branch_is_main,
+    check_tag_exists,
+    commit_all_changes,
+    create_tag,
+    create_branch,
+    get_top_level_folder,
+    pull,
+    switch,
+)
+from tools_common.os import rm_dir, mv_file
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='''Create release tags for Azure Sample Webapp. These tags will not be pushed by the script, this
+        operation must be done by the user.
+        '''
+    )
+    parser.add_argument("-v", "--version", help="Version to release (e.g. 2.0.0)", required=True)
+    args = parser.parse_args()
+    return args.version
+
+
+def check_all(version_brewery, version_vanilla):
+    return (
+        # check_branch_is_main()
+        # and
+        pull()
+        and check_head()
+        and not check_tag_exists(version_brewery)
+        and not check_tag_exists(version_vanilla)
+        )
+
+
+def remove_specific_files():
+    root_folder = get_top_level_folder()
+    print(root_folder)
+    clean_cypress(root_folder)
+    clean_config(root_folder)
+
+
+def clean_cypress(root_folder):
+    files_to_remove = [
+        'commons/actions/brewery',
+        'commons/constants/brewery',
+        'integration/brewery',
+    ]
+    for file_to_remove in files_to_remove:
+        path = os.path.join(root_folder, 'cypress', file_to_remove)
+        rm_dir(path)
+
+
+def clean_config(root_folder):
+    config_file_path = os.path.join(root_folder, 'src/config/ScenarioParameters.js')
+    vanilla_config_file_path = os.path.join(root_folder, 'src/config/ScenarioParameters.vanilla.js')
+    mv_file(vanilla_config_file_path, config_file_path)
+
+
+def main():
+    version = parse_arguments()
+    version_brewery = f"v{version}-brewery"
+    version_vanilla = f"v{version}-vanilla"
+    branch_vanilla = f"release/{version}-vanilla"
+
+    if not check_all(version_brewery, version_vanilla):
+        print('error')
+        sys.exit(1)
+
+    create_tag(version_brewery)
+    create_branch(branch_vanilla)
+    remove_specific_files()
+    commit_all_changes(f'chore: prepare release {version_vanilla}')
+    create_tag(version_vanilla)
+    switch('main')
+    print('''
+The release script ran successfully. Please check created tags are correct, and push them with:
+
+git push --tags
+''')
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/tools_common/exec.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Cosmo Tech.
+# Licensed under the MIT license.
+
+
+import shlex
+import subprocess
+import time
+
+
+def run_command(command, cwd=None, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE):
+    """
+    Run a command and return its standard output, error output, return code and
+    execution time.
+    :param command: Command to run
+    :param cwd: Working directory to run the command
+    :param stdout: Standard output (valid values are subprocess.PIPE,
+    subprocess.DEVNULL, file descriptors or file objects, see subprocess
+    documentation for more information)
+    :param stderr: Standard error (valid values are subprocess.PIPE,
+    subprocess.DEVNULL, file descriptors or file objects, see subprocess
+    documentation for more information)
+    """
+    # Measure time & run
+    start = time.time()
+    p = subprocess.Popen(
+        shlex.split(command), stdout=stdout, stderr=stderr, cwd=cwd)
+    out, err = p.communicate()
+    duration = time.time() - start
+    ret_code = p.returncode
+    # Process output
+    out = out.decode('utf-8') if out else out
+    err = err.decode('utf-8') if err else err
+    return out, err, ret_code, duration
