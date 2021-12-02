aeb4e91ea1eadea02e98c1546938129046f99ba0
FMO-Cosmo <81355469+FMO-Cosmo@users.noreply.github.com>
2021-12-02 10:56:50 +0100

    change theme to CosmoLight

--- src/theme/custom/cosmoDark/picture.js
+++ src/theme/custom/cosmoDark/picture.js
@@ -3,6 +3,6 @@
 
 // Set here the logo and the login wallpaper url
 export default {
-  logo: 'theme/cosmotechDark.png',
-  auth: 'theme/authDark.png',
+  logo: 'theme/cosmotechLight.png',
+  auth: 'theme/authLight.png',
 };
