8c87dee799b314163ad27caff54ff2c06c8d1b05
Nicolas Borde <nicolas.borde@cosmotech.com>
2021-12-15 17:07:30 +0100

    feat: Ag Grid theme is now setable in themes config

---
+++ src/theme/custom/cosmoDark/grid.js
@@ -0,0 +1,7 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+// Set here the theme from Ag Grid
+export default {
+  agTheme: 'ag-theme-balham-dark',
+};
---
+++ src/theme/custom/default/grid.js
@@ -0,0 +1,9 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+// Set here the theme from Ag Grid
+// import 'ag-grid-community/dist/styles/ag-theme-balham.css'; // Default theme
+
+export default {
+  agTheme: 'ag-theme-balham',
+};
