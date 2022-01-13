4f3a9cbb9fbb3401152c5e19439b53468142f733
Nicolas Borde <nicolas.borde@cosmotech.com>
2022-01-13 19:26:40 +0100

    test: add About dialog e2e test

--- cypress/commons/actions/generic/AppBar.js
+++ cypress/commons/actions/generic/AppBar.js
@@ -33,10 +33,19 @@ function getSupportPageLink() {
   const supportPageLink = `a[href="${SUPPORT_URL}"]`;
   return cy.get(supportPageLink);
 }
+function getAboutButton() {
+  return cy.get(GENERIC_SELECTORS.helpMenu.aboutButton);
+}
+function getAboutDialogCloseButton() {
+  return cy.get(GENERIC_SELECTORS.helpMenu.aboutDialogCloseButton);
+}
+function getAboutDialog() {
+  return cy.get(GENERIC_SELECTORS.helpMenu.aboutDialog);
+}
 
 // User profile actions
 function openUserInfoMenu() {
-  getUserInfoMenuButton().click();
+  return getUserInfoMenuButton().click();
 }
 function openLanguageSelectorInMenu() {
   return getLanguageSelectorButton().click();
@@ -44,7 +53,6 @@ function openLanguageSelectorInMenu() {
 function selectLanguageInMenu(lang) {
   return getLanguageChangeButton(lang).click();
 }
-
 function switchLanguageTo(lang) {
   openUserInfoMenu();
   openLanguageSelectorInMenu();
@@ -54,6 +62,12 @@ function switchLanguageTo(lang) {
 // Help menu actions
 function openHelpMenu() {
   return getHelpMenu().click();
+}
+function openAboutDialog() {
+  return getAboutButton().click();
+}
+function closeAboutDialog() {
+  return getAboutDialogCloseButton().click();
 }
 
 export const AppBar = {
@@ -65,9 +79,13 @@ export const AppBar = {
   getHelpMenu,
   getDocumentationLink,
   getSupportPageLink,
+  getAboutButton,
+  getAboutDialog,
   openUserInfoMenu,
   openLanguageSelectorInMenu,
   selectLanguageInMenu,
   switchLanguageTo,
   openHelpMenu,
+  openAboutDialog,
+  closeAboutDialog,
 };
