3ebb7f165a07ad97cc5ead6a793bc1cc969b0659
Tristan Huet <tristan.huet@cosmotech.com>
2022-01-26 18:10:09 +0100

    chore: add functions in python scripts lib to manage version in package.json file

---
+++ scripts/tools_common/jq.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Cosmo Tech.
+# Licensed under the MIT license.
+
+
+import os
+from tools_common.exec import has_command, run_command
+
+
+def check_jq():
+    """
+    Check if jq is installed.
+    """
+    return has_command("jq")
+
+
+def _on_command_error(msg, out, err):
+    print(msg)
+    if out:
+        print(out)
+    if err:
+        print(err)
+
+
+def get_npm_package_version(project_root_folder):
+    """
+    Get project version from package.json.
+    """
+
+    command = "jq .version package.json"
+    out, err, ret_code, _ = run_command(command, cwd=project_root_folder)
+    if err or ret_code != 0:
+        _on_command_error('Something went wrong when parsing the package.json file.', out, err)
+        return None
+
+    try:
+        return out.split('\n')[0].replace('"', "")
+    except Exception:
+        _on_command_error('Something went wrong when parsing the package.json file.', out, err)
+        return None
+
+
+def set_npm_package_version(project_root_folder, new_version):
+    """
+    Set project version in package.json.
+    """
+    command = f"jq --arg version {new_version} '.version |= $version' package.json"
+    out, err, ret_code, _ = run_command(command, cwd=project_root_folder)
+    if err or ret_code != 0:
+        _on_command_error('Something went wrong when writing the new version in the package.json file.', out, err)
+        return False
+
+    package_file_path = os.path.join(project_root_folder, 'package.json')
+    with open(package_file_path, 'w+') as file:
+        file.write(out)
+    return True
