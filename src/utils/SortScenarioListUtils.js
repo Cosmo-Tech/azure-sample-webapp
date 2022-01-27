62b147fbed27da485a250c80c00d89d760380955
Nicolas Borde <nicolas.borde@cosmotech.com>
2022-01-27 13:52:37 +0100

    feat: add getFirstScenarioMaster function in utils

--- src/utils/SortScenarioListUtils.js
+++ src/utils/SortScenarioListUtils.js
@@ -5,10 +5,16 @@ import rfdc from 'rfdc';
 
 const clone = rfdc();
 
+const _sortByName = (scenarios) => {
+  return scenarios.sort((scenarioA, scenarioB) =>
+    scenarioA.name.toUpperCase() < scenarioB.name.toUpperCase() ? -1 : 1
+  );
+};
+
 export const sortScenarioList = (scenarioList) => {
   const sortedList = [];
   let scenarioListCopy = clone(scenarioList);
-  scenarioListCopy.sort((a, b) => (a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1));
+  _sortByName(scenarioListCopy);
 
   const buildScenarioTree = (idParent, depth) => {
     const scenarioListToFilter = [];
@@ -26,4 +32,17 @@ export const sortScenarioList = (scenarioList) => {
   buildScenarioTree(null, 0);
 
   return sortedList;
+};
+
+export const getFirstScenarioMaster = (scenarioList) => {
+  if (scenarioList.length > 0) {
+    const scenarioMasterList = scenarioList.filter((scenario) => scenario.parentId === null);
+    if (scenarioMasterList.length === 0) {
+      console.warn('Scenario list is corrupted, no root scenarios found');
+      return _sortByName(scenarioList)[0];
+    }
+    return _sortByName(scenarioMasterList)[0];
+  } else {
+    return null;
+  }
 };
