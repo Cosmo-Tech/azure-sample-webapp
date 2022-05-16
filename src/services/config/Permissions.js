c6c440d6602118f0fef05918b5ea325b069a44f3
Tristan Huet <tristan.huet@cosmotech.com>
2022-05-16 11:00:58 +0200

    feat: add new permission canChangeScenarioValidationStatus

--- src/services/config/Permissions.js
+++ src/services/config/Permissions.js
@@ -5,6 +5,7 @@
  * Defines permissions on WebApplication actions
  */
 export const PERMISSIONS = {
+  canChangeScenarioValidationStatus: 'can-change-scenario-validation-status',
   canCreateScenario: 'can-create-scenario',
   canDeleteScenario: 'can-delete-scenario',
   canEditOrLaunchScenario: 'can-edit-launch-scenario',
