d46fcd42aeaec6d86394c29199c7b52ca9086190
Tristan Huet <tristan.huet@cosmotech.com>
2022-06-07 10:47:12 +0200

    chore: remove CSP from staticwebapp.config.json and set up csp-html-webpack-plugin instead

---
+++ config-overrides.js
@@ -0,0 +1,44 @@
+const { override } = require('customize-cra');
+const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
+
+const cspConfigPolicy = {
+  'default-src': "'none'",
+  'base-uri': "'self'",
+  'connect-src': [
+    "'self'",
+    '*.api.cosmotech.com',
+    'https://login.microsoftonline.com',
+    'https://dc.services.visualstudio.com',
+  ],
+  'script-src': ["'self'"],
+  'img-src': ["'self'", 'data:'],
+  'style-src': ["'self'", "'unsafe-inline'"],
+  'font-src': ['data:'],
+  'frame-src': 'https://app.powerbi.com',
+  'manifest-src': "'self'",
+  'require-trusted-types-for': "'script'",
+};
+
+// Disable hashes and nonces for scripts and styles
+const cspConfigOptions = {
+  hashEnabled: {
+    'script-src': true,
+    'style-src': false,
+  },
+  nonceEnabled: {
+    'script-src': false,
+    'style-src': false,
+  },
+};
+
+function addCspHtmlWebpackPlugin(config) {
+  if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(new CspHtmlWebpackPlugin(cspConfigPolicy, cspConfigOptions));
+  }
+
+  return config;
+}
+
+module.exports = {
+  webpack: override(addCspHtmlWebpackPlugin),
+};
