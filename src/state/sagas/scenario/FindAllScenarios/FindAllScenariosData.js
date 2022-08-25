f316dfd4c1ea423ccb76f93b6c7134708260c15c
Tristan Huet <tristan.huet@cosmotech.com>
2022-08-25 14:16:38 +0200

    feat: add permissions info in scenarios security when fetching scenarios data

--- src/state/sagas/scenario/FindAllScenarios/FindAllScenariosData.js
+++ src/state/sagas/scenario/FindAllScenarios/FindAllScenariosData.js
@@ -1,18 +1,75 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import { put, takeEvery, call } from 'redux-saga/effects';
+import { call, put, select, takeEvery } from 'redux-saga/effects';
 import { SCENARIO_ACTIONS_KEY } from '../../../commons/ScenarioConstants';
 import { ORGANIZATION_ID } from '../../../../config/GlobalConfiguration';
 import { Api } from '../../../../services/config/Api';
 import { STATUSES } from '../../../commons/Constants';
 import { formatParametersFromApi } from '../../../../utils/ApiUtils';
+import { ACL_ROLES } from '../../../../services/config/accessControl';
+import { ScenariosUtils } from '../../../../utils';
+
+const getUserEmail = (state) => state.auth.userEmail;
+const getUserId = (state) => state.auth.userId;
+
+// TODO: Remove hard-coded values before mergiing branch
+// vvvvvvvvv CODE TO REMOVE vvvvvvvvvvvvvvvv
+const USERS_EMAILS = ['alice@somecompany.com', 'bob@somecompany.com', 'tristan.huet@cosmotech.com'];
+const FAKE_SECURITY_DATA_EMPTY = { default: [], accessControlList: [] };
+const FAKE_SECURITY_DATA_DEFAULT_READ_ONLY = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: [],
+};
+const FAKE_SECURITY_DATA_SPECIFIC_WRITERS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.WRITER],
+  })),
+};
+const FAKE_SECURITY_DATA_SPECIFIC_VALIDATORS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.VALIDATOR],
+  })),
+};
+const FAKE_SECURITY_DATA_SPECIFIC_ADMINS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.ADMIN],
+  })),
+};
+const FAKE_SECURITY_DATA_ARRAY = [
+  FAKE_SECURITY_DATA_EMPTY,
+  FAKE_SECURITY_DATA_DEFAULT_READ_ONLY,
+  FAKE_SECURITY_DATA_SPECIFIC_WRITERS,
+  FAKE_SECURITY_DATA_SPECIFIC_VALIDATORS,
+  FAKE_SECURITY_DATA_SPECIFIC_ADMINS,
+];
+const devPatchScenarioWithFakeSecurityData = (scenario) => {
+  const number = Number(scenario.id.match(/\d+/)?.[0] || '0');
+  const fakeSecurityData = FAKE_SECURITY_DATA_ARRAY[(number + 1) % 5];
+  scenario.security = {
+    ...scenario.security,
+    ...fakeSecurityData,
+  };
+
+  console.log(scenario.name + ' ' + (fakeSecurityData.accessControlList?.[0]?.roles?.[0] || 'none'));
+};
+// ^^^^^^^^^^ CODE TO REMOVE ^^^^^^^^^^
 
 // generators function
 export function* getAllScenariosData(workspaceId) {
   // yield keyword is here to milestone and save the action
+  const userEmail = yield select(getUserEmail);
+  const userId = yield select(getUserId);
   const { data } = yield call(Api.Scenarios.findAllScenarios, ORGANIZATION_ID, workspaceId);
   data.forEach((scenario) => (scenario.parametersValues = formatParametersFromApi(scenario.parametersValues)));
+  data.forEach((scenario) => devPatchScenarioWithFakeSecurityData(scenario));
+  data.forEach((scenario) => ScenariosUtils.patchScenarioWithUserPermissions(scenario, userEmail, userId));
   yield put({
     type: SCENARIO_ACTIONS_KEY.SET_ALL_SCENARIOS,
     list: data,
--- src/state/sagas/scenario/FindScenarioById/FindScenarioByIdData.js
+++ src/state/sagas/scenario/FindScenarioById/FindScenarioByIdData.js
@@ -1,24 +1,81 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, select, takeEvery } from 'redux-saga/effects';
 import { SCENARIO_ACTIONS_KEY } from '../../../commons/ScenarioConstants';
 import { STATUSES } from '../../../commons/Constants';
 import { ORGANIZATION_ID } from '../../../../config/GlobalConfiguration';
 import { formatParametersFromApi } from '../../../../utils/ApiUtils';
+import { ScenariosUtils } from '../../../../utils';
 import { SCENARIO_RUN_STATE } from '../../../../services/config/ApiConstants';
+import { ACL_ROLES } from '../../../../services/config/accessControl';
 import { Api } from '../../../../services/config/Api';
 import { dispatchSetApplicationErrorMessage } from '../../../dispatchers/app/ApplicationDispatcher';
 import { t } from 'i18next';
 
+// TODO: confirm if roles are stored by id or by email
+const getUserEmail = (state) => state.auth.userEmail;
+const getUserId = (state) => state.auth.userId;
+
+// TODO: Remove hard-coded values before mergiing branch
+// vvvvvvvvv CODE TO REMOVE vvvvvvvvvvvvvvvv
+const USERS_EMAILS = ['alice@somecompany.com', 'bob@somecompany.com', 'tristan.huet@cosmotech.com'];
+const FAKE_SECURITY_DATA_EMPTY = { default: [], accessControlList: [] };
+const FAKE_SECURITY_DATA_DEFAULT_READ_ONLY = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: [],
+};
+const FAKE_SECURITY_DATA_SPECIFIC_WRITERS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.WRITER],
+  })),
+};
+const FAKE_SECURITY_DATA_SPECIFIC_VALIDATORS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.VALIDATOR],
+  })),
+};
+const FAKE_SECURITY_DATA_SPECIFIC_ADMINS = {
+  default: [ACL_ROLES.SCENARIO.READER],
+  accessControlList: USERS_EMAILS.map((email) => ({
+    id: email,
+    roles: [ACL_ROLES.SCENARIO.ADMIN],
+  })),
+};
+const FAKE_SECURITY_DATA_ARRAY = [
+  FAKE_SECURITY_DATA_EMPTY,
+  FAKE_SECURITY_DATA_DEFAULT_READ_ONLY,
+  FAKE_SECURITY_DATA_SPECIFIC_WRITERS,
+  FAKE_SECURITY_DATA_SPECIFIC_VALIDATORS,
+  FAKE_SECURITY_DATA_SPECIFIC_ADMINS,
+];
+const devPatchScenarioWithFakeSecurityData = (scenario) => {
+  const number = Number(scenario.id.match(/\d+/)?.[0] || '0');
+  const fakeSecurityData = FAKE_SECURITY_DATA_ARRAY[(number + 1) % 5];
+  scenario.security = {
+    ...scenario.security,
+    ...fakeSecurityData,
+  };
+};
+// ^^^^^^^^^^ CODE TO REMOVE ^^^^^^^^^^
+
 export function* fetchScenarioByIdData(action) {
   try {
+    const userEmail = yield select(getUserEmail);
+    const userId = yield select(getUserId);
     yield put({
       type: SCENARIO_ACTIONS_KEY.SET_CURRENT_SCENARIO,
       status: STATUSES.LOADING,
     });
+
     const { data } = yield call(Api.Scenarios.findScenarioById, ORGANIZATION_ID, action.workspaceId, action.scenarioId);
     data.parametersValues = formatParametersFromApi(data.parametersValues);
+    devPatchScenarioWithFakeSecurityData(data);
+    ScenariosUtils.patchScenarioWithUserPermissions(data, userEmail, userId);
     yield put({
       type: SCENARIO_ACTIONS_KEY.SET_SCENARIO_VALIDATION_STATUS,
       status: STATUSES.SUCCESS,
