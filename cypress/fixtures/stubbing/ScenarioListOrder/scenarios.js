ef9e7b37032f59e0be1cbe3867ad5d3765452b12
Tristan Huet <tristan.huet@cosmotech.com>
2022-08-29 11:41:21 +0200

    test: add default data and templates for stubbing in cypress tests
    Co-authored-by: Nicolas Borde <nicolas.borde@cosmotech.com>

---
+++ cypress/fixtures/stubbing/ScenarioListOrder/scenarios.js
@@ -0,0 +1,41 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { SCENARIO_EXAMPLE } from '../default/scenarios';
+
+export const ALL_ROOT_SCENARIOS = [
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr01',
+    name: 'A1 - Test Cypress - Stubbed scenario 1',
+  },
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr02',
+    name: 'A2 - Test Cypress - Stubbed scenario 2',
+  },
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr03',
+    name: 'A3 - Test Cypress - Stubbed scenario 3',
+  },
+];
+
+export const PARENT_AND_CHILD_SCENARIOS = [
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr01',
+    name: 'A1 - Test Cypress - Stubbed root scenario 1',
+  },
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr02',
+    parentId: 's-stubbedscnr01',
+    name: 'A1.1 - Test Cypress - Stubbed child scenario 1.1',
+  },
+  {
+    ...SCENARIO_EXAMPLE,
+    id: 's-stubbedscnr03',
+    name: 'A2 - Test Cypress - Stubbed root scenario 2',
+  },
+];
---
+++ cypress/fixtures/stubbing/default/index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+export { ACTUAL_ADT_INSTANCE_DATASET, DEFAULT_DATASETS_LIST } from './datasets';
+export { SCENARIO_RUN_EXAMPLE, DEFAULT_SCENARIO_RUNS_LIST } from './scenarioRuns';
+export { SCENARIO_EXAMPLE, DEFAULT_SCENARIOS_LIST } from './scenarios';
+export { SOLUTION_PARAMETER_EXAMPLE, RUN_TEMPLATE_EXAMPLE, DEFAULT_SOLUTION } from './solution';
+export { WORKSPACE_EXAMPLE, DEFAULT_WORKSPACE } from './workspace';
+export { USER_EXAMPLE } from './users';
