c0bda1c8410bd095b50a27835c4e1f3529d6cf52
Vincent Carluer <vcarluer@gmail.com>
2022-11-08 19:11:35 +0100

    correct scope

--- src/config/GlobalConfiguration.js
+++ src/config/GlobalConfiguration.js
@@ -6,7 +6,7 @@ export const AZURE_TENANT_ID = 'e9641c78-d0d6-4d09-af63-168922724e7f';
 export const APP_REGISTRATION_CLIENT_ID = 'aa0096de-71da-47e5-aa7e-d6230e4183bd';
 
 // API information
-export const COSMOTECH_API_SCOPE = 'https://testmarketplace.azure.cosmo-platform.com';
+export const COSMOTECH_API_SCOPE = 'https://testmarketplace.azure.cosmo-platform.com/platform';
 export const DEFAULT_BASE_PATH = 'https://testmarketplace.azure.cosmo-platform.com';
 
 // Cosmo solution identifiers
