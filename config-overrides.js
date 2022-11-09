78439ea28bc80055de0bd87a98810c1c892f12d1
Vincent Carluer <vcarluer@gmail.com>
2022-11-09 11:38:32 +0100

    no more v1 in csp

--- config-overrides.js
+++ config-overrides.js
@@ -23,7 +23,7 @@ const cspConfigPolicy = {
     'https://login.microsoftonline.com',
     'https://dc.services.visualstudio.com',
     'https://testmarketplace.azure.cosmo-platform.com',
-    'https://api.privatetest.com/v1',
+    'https://api.privatetest.com',
   ].concat(extraConnectSources),
   'script-src': ["'self'"],
   'img-src': ["'self'", 'data:'],
