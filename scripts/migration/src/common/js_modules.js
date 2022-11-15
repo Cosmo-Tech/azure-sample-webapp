590f1be307a943838472ed60bcefc6b1cb4fbb17
Tristan Huet <tristan.huet@cosmotech.com>
2022-11-15 18:57:54 +0100

    chore: [PROD-10047] add migration script for v5.0.0

---
+++ scripts/migration/src/common/js_modules.js
@@ -0,0 +1,19 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+const fs = require('fs');
+const { join } = require('path');
+
+const copyFileToMJS = (inputFolder, inputFileName, outputFolder) => {
+  const newFileName = inputFileName.slice(0, -3) + 'mjs';
+  const oldFilePath = join(inputFolder, inputFileName);
+  const newFilePath = join(outputFolder, newFileName);
+  fs.copyFileSync(oldFilePath, newFilePath);
+  return newFilePath;
+};
+
+const parseESFile = async (filePath) => {
+  return await import(filePath);
+};
+
+module.exports = { copyFileToMJS, parseESFile };
