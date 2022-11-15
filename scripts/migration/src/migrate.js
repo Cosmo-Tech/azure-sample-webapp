ab73b899dc0cc0c9dd7751fe34a4683d18308e5d
Tristan Huet <tristan.huet@cosmotech.com>
2022-11-15 18:57:54 +0100

    chore: [PROD-10047] add migration script for v5.0.0

--- scripts/migration/src/migrate.js
+++ scripts/migration/src/migrate.js
@@ -7,10 +7,14 @@ const { getParserInstance } = require('./common/parser.js');
 const { runAllChecks } = require('./common/check.js');
 const { VALID_TARGET_VERSIONS, LATEST_VERSION } = require('./common/constants.js');
 const v3 = require('./v3/migrate_to_v3.js');
+const v5 = require('./v5/migrate_to_v5.js');
 
 const parser = getParserInstance();
 parser.add_argument('target', { help: `Target version for migration (e.g. ${LATEST_VERSION})` });
 parser.add_argument('-p', '--project-dir', { help: 'Root folder of the webapp to migrate', default: '.' });
+parser.add_argument('-s', '--solution', {
+  help: 'Path to solution file (used when migrating to v5 to generate a new solution YAML file)',
+});
 
 async function main() {
   const args = parser.parse_args();
@@ -18,6 +22,9 @@ async function main() {
   switch (args.target) {
     case 'v3':
       await v3.run(args);
+      break;
+    case 'v5':
+      await v5.run(args);
       break;
     default:
       console.error('Error: Unknown target version. Valid options are: ' + VALID_TARGET_VERSIONS.join(', '));
