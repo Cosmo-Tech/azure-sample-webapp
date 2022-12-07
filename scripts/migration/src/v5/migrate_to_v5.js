ab6819ca9452caa1b0afe69b420a56cd47df9cf4
Tristan Huet <tristan.huet@cosmotech.com>
2022-12-07 17:10:59 +0100

    chore: add handling of PowerBI configuration files in migration script

--- scripts/migration/src/v5/migrate_to_v5.js
+++ scripts/migration/src/v5/migrate_to_v5.js
@@ -1,14 +1,17 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-const { setRootFolder } = require('./project.js');
+const { createOutputFolder, setRootFolder } = require('./project.js');
 const { runAllChecks } = require('./check.js');
-const { dumpConfigToYaml } = require('./merge_config_in_solution_yaml.js');
+const { dumpConfigToYaml: migrateSolution } = require('./merge_config_in_solution_yaml.js');
+const { dumpConfigToYaml: migrateWorkspace } = require('./merge_config_in_workspace_yaml.js');
 
 const run = async (args) => {
   setRootFolder(args.project_dir);
-  runAllChecks(args.solution);
-  await dumpConfigToYaml(args.solution);
+  runAllChecks(args.solution, args.workspace);
+  createOutputFolder();
+  await migrateSolution(args.solution);
+  await migrateWorkspace(args.workspace);
 };
 
 module.exports = { run };
