e2cb29c09a7468c9abbae2495f2f3e2a2e73f5a0
Tristan Huet <tristan.huet@cosmotech.com>
2022-12-07 17:10:59 +0100

    chore: add handling of PowerBI configuration files in migration script

--- scripts/migration/src/v5/project.js
+++ scripts/migration/src/v5/project.js
@@ -1,9 +1,12 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
+const fs = require('fs');
 const { join, resolve } = require('path');
+const { copyFileToMJS } = require('../common/js_modules.js');
 
-const MANDATORY_CONFIG_FILES = ['ScenarioParameters.js'];
+const MANDATORY_CONFIG_FILES = [];
+const OPTIONAL_CONFIG_FILES = ['ScenarioParameters.js'];
 let rootFolder = null;
 
 const setRootFolder = (newRootFolder) => {
@@ -18,9 +21,26 @@ const getOutputFolder = () => {
   return join(rootFolder, 'config_v5');
 };
 
+const createOutputFolder = () => {
+  fs.mkdirSync(getOutputFolder(), { recursive: true });
+};
+
+const clearFileFromOutputFolder = (fileName) => {
+  const filePath = join(getOutputFolder(), fileName);
+  if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+};
+
+const copyConfigFileToMJS = (fileName) => {
+  return copyFileToMJS(getConfigFolder(), fileName, getOutputFolder());
+};
+
 module.exports = {
   setRootFolder,
   getConfigFolder,
   getOutputFolder,
+  createOutputFolder,
+  clearFileFromOutputFolder,
+  copyConfigFileToMJS,
   MANDATORY_CONFIG_FILES,
+  OPTIONAL_CONFIG_FILES,
 };
