4bfc90bac082dd298e35ed261c9b4ba5e70d38cb
Vincent Carluer <vcarluer@gmail.com>
2022-12-12 11:09:32 +0100

    pentest web app config

--- src/config/GlobalConfiguration.js
+++ src/config/GlobalConfiguration.js
@@ -3,12 +3,12 @@
 
 // Azure App registration details
 export const AZURE_TENANT_ID = 'e413b834-8be8-4822-a370-be619545cb49';
-export const APP_REGISTRATION_CLIENT_ID = 'd104fbd6-9464-45d5-a022-83c90ad56906';
+export const APP_REGISTRATION_CLIENT_ID = '4bd8a472-2aee-4815-8431-580eca3ed5a7';
 
 // API information
-export const COSMOTECH_API_SCOPE = 'http://dev.api.cosmotech.com/platform';
-export const DEFAULT_BASE_PATH = 'https://dev.api.cosmotech.com/v2';
+export const COSMOTECH_API_SCOPE = 'https://pentest.api.cosmotech.com/platform';
+export const DEFAULT_BASE_PATH = 'https://pentest.api.cosmotech.com/v2';
 
 // Cosmo solution identifiers
-export const ORGANIZATION_ID = 'O-gZYpnd27G7';
-export const WORKSPACE_ID = 'W-rXeBwRa0PM';
+export const ORGANIZATION_ID = 'o-jkj2p4pnd794';
+export const WORKSPACE_ID = 'w-mod05move57n';
