b2b20f341915a6db507a1779f08580e135fb3bc9
Tristan Huet <tristan.huet@cosmotech.com>
2022-12-14 14:26:21 +0100

    feat: add handling of instance view configuration files in migration script

--- scripts/migration/src/v5/migrate_to_v5.js
+++ scripts/migration/src/v5/migrate_to_v5.js
@@ -4,7 +4,7 @@
 const { createOutputFolder, setRootFolder } = require('./project.js');
 const { runAllChecks } = require('./check.js');
 const { dumpConfigToYaml: migrateSolution } = require('./merge_config_in_solution_yaml.js');
-const { dumpConfigToYaml: migrateWorkspace } = require('./merge_config_in_workspace_yaml.js');
+const { parseAndDumpWorkspaceConfig: migrateWorkspace } = require('./merge_config_in_workspace_yaml.js');
 
 const run = async (args) => {
   setRootFolder(args.project_dir);
