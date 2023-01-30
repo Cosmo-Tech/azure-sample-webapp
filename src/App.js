fc47a1951f15d7a49ab300480362f01a4392107e
Nicolas Borde <nicolas.borde@cosmotech.com>
2023-01-30 10:57:18 +0100

    chore: minor fix on theme provider tag

--- src/App.js
+++ src/App.js
@@ -93,20 +93,18 @@ const App = () => {
   }, [isConnecting, isAuthenticated, isLoading, applicationStatus]);
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <SessionTimeoutDialog
-          getRemainingTimeLabel={getRemainingTimeLabel}
-          labels={sessionTimeoutLabels}
-          open={isSessionTimeoutDialogOpen}
-          onClose={closeSessionTimeoutDialog}
-          onLogOut={logOut}
-          timeout={SESSION_TIMEOUT_PROMPT_DELAY_IN_SECONDS}
-        />
-        {getAppContent()}
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SessionTimeoutDialog
+        getRemainingTimeLabel={getRemainingTimeLabel}
+        labels={sessionTimeoutLabels}
+        open={isSessionTimeoutDialogOpen}
+        onClose={closeSessionTimeoutDialog}
+        onLogOut={logOut}
+        timeout={SESSION_TIMEOUT_PROMPT_DELAY_IN_SECONDS}
+      />
+      {getAppContent()}
+    </ThemeProvider>
   );
 };
 
