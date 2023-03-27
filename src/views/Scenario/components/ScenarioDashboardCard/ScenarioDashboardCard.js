4ce8acab86950010c76d2c0b374b7a76da8fc679
Tristan Huet <tristan.huet@cosmotech.com>
2023-03-27 17:23:23 +0200

    feat: [PROD-8729] add an accordion to make the scenario view PowerBI report collapsible

--- src/views/Scenario/components/ScenarioDashboardCard/ScenarioDashboardCard.js
+++ src/views/Scenario/components/ScenarioDashboardCard/ScenarioDashboardCard.js
@@ -1,61 +1,64 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import React, { useMemo } from 'react';
-import { Card, CardContent, Paper, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Card, Grid, Paper, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
 import { ScenarioPowerBiReport } from '../index';
 import { useScenarioDashboardCard } from './ScenarioDashboardCardHook';
-import { makeStyles } from '@mui/styles';
 import { useTranslation } from 'react-i18next';
 
-const useStyles = makeStyles((theme) => ({
-  dashboardTitle: {
-    display: 'grid',
-    alignItems: 'baseline',
-    gridTemplateColumns: 'repeat(10, 1fr)',
-    '& p:last-child': {
-      gridColumn: '2 / span 8',
-      justifySelf: 'center',
-    },
-    '& p:first-child': {
-      justifySelf: 'left',
-      gridColumn: 1,
-    },
-  },
-}));
+const STORAGE_DASHBOARDS_ACCORDION_EXPANDED_KEY = 'dashboardsAccordionExpanded';
+
 const ScenarioDashboardCard = () => {
-  const classes = useStyles();
   const { t } = useTranslation();
   const { hasScenarioBeenRun, isDashboardSync } = useScenarioDashboardCard();
 
-  const dashboardCardStyle = useMemo(
-    () =>
-      hasScenarioBeenRun
-        ? {
-            px: 3,
-            '&:last-child': { pb: 2 },
-            bgcolor: !isDashboardSync ? 'dashboard.warning' : undefined,
-          }
-        : null,
-    [hasScenarioBeenRun, isDashboardSync]
+  const [isDashboardsAccordionExpanded, setIsDashboardsAccordionExpanded] = useState(
+    localStorage.getItem(STORAGE_DASHBOARDS_ACCORDION_EXPANDED_KEY) === 'true'
   );
+  const toggleDashboardsAccordion = () => {
+    const isExpanded = !isDashboardsAccordionExpanded;
+    setIsDashboardsAccordionExpanded(isExpanded);
+    localStorage.setItem(STORAGE_DASHBOARDS_ACCORDION_EXPANDED_KEY, isExpanded);
+  };
+
   return (
     <Card component={Paper} sx={{ p: 0 }}>
-      <CardContent sx={dashboardCardStyle}>
-        {hasScenarioBeenRun && (
-          <div className={classes.dashboardTitle}>
-            <Typography sx={{ ml: 3, mb: 2 }}>{t('commoncomponents.iframe.title')}</Typography>
-            {!isDashboardSync && (
-              <Typography>
-                <WarningAmberOutlinedIcon sx={{ mr: 1, verticalAlign: 'text-bottom' }} />
-                {t('commoncomponents.iframe.scenario.results.warning.notSync')}
-              </Typography>
-            )}
-          </div>
-        )}
-        <ScenarioPowerBiReport />
-      </CardContent>
+      <Accordion
+        expanded={isDashboardsAccordionExpanded}
+        sx={{ pb: 1.5, pt: 1.5, bgcolor: hasScenarioBeenRun && !isDashboardSync ? 'dashboard.warning' : undefined }}
+      >
+        <AccordionSummary
+          data-cy="dashboards-accordion-summary"
+          expandIcon={<ExpandMoreIcon />}
+          onClick={toggleDashboardsAccordion}
+          sx={{ flexDirection: 'row-reverse', maxHeight: '48px' }}
+        >
+          <Grid
+            container
+            sx={{ direction: 'row', alignItems: 'center', justifyContent: 'space-between', flexWrap: 'nowrap' }}
+          >
+            <Grid sx={{ ml: '10px' }}>
+              <Typography>{t('commoncomponents.iframe.title', 'Dashboard')}</Typography>
+            </Grid>
+            <Grid item sx={{ flexGrow: '1' }}>
+              {!isDashboardSync && hasScenarioBeenRun && (
+                <div>
+                  <Typography sx={{ textAlign: 'center', width: '100%' }}>
+                    <WarningAmberOutlinedIcon sx={{ mr: 1, verticalAlign: 'text-bottom' }} />
+                    {t('commoncomponents.iframe.scenario.results.warning.notSync')}
+                  </Typography>
+                </div>
+              )}
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails sx={{ pl: '0px', width: '100%' }}>
+          <ScenarioPowerBiReport />
+        </AccordionDetails>
+      </Accordion>
     </Card>
   );
 };
