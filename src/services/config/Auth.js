06718656411ec29ba68fb94be71489447f1267a6
Tristan Huet <tristan.huet@cosmotech.com>
2023-05-12 12:25:19 +0200

    fix: use custom header csm-authorization to transfer api token to azure function
    This work-around is an attempt to prevent Azure Function from stripping
    some headers that we need to keep for token validation.

--- src/services/config/Auth.js
+++ src/services/config/Auth.js
@@ -9,7 +9,7 @@ export const POWER_BI_API_DEFAULT_SCOPE = 'https://analysis.windows.net/powerbi/
 
 const APP_REGISTRATION_CLIENT_ID = ConfigService.getParameterValue('APP_REGISTRATION_CLIENT_ID');
 const AZURE_TENANT_ID = ConfigService.getParameterValue('AZURE_TENANT_ID');
-const COSMOTECH_API_SCOPE = ConfigService.getParameterValue('COSMOTECH_API_SCOPE');
+export const COSMOTECH_API_SCOPE = ConfigService.getParameterValue('COSMOTECH_API_SCOPE');
 
 // AuthMSAL configuration
 const MSAL_CONFIG = {
