5f7d0d50c3d717c4925c1f3d86dfae30c10b0237
Tristan Huet <tristan.huet@cosmotech.com>
2023-05-25 20:17:28 +0200

    test: update stubbed list of datasets for marketplace API

--- cypress/fixtures/stubbing/default/datasets.js
+++ cypress/fixtures/stubbing/default/datasets.js
@@ -3,15 +3,17 @@
 
 export const DEFAULT_DATASET = {
   id: 'D-stbdefault',
-  name: 'Demo Brewery ADT reference',
+  name: 'Brewery Baby Dataset ADT',
   description: 'Brewery reference model in ADT',
   ownerId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
   tags: ['ADT', 'Brewery', 'Reference', 'dataset'],
   connector: {
-    id: 'c-pn2e3l74g9pd',
-    name: 'ADT Connector',
-    version: '2.3.3',
-    parametersValues: { AZURE_DIGITAL_TWINS_URL: 'https://o-gzypnd27g7-demobrewery.api.weu.digitaltwins.azure.net' },
+    id: 'c-dnv2kdogy2kn',
+    name: 'Brewery Baby Connector ADT',
+    version: '2.5.1',
+    parametersValues: {
+      AZURE_DIGITAL_TWINS_URL: 'https://o-qemo3e7y75mk-brewerybabydeploy.api.eus2.digitaltwins.azure.net',
+    },
   },
   fragmentsIds: null,
   validatorId: null,
@@ -19,16 +21,18 @@ export const DEFAULT_DATASET = {
 };
 
 export const ACTUAL_ADT_INSTANCE_DATASET = {
-  id: 'D-4jwyQnmv7jx',
-  name: 'Demo Brewery ADT reference',
+  id: 'd-wk6xmgd7meq8',
+  name: 'Brewery Baby Dataset ADT',
   description: 'Brewery reference model in ADT',
   ownerId: '3a869905-e9f5-4851-a7a9-3079aad49dff',
   tags: ['ADT', 'Brewery', 'Reference', 'dataset'],
   connector: {
-    id: 'c-pn2e3l74g9pd',
-    name: 'ADT Connector',
-    version: '2.3.3',
-    parametersValues: { AZURE_DIGITAL_TWINS_URL: 'https://o-gzypnd27g7-demobrewery.api.weu.digitaltwins.azure.net' },
+    id: 'c-dnv2kdogy2kn',
+    name: 'Brewery Baby Connector ADT',
+    version: '2.5.1',
+    parametersValues: {
+      AZURE_DIGITAL_TWINS_URL: 'https://o-qemo3e7y75mk-brewerybabydeploy.api.eus2.digitaltwins.azure.net',
+    },
   },
   fragmentsIds: null,
   validatorId: null,
