ff6cf176c219af7a5b6ec98fc87e29ffcb7cd8f1
Tristan Huet <tristan.huet@cosmotech.com>
2023-06-09 10:40:41 +0200

    test: update expected errors in tests when importing invalid files

--- cypress/integration/brewery/TableParameters-invalid_files.spec.js
+++ cypress/integration/brewery/TableParameters-invalid_files.spec.js
@@ -9,6 +9,35 @@ import { DEFAULT_SCENARIOS_LIST } from '../../fixtures/stubbing/default';
 const CSV_INVALID_FILE_PATH = 'customers_invalid.csv';
 const XLSX_INVALID_FILE_PATH = 'customers_invalid.xlsx';
 
+const EXPECTED_ERRORS_ON_CSV_IMPORT = [
+  { summary: 'Too few fields', loc: 'Line 2' },
+  { summary: 'Incorrect int value', loc: 'Line 3 , Column 2 ("age")' },
+  { summary: 'Incorrect bool value' },
+  { summary: 'Incorrect enum value' },
+  { summary: 'Incorrect date value' },
+  { summary: 'Incorrect number value' },
+  { summary: 'Incorrect int value' },
+  { summary: 'Incorrect bool value' },
+  { summary: 'Incorrect enum value' },
+  { summary: 'Incorrect date value' },
+  { summary: 'Incorrect number value' },
+];
+
+const EXPECTED_ERRORS_ON_XLSX_IMPORT = [
+  { summary: 'Empty field', loc: 'Line 2, Column 3 ("canDrinkAlcohol")' },
+  { summary: 'Empty field', loc: 'Line 2, Column 4 ("favoriteDrink")' },
+  { summary: 'Incorrect int value', loc: 'Line 3 , Column 2 ("age")' },
+  { summary: 'Incorrect bool value' },
+  { summary: 'Incorrect enum value' },
+  { summary: 'Incorrect date value' },
+  { summary: 'Incorrect number value' },
+  { summary: 'Incorrect int value' },
+  { summary: 'Incorrect bool value' },
+  { summary: 'Incorrect enum value' },
+  { summary: 'Incorrect date value' },
+  { summary: 'Incorrect number value' },
+];
+
 describe('Table parameters invalid files operations', () => {
   before(() => {
     stub.start({
@@ -27,22 +56,6 @@ describe('Table parameters invalid files operations', () => {
   });
 
   it('can import invalid files and display the errors panel', () => {
-    const checkErrorsPanel = () => {
-      const expectedErrors = [
-        { summary: 'Missing fields', loc: 'Line 1' },
-        { summary: 'Incorrect int value', loc: 'Line 2 , Column 1 ("age")' },
-        { summary: 'Incorrect bool value' },
-        { summary: 'Incorrect enum value' },
-        { summary: 'Incorrect date value' },
-        { summary: 'Incorrect number value' },
-        { summary: 'Incorrect int value' },
-        { summary: 'Incorrect bool value' },
-        { summary: 'Incorrect enum value' },
-        { summary: 'Incorrect date value' },
-        { summary: 'Incorrect number value' },
-      ];
-      BreweryParameters.checkCustomersErrorsPanelFromList(expectedErrors);
-    };
     ScenarioSelector.getScenarioSelectorInput().should('have.value', DEFAULT_SCENARIOS_LIST[0].name);
     ScenarioParameters.expandParametersAccordion();
     BreweryParameters.switchToCustomersTab();
@@ -50,9 +63,9 @@ describe('Table parameters invalid files operations', () => {
     BreweryParameters.getCustomersExportButton().should('be.visible');
     BreweryParameters.getCustomersErrorsPanel().should('not.exist');
     BreweryParameters.importCustomersTableData(CSV_INVALID_FILE_PATH);
-    checkErrorsPanel();
+    BreweryParameters.checkCustomersErrorsPanelFromList(EXPECTED_ERRORS_ON_CSV_IMPORT);
     BreweryParameters.importCustomersTableData(XLSX_INVALID_FILE_PATH);
-    checkErrorsPanel();
+    BreweryParameters.checkCustomersErrorsPanelFromList(EXPECTED_ERRORS_ON_XLSX_IMPORT);
     ScenarioParameters.discard();
     BreweryParameters.getCustomersErrorsPanel().should('not.exist');
   });
