1407d41a0a1be0ede3a3bd173a8fc1d530472d96
Guillaume Boudaille <guillaume.boudaille@epitech.eu>
2023-06-13 16:38:24 +0200

    test: Add cypress test to be sure that column grouping works

---
+++ cypress/integration/brewery/TableParameters-columnGrouping.spec.js
@@ -0,0 +1,39 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { Scenarios, ScenarioParameters, Login } from '../../commons/actions';
+import { BreweryParameters } from '../../commons/actions/brewery';
+import { stub } from '../../commons/services/stubbing';
+import { SOLUTIONS } from '../../fixtures/stubbing/TableParameters-columnGrouping/solution';
+
+describe('check if column grouping feature still up', () => {
+  before(() => {
+    stub.start({
+      GET_DATASETS: true,
+      GET_SCENARIOS: true,
+      GET_WORKSPACES: true,
+      GET_SOLUTIONS: true,
+      PERMISSIONS_MAPPING: true,
+    });
+    stub.setSolutions(SOLUTIONS);
+  });
+
+  beforeEach(() => {
+    Login.login();
+  });
+
+  after(() => {
+    stub.stop();
+  });
+
+  it('Look if column grouping is successfully displayed and visible', () => {
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.importCustomersTableData('customers.csv');
+    cy.get('.ag-header-group-cell-label:first').within(() => {
+      cy.get('.ag-header-group-text').should('have.text', 'GroupToSearch');
+    });
+  });
+});
