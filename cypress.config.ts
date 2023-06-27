0b352e9c45bbd75310f5471ca1c282e436becf96
Tristan Huet <tristan.huet@cosmotech.com>
2023-06-27 10:26:00 +0200

    chore: migrate cypress tests & config with v10 migration script

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  numTestsKeptInMemory: 0,
+  video: false,
+  watchForFileChanges: false,
+  requestTimeout: 60000,
+  responseTimeout: 60000,
+  viewportWidth: 1920,
+  viewportHeight: 966,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+});
