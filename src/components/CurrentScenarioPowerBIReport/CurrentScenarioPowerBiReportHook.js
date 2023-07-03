3fe8e42980b3bbb91fed8a978fc2c55a06ba3da9
Tristan Huet <tristan.huet@cosmotech.com>
2023-07-03 10:25:49 +0200

    refactor: factorize generic hook useDownloadLogsFile

--- src/components/CurrentScenarioPowerBIReport/CurrentScenarioPowerBiReportHook.js
+++ src/components/CurrentScenarioPowerBIReport/CurrentScenarioPowerBiReportHook.js
@@ -1,17 +1,13 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useCurrentScenarioData, useScenarioListData } from '../../state/hooks/ScenarioHooks';
 import { usePowerBIInfo } from '../../state/hooks/PowerBIHooks';
 import { useWorkspaceChartsLogInWithUserCredentials } from '../../state/hooks/WorkspaceHooks';
-import { useOrganizationId } from '../../state/hooks/OrganizationHooks';
-
-import ScenarioRunService from '../../services/scenarioRun/ScenarioRunService';
-import { LOG_TYPES } from '../../services/scenarioRun/ScenarioRunConstants';
-import { SCENARIO_RUN_LOG_TYPE } from '../../services/config/FunctionalConstants';
+import { useDownloadLogsFile } from '../../hooks/ScenarioRunHooks';
 
 import { getReportLabels } from './labels';
 
@@ -22,19 +18,7 @@ export const useCurrentScenarioPowerBiReport = () => {
   const scenarioListData = useScenarioListData();
   const reports = usePowerBIInfo();
   const logInWithUserCredentials = useWorkspaceChartsLogInWithUserCredentials();
-  const organizationId = useOrganizationId();
-
-  const downloadLogsFile = useCallback(
-    () =>
-      currentScenarioData?.lastRun
-        ? ScenarioRunService.downloadLogsFile(
-            organizationId,
-            currentScenarioData.lastRun,
-            LOG_TYPES[SCENARIO_RUN_LOG_TYPE]
-          )
-        : null,
-    [organizationId, currentScenarioData?.lastRun]
-  );
+  const downloadLogsFile = useDownloadLogsFile();
 
   const reportLabels = useMemo(() => getReportLabels(t), [t]);
   const language = useMemo(() => i18n.language, [i18n.language]);
