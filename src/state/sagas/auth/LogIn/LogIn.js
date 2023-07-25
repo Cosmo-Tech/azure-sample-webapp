94dc510b143622584405947c37aa63c9a6c1d19c
Tristan Huet <tristan.huet@cosmotech.com>
2023-07-25 16:16:08 +0200

    refactor: refactor login saga
    Co-authored-by: Elena Sasova <sassova.elena@gmail.com>
    Co-authored-by: Nicolas Borde <nicolas.borde@cosmotech.com>
    Co-authored-by: Guillaume Boudaille <guillaume.boudaille@epitech.eu>

--- src/state/sagas/auth/LogIn/LogIn.js
+++ src/state/sagas/auth/LogIn/LogIn.js
@@ -1,57 +1,38 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import { Auth } from '@cosmotech/core';
+
 import { put, takeEvery } from 'redux-saga/effects';
+import { Auth } from '@cosmotech/core';
 import { AUTH_ACTIONS_KEY, AUTH_STATUS } from '../../../commons/AuthConstants';
 
 const UNKNOWN_ERROR_MESSAGE =
   'Unknown error. Authentication failed\nIf the problem persists, please contact your administrator.';
 
-// Generator function to fetch authentication data
 export function* tryLogIn(action) {
   try {
-    // Start by signing in if an authentication provider is defined
     if (action.provider) {
       Auth.setProvider(action.provider);
       yield Auth.signIn();
     }
-    // Check if the user is authenticated
-    const authenticated = yield Auth.isUserSignedIn();
-    if (authenticated) {
-      const userRoles = Auth.getUserRoles();
-      const userPermissions = [];
 
-      // If the user is authenticated, set the auth data
-      yield put({
-        error: '',
-        type: AUTH_ACTIONS_KEY.SET_AUTH_DATA,
-        userEmail: Auth.getUserEmail(),
-        userId: Auth.getUserId(),
-        userName: Auth.getUserName(),
-        profilePic: Auth.getUserPicUrl(),
-        roles: userRoles,
-        permissions: userPermissions,
-        status: AUTH_STATUS.AUTHENTICATED,
-      });
-      localStorage.removeItem('logoutByTimeout');
-    } else {
-      yield put({
-        error: '',
-        type: AUTH_ACTIONS_KEY.SET_AUTH_DATA,
-        userEmail: '',
-        userId: '',
-        userName: '',
-        profilePic: '',
-        roles: [],
-        permissions: [],
-        status: AUTH_STATUS.ANONYMOUS,
-      });
-    }
+    const isAuthenticated = yield Auth.isUserSignedIn();
+    yield put({
+      error: '',
+      type: AUTH_ACTIONS_KEY.SET_AUTH_DATA,
+      userEmail: isAuthenticated ? Auth.getUserEmail() : '',
+      userId: isAuthenticated ? Auth.getUserId() : '',
+      userName: isAuthenticated ? Auth.getUserName() : '',
+      profilePic: isAuthenticated ? Auth.getUserPicUrl() : '',
+      roles: isAuthenticated ? Auth.getUserRoles() : [],
+      permissions: [],
+      status: isAuthenticated ? AUTH_STATUS.AUTHENTICATED : AUTH_STATUS.ANONYMOUS,
+    });
+
+    if (isAuthenticated) localStorage.removeItem('logoutByTimeout');
   } catch (error) {
     if (!error.errorMessage) console.error(error);
-    const errorMessage = error.errorMessage || UNKNOWN_ERROR_MESSAGE;
     yield put({
-      error: errorMessage,
+      error: error.errorMessage ?? UNKNOWN_ERROR_MESSAGE,
       type: AUTH_ACTIONS_KEY.SET_AUTH_DATA,
       userEmail: '',
       userId: '',
@@ -65,7 +46,6 @@ export function* tryLogIn(action) {
   }
 }
 
-// Watch authentication actions
 function* logIn() {
   yield takeEvery(AUTH_ACTIONS_KEY.REQUEST_LOG_IN, tryLogIn);
 }
