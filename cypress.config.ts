53f5cbce740280982ea0b082810dbdb7d83d4773
Tristan Huet <tristan.huet@cosmotech.com>
2023-07-25 19:57:44 +0200

    test: set experimental cypress option to decrease number of crashes in chromium-based browsers

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  experimentalMemoryManagement: true,
   numTestsKeptInMemory: 0,
   video: false,
   watchForFileChanges: false,
