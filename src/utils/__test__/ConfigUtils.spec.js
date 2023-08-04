5bb6d53a8c415104f06fc2ea13be0738e68ceb8f
Elena Sasova <sassova.elena@gmail.com>
2023-08-04 15:07:48 +0200

    test: [PROD-11970] add UT to check  warnings generation

--- src/utils/__test__/ConfigUtils.spec.js
+++ src/utils/__test__/ConfigUtils.spec.js
@@ -2,6 +2,8 @@
 // Licensed under the MIT license.
 
 import { ConfigUtils } from '../ConfigUtils';
+import merge from 'deepmerge';
+import { SolutionSchema } from '../schemas/SolutionSchema';
 
 describe('buildExtendedVarType with possible values', () => {
   test.each`
@@ -90,4 +92,213 @@ describe('getConversionMethod with possible values', () => {
       expect(res).toStrictEqual(expectedRes);
     }
   );
+});
+
+describe('warnings in case of solution misconfiguration', () => {
+  let spyConsoleWarn;
+
+  beforeAll(() => {
+    spyConsoleWarn = jest.spyOn(console, 'warn').mockImplementation();
+  });
+
+  afterEach(() => {
+    spyConsoleWarn.mockClear();
+  });
+
+  const solution = {
+    id: 'solution1',
+    organizationId: 'O-123456',
+    url: 'someUrl',
+    parameters: [
+      {
+        id: 'parameter1',
+        varType: 'int',
+        options: {
+          validation: 'someValidation',
+        },
+      },
+      {
+        id: 'parameter2',
+        varType: '%DATASETID%',
+        options: {
+          subType: 'TABLE',
+          columns: [
+            {
+              field: 'field1',
+              acceptsEmptyFields: true,
+            },
+            { field: 'field2', acceptsEmptyFields: false },
+          ],
+        },
+      },
+    ],
+    parameterGroups: [
+      {
+        id: 'pg1',
+        parameters: ['parameter1', 'parameter2'],
+      },
+    ],
+    runTemplates: [
+      {
+        id: 'rt1',
+        name: 'Run template 1',
+      },
+    ],
+  };
+
+  const solutionWithWrongType = solution.parameterGroups[0].parameters.push(5);
+  const solutionWithWrongKey = { ...solution, csmsimulator: 'csmsimulator' };
+  const solutionWithWrongParameterKey = {
+    ...solution,
+    parameters: [...solution.parameters, { id: 'p3', vartype: 'number' }],
+  };
+  const solutionWithWrongOptionsKey = {
+    ...solution,
+    parameters: [...solution.parameters, { id: 'p4', varType: '%DATASETID%', options: { subtype: 'TABLE' } }],
+  };
+  const solutionWithWrongColumnKey = {
+    ...solution,
+    parameters: [
+      ...solution.parameters,
+      { id: 'p5', varType: '%DATASETID%', options: { columns: [{ acceptsEmptyField: true }] } },
+    ],
+  };
+  const solutionWithWrongPGKey = {
+    ...solution,
+    parameterGroups: [...solution.parameterGroups, { id: 'pg2', istable: false }],
+  };
+  const solutionWithWrongPGOptionsKey = {
+    ...solution,
+    parameterGroups: [...solution.parameterGroups, { id: 'pg3', options: { authorizedroles: [] } }],
+  };
+  const solutionWithWrongRunTemplateKey = {
+    ...solution,
+    runTemplates: [...solution.runTemplates, { id: 'rt2', computesize: 'computesize' }],
+  };
+  const solutionWithSixWrongKeys = merge.all([
+    solutionWithWrongKey,
+    solutionWithWrongParameterKey,
+    solutionWithWrongOptionsKey,
+    solutionWithWrongPGKey,
+    solutionWithWrongPGOptionsKey,
+    solutionWithWrongRunTemplateKey,
+  ]);
+
+  const solutionWithDeeplyNestedColumnsGroup = {
+    ...solution,
+    parameters: [
+      {
+        id: 'nestedColumnsTable',
+        varType: '%DATASETID%',
+        options: {
+          subType: 'TABLE',
+          columns: [
+            { field: 'column1', type: ['int'] },
+            {
+              headerName: 'columnGroup1',
+              children: [
+                { field: 'column2' },
+                { field: 'column3' },
+                {
+                  headerName: 'columnGroup2',
+                  children: [
+                    { field: 'column4' },
+                    { field: 'column5' },
+                    {
+                      headerName: 'columnGroup3',
+                      children: [
+                        { field: 'column6' },
+                        { field: 'column7' },
+                        {
+                          headerName: 'columnGroup4',
+                          children: [
+                            { field: 'column8' },
+                            { field: 'column9' },
+                            {
+                              headerName: 'columnGroup5',
+                              children: [
+                                { field: 'column10' },
+                                { field: 'column11' },
+                                {
+                                  headerName: 'columnGroup6',
+                                  children: [
+                                    { field: 'column12' },
+                                    { field: 'column13' },
+                                    {
+                                      headerName: 'columnGroup7',
+                                      children: [{ field: 'column14' }, { field: 'column15' }],
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            { field: 'column16', type: ['int'] },
+          ],
+        },
+      },
+    ],
+  };
+
+  test.each`
+    solution
+    ${solution}
+    ${solutionWithWrongType}
+    ${undefined}
+    ${null}
+    ${{}}
+    ${solutionWithDeeplyNestedColumnsGroup}
+  `('checks console.warn is not displayed when no unknown keys in solution', ({ solution }) => {
+    ConfigUtils.checkUnknownKeysInConfig(SolutionSchema, solution);
+    expect(spyConsoleWarn).toHaveBeenCalledTimes(0);
+  });
+
+  test.each`
+    solution                           | expectedCalledWith                 | expectedKey
+    ${solutionWithWrongKey}            | ${'Your solution contains'}        | ${'csmsimulator'}
+    ${solutionWithWrongParameterKey}   | ${"Parameter with id 'p3'"}        | ${'vartype'}
+    ${solutionWithWrongOptionsKey}     | ${"Parameter with id 'p4'"}        | ${'subtype'}
+    ${solutionWithWrongColumnKey}      | ${"Parameter with id 'p5'"}        | ${'acceptsEmptyField'}
+    ${solutionWithWrongPGKey}          | ${"Parameter group with id 'pg2'"} | ${'istable'}
+    ${solutionWithWrongPGOptionsKey}   | ${"Parameter group with id 'pg3'"} | ${'authorizedroles'}
+    ${solutionWithWrongRunTemplateKey} | ${"Run template with id 'rt2'"}    | ${'computesize'}
+  `('checks console.warn with given solution config', ({ solution, expectedCalledWith, expectedKey }) => {
+    ConfigUtils.checkUnknownKeysInConfig(SolutionSchema, solution);
+    expect(spyConsoleWarn).toHaveBeenCalledTimes(1);
+    expect(spyConsoleWarn).toHaveBeenCalledWith(expect.stringContaining(expectedCalledWith));
+    expect(spyConsoleWarn).toHaveBeenCalledWith(expect.stringContaining(expectedKey));
+  });
+  test('checks six console.warns are displayed for six errors in config', () => {
+    ConfigUtils.checkUnknownKeysInConfig(SolutionSchema, solutionWithSixWrongKeys);
+    expect(spyConsoleWarn).toHaveBeenCalledTimes(6);
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(1, expect.stringContaining("Parameter with id 'p3'"));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(1, expect.stringContaining('vartype'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(2, expect.stringContaining("Parameter with id 'p4'"));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(2, expect.stringContaining('subtype'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(3, expect.stringContaining("Parameter group with id 'pg2'"));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(3, expect.stringContaining('istable'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(4, expect.stringContaining("Parameter group with id 'pg3'"));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(4, expect.stringContaining('authorizedroles'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(5, expect.stringContaining("Run template with id 'rt2'"));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(5, expect.stringContaining('computesize'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(6, expect.stringContaining('Your solution contains'));
+    expect(spyConsoleWarn).toHaveBeenNthCalledWith(6, expect.stringContaining('csmsimulator'));
+  });
+  test('error is detected in deeply nested columns object', () => {
+    const solutionWithDeeplyNestedColumnsGroupWithErrors = { ...solutionWithDeeplyNestedColumnsGroup };
+    // eslint-disable-next-line max-len
+    solutionWithDeeplyNestedColumnsGroupWithErrors.parameters[0].options.columns[1].children[2].children[2].children[2].children[2].children[2].children[2].headername =
+      'columnsGroup6';
+    ConfigUtils.checkUnknownKeysInConfig(SolutionSchema, solutionWithDeeplyNestedColumnsGroupWithErrors);
+    expect(spyConsoleWarn).toHaveBeenCalledTimes(1);
+    expect(spyConsoleWarn).toHaveBeenCalledWith(expect.stringContaining("Parameter with id 'nestedColumnsTable'"));
+    expect(spyConsoleWarn).toHaveBeenCalledWith(expect.stringContaining('headername'));
+  });
 });
