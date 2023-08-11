a683b44a67df51b5c80a5fcf5e13444ce3af41fd
Elena Sasova <sassova.elena@gmail.com>
2023-08-11 10:26:20 +0200

    test: [PROD-11970] add UT to check the function generating zod object for options

---
+++ src/utils/__test__/SchemasUtils.spec.js
@@ -0,0 +1,22 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { SchemasUtils } from '../schemas/SchemasUtils';
+
+describe('schema generation for custom options', () => {
+  test.each`
+    options                                | expectedKeys
+    ${['option']}                          | ${['option']}
+    ${['option1', 'option2']}              | ${['option1', 'option2']}
+    ${undefined}                           | ${[]}
+    ${null}                                | ${[]}
+    ${[]}                                  | ${[]}
+    ${'option'}                            | ${[]}
+    ${['option1', ['option2', 'option3']]} | ${['option1', 'option2', 'option3']}
+    ${[1, 2, 3]}                           | ${['1', '2', '3']}
+  `('schema is generated for custom options', ({ options, expectedKeys }) => {
+    const optionObject = SchemasUtils.getCustomOptionsZodObject(options);
+    const keySchema = optionObject.keyof();
+    expect(keySchema._def.values).toStrictEqual(expectedKeys);
+  });
+});
