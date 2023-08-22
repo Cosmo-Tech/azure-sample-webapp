6a743e85b2672e00334fd4d096058bf26f3fba82
Tristan Huet <tristan.huet@cosmotech.com>
2023-08-22 10:22:00 +0200

    test: add optional env variable to support staging API scope format

--- cypress/plugins/auth/authMSAL.config.js
+++ cypress/plugins/auth/authMSAL.config.js
@@ -4,20 +4,22 @@
 // c.f. README.md file in the cypress folder for a description of the env variables below
 const CY_MSAL_TENANT_ID = process.env.CY_MSAL_TENANT_ID;
 if (!CY_MSAL_TENANT_ID)
-  console.warn('MSAL tenant id is empty ! Please set CY_MSAL_TENANT_ID env variable when running cypress');
+  console.warn('MSAL tenant id is empty! Please set CY_MSAL_TENANT_ID env variable when running cypress');
 
 const CY_MSAL_CLIENT_ID = process.env.CY_MSAL_CLIENT_ID;
 if (!CY_MSAL_CLIENT_ID)
-  console.warn('MSAL client id is empty ! Please set CY_MSAL_CLIENT_ID env variable when running cypress');
+  console.warn('MSAL client id is empty! Please set CY_MSAL_CLIENT_ID env variable when running cypress');
 
 const CY_MSAL_CLIENT_SECRET = process.env.CY_MSAL_CLIENT_SECRET;
 if (!CY_MSAL_CLIENT_SECRET)
-  console.warn('MSAL client secret is empty ! Please set CY_MSAL_CLIENT_SECRET env variable when running cypress');
+  console.warn('MSAL client secret is empty! Please set CY_MSAL_CLIENT_SECRET env variable when running cypress');
 
 const CY_MSAL_API_HOSTNAME_FOR_SCOPE = process.env.CY_MSAL_API_HOSTNAME_FOR_SCOPE;
-if (!CY_MSAL_API_HOSTNAME_FOR_SCOPE)
+const CY_MSAL_SCOPE = process.env.CY_MSAL_SCOPE;
+if (!CY_MSAL_API_HOSTNAME_FOR_SCOPE && !CY_MSAL_SCOPE)
   console.warn(
-    'API hostname for MSAL scope is empty ! Please set CY_MSAL_API_HOSTNAME_FOR_SCOPE env variable when running cypress'
+    'API hostname for MSAL scope is empty! Please set CY_MSAL_SCOPE or CY_MSAL_API_HOSTNAME_FOR_SCOPE env variable ' +
+      'when running cypress'
   );
 
 const msalSecretConfig = {
@@ -29,7 +31,7 @@ const msalSecretConfig = {
 };
 
 const tokenRequest = {
-  scopes: [`http://${CY_MSAL_API_HOSTNAME_FOR_SCOPE}/.default`],
+  scopes: [CY_MSAL_SCOPE ?? `http://${CY_MSAL_API_HOSTNAME_FOR_SCOPE}/.default`],
 };
 
 module.exports = {
