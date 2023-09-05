4bd7ad64e68f41b55c0b7a9072deb064b2d59d0d
Elena Sasova <sassova.elena@gmail.com>
2023-09-05 15:40:14 +0200

    test: [PROD-12144] add UT for running state in ScenarioActions

---
+++ tests/MuiComponentsTesting/TypographyTesting.js
@@ -0,0 +1,18 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { getByDataCy } from '../utils';
+
+export class TypographyTesting {
+  constructor({ dataCy }) {
+    this._dataCy = dataCy;
+  }
+
+  get Typography() {
+    return getByDataCy(this._dataCy);
+  }
+
+  get text() {
+    return this.Typography.textContent;
+  }
+}
