f9efa7995d4eafb9cc24c53c84810befaec2797b
Tristan Huet <tristan.huet@cosmotech.com>
2023-09-13 16:24:41 +0200

    test: add cypress tests for dates input & timezones

---
+++ cypress/e2e/brewery/DatesAndTimezones.cy.js
@@ -0,0 +1,118 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { Scenarios, ScenarioSelector, ScenarioParameters, Login } from '../../commons/actions';
+import { BreweryParameters } from '../../commons/actions/brewery';
+import { stub } from '../../commons/services/stubbing';
+import { SCENARIOS, SOLUTION } from '../../fixtures/stubbing/DatesAndTimezones';
+
+// NOTE: this test should be launched from terminal with different timezones to check that timzeones have no impact on
+// the dates displayed
+
+describe('Scenario parameters of type date', () => {
+  before(() => {
+    stub.start({
+      GET_DATASETS: true,
+      GET_SCENARIOS: true,
+      GET_WORKSPACES: true,
+      GET_ORGANIZATION: true,
+      GET_SOLUTIONS: true,
+      UPDATE_SCENARIO: true,
+      PERMISSIONS_MAPPING: true,
+    });
+  });
+
+  beforeEach(() => {
+    Login.login();
+    stub.setScenarios(SCENARIOS);
+    stub.setSolutions([SOLUTION]);
+  });
+
+  after(() => {
+    stub.stop();
+  });
+
+  it('should display correct dates from saved values in existing scenario', () => {
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(SCENARIOS[0].name, SCENARIOS[0].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.getStartDateInput().should('value', '01/20/2025');
+    BreweryParameters.getEndDateInput().should('value', '07/25/2025');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+
+    BreweryParameters.getStartDateInput().clear();
+    BreweryParameters.getStartDateInput().type('07/25/2025');
+    BreweryParameters.getStartDateInput().should('value', '07/25/2025');
+    BreweryParameters.getEndDateHelperText().should('be.visible').contains('start_date').contains('end_date');
+
+    BreweryParameters.getEndDateInput().clear();
+    BreweryParameters.getEndDateInput().type('07/26/2025');
+    BreweryParameters.getEndDateInput().should('value', '07/26/2025');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+
+    const validateScenarioUpdateRequest = (req) => {
+      const values = req.body.parametersValues;
+      expect(values.find((el) => el.parameterId === 'start_date').value).to.equal('2025-07-25T00:00:00.000Z');
+      expect(values.find((el) => el.parameterId === 'end_date').value).to.equal('2025-07-26T00:00:00.000Z');
+    };
+    const saveOptions = { updateOptions: { validateRequest: validateScenarioUpdateRequest } };
+    ScenarioParameters.save(saveOptions);
+  });
+
+  it('should display correct dates from default values in unitialized scenario', () => {
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(SCENARIOS[1].name, SCENARIOS[1].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.getStartDateInput().should('value', '01/13/2025');
+    BreweryParameters.getEndDateInput().should('value', '07/13/2025');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+
+    BreweryParameters.getStartDateInput().clear();
+    BreweryParameters.getStartDateInput().type('07/13/2025');
+    BreweryParameters.getStartDateInput().should('value', '07/13/2025');
+    BreweryParameters.getEndDateHelperText().should('be.visible').contains('start_date').contains('end_date');
+
+    BreweryParameters.getEndDateInput().clear();
+    BreweryParameters.getEndDateInput().type('07/14/2025');
+    BreweryParameters.getEndDateInput().should('value', '07/14/2025');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+
+    const validateScenarioUpdateRequest = (req) => {
+      const values = req.body.parametersValues;
+      expect(values.find((el) => el.parameterId === 'start_date').value).to.equal('2025-07-13T00:00:00.000Z');
+      expect(values.find((el) => el.parameterId === 'end_date').value).to.equal('2025-07-14T00:00:00.000Z');
+    };
+    const saveOptions = { updateOptions: { validateRequest: validateScenarioUpdateRequest } };
+    ScenarioParameters.save(saveOptions);
+  });
+
+  it('should display correct dates for min & max dates', () => {
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(SCENARIOS[2].name, SCENARIOS[2].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.getStartDateInput().should('value', '01/13/2025');
+    BreweryParameters.getEndDateInput().should('value', '07/13/2025');
+    BreweryParameters.getStartDateHelperText().should('not.exist');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+
+    BreweryParameters.getStartDateInput().clear();
+    BreweryParameters.getStartDateInput().type('12/31/2024');
+    BreweryParameters.getStartDateHelperText().should('be.visible').contains('Minimum date').contains('01/01/2025');
+    BreweryParameters.getStartDateInput().clear();
+    BreweryParameters.getStartDateInput().type('01/02/2026');
+    BreweryParameters.getStartDateHelperText().should('be.visible').contains('Maximum date').contains('01/01/2026');
+    BreweryParameters.getStartDateInput().clear();
+    BreweryParameters.getStartDateInput().type('01/13/2025');
+    BreweryParameters.getStartDateHelperText().should('not.exist');
+
+    BreweryParameters.getEndDateInput().clear();
+    BreweryParameters.getEndDateInput().type('06/30/2025');
+    BreweryParameters.getEndDateHelperText().should('be.visible').contains('Minimum date').contains('07/01/2025');
+    BreweryParameters.getEndDateInput().clear();
+    BreweryParameters.getEndDateInput().type('07/02/2026');
+    BreweryParameters.getEndDateHelperText().should('be.visible').contains('Maximum date').contains('07/01/2026');
+    BreweryParameters.getEndDateInput().clear();
+    BreweryParameters.getEndDateInput().type('07/13/2025');
+    BreweryParameters.getEndDateHelperText().should('not.exist');
+  });
+});
---
+++ cypress/fixtures/stubbing/DatesAndTimezones/solutions.js
@@ -0,0 +1,42 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { DEFAULT_SOLUTION, RUN_TEMPLATE_EXAMPLE, SOLUTION_PARAMETER_EXAMPLE } from '../default';
+
+const SCENARIO_PARAMETERS = [
+  {
+    ...SOLUTION_PARAMETER_EXAMPLE,
+    id: 'start_date',
+    varType: 'date',
+    labels: null,
+    defaultValue: '2025-01-13T00:00:00Z', // Winter time, date at midnight UTC
+    minValue: '2025-01-01T00:00:00Z',
+    maxValue: '2026-01-01T00:00:00Z',
+  },
+  {
+    ...SOLUTION_PARAMETER_EXAMPLE,
+    id: 'end_date',
+    varType: 'date',
+    labels: null,
+    defaultValue: '2025-07-13T12:00:00Z', // Summer time, date NOT at midnight UTC
+    minValue: '2025-07-01T00:00:00Z',
+    maxValue: '2026-07-01T00:00:00Z',
+    options: { validation: '> start_date' },
+  },
+];
+
+export const SOLUTION = {
+  ...DEFAULT_SOLUTION,
+  parameters: SCENARIO_PARAMETERS,
+  parameterGroups: [{ id: 'dates', parameters: ['start_date', 'end_date'] }],
+  runTemplates: [
+    {
+      ...RUN_TEMPLATE_EXAMPLE,
+      id: 'dates',
+      name: 'Run template with dates',
+      description: 'Run template with dates',
+      tags: [],
+      parameterGroups: ['dates'],
+    },
+  ],
+};
