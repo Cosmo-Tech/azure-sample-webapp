987f1cfb977bde538deecf9b872d3e5148668225
Tristan Huet <tristan.huet@cosmotech.com>
2023-09-15 17:51:24 +0200

    fix: force date values received from (and sent to) back-end to be set to midnight UTC

--- src/utils/scenarioParameters/generic/ConversionFromString.js
+++ src/utils/scenarioParameters/generic/ConversionFromString.js
@@ -1,6 +1,7 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
+import { DateUtils } from '@cosmotech/core';
 import { DATASET_ID_VARTYPE } from '../../../services/config/ApiConstants';
 
 function _convertEnumFromString(parameterValue) {
@@ -24,7 +25,19 @@ function _convertBoolFromString(parameterValue) {
 }
 
 function _convertDateFromString(parameterValue) {
-  return new Date(parameterValue);
+  let parsedDate = DateUtils.parseISO(parameterValue);
+  if (!DateUtils.isValidDate(parsedDate)) {
+    parsedDate = new Date(parameterValue);
+    if (!DateUtils.isValidDate(parsedDate)) {
+      console.error(`Value "${parameterValue}" of date parameter couldn't be parsed.`);
+    } else {
+      console.warn(
+        `Value ${parameterValue} of date parameter does not match ISO format. Behavior may be inconsistent if ` +
+          'timezones are not defined.'
+      );
+    }
+  }
+  return DateUtils.getDateAtMidnightUTC(parsedDate);
 }
 
 function _convertDatasetIdFromString(parameterValue) {
