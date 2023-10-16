9c97340d9c440cbc52f0a542611147d2d88729e8
Tristan Huet <tristan.huet@cosmotech.com>
2023-10-16 16:08:51 +0200

    feat: [PROD-12490] add DatasetMetadata component

---
+++ src/views/DatasetManager/components/DatasetMetadata/components/MetadataItem/MetadataItem.js
@@ -0,0 +1,37 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Typography } from '@mui/material';
+import MetadataLabel from '../MetadataLabel';
+
+const MetadataItem = (props) => {
+  const { id, label, value, action } = props;
+
+  return value != null ? (
+    <Grid item data-cy={`dataset-metadata-${id}`}>
+      <Grid
+        container
+        sx={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', flexWrap: 'nowrap' }}
+      >
+        <MetadataLabel label={label}></MetadataLabel>
+        <Typography variant="body1">{value}</Typography>
+        {action}
+      </Grid>
+    </Grid>
+  ) : null;
+};
+
+MetadataItem.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  action: PropTypes.node,
+};
+
+MetadataItem.defaultProps = {
+  id: 'item',
+};
+
+export default MetadataItem;
