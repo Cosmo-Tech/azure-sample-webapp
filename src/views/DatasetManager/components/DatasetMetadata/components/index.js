b68aa4d2b04989871e5935b4179f5e3af32fac63
Tristan Huet <tristan.huet@cosmotech.com>
2023-10-16 16:08:51 +0200

    feat: [PROD-12490] add DatasetMetadata component

---
+++ src/views/DatasetManager/components/DatasetMetadata/components/index.js
@@ -0,0 +1,6 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+export { default as MetadataItem } from './MetadataItem';
+export { default as MetadataLabel } from './MetadataLabel';
+export { default as DescriptionEditor } from './DescriptionEditor';
