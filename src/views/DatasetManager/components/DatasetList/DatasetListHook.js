34ead7e55ae52c82aa5d0a393a57f532731d2899
Tristan Huet <tristan.huet@cosmotech.com>
2023-10-26 18:59:06 +0200

    fix: connect dataset list and metadata to redux

--- src/views/DatasetManager/components/DatasetList/DatasetListHook.js
+++ src/views/DatasetManager/components/DatasetList/DatasetListHook.js
@@ -1,168 +1,22 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-// import { useDatasetListData } from '../../../../../state/hooks/DatasetHooks';
-
 import { ResourceUtils } from '@cosmotech/core';
 import {
   useCurrentDataset,
+  useDatasetListData,
   useDeleteDataset,
   useRefreshDataset,
   useSelectDataset,
 } from '../../../../state/hooks/DatasetHooks';
 
 export const useDatasetList = () => {
-  // TODO: use the hook when it will be adapted to v3
-  // const datasetList = useDatasetListData();
-  const datasetList = [
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym7',
-      main: true,
-      name: 'Dataset 1',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: null,
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym6',
-      main: true,
-      name: 'Dataset 2',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: 'd-65rkx9r8ym7',
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym5',
-      main: true,
-      name: 'Dataset 3',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: null,
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym2',
-      main: true,
-      name: 'Dataset 4',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: 'd-65rkx9r8ym5',
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym9',
-      main: true,
-      name: 'Dataset 5',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: 'd-65rkx9r8ym5',
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-    {
-      creationDate: 1697444488457,
-      description: 'El pueblo unido jamas sara vencido',
-      fragmentsIds: null,
-      id: 'd-65rkx9r8ym3',
-      main: true,
-      name: 'Dataset 6',
-      organizationId: 'O-gZYpnd27G7',
-      ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-      parentId: 'd-65rkx9r8ym9',
-      queries: ['MATCH (n:Users) return n'],
-      refreshDate: 1697443823284,
-      security: null,
-      source: {
-        location: 'o-gzypnd27g7',
-        name: 'csmphoenixdev',
-        path: 'w-81264wr3xw5q5/demobrewery',
-      },
-      sourceType: 'Twincache',
-      status: 'READY',
-      tags: ['storage', 'file'],
-      twingraphId: 't-nyzr704dmor',
-      validatorId: null,
-    },
-  ];
+  const datasetList = useDatasetListData().filter((dataset) => dataset.main === true);
   const sortedDatasetList = ResourceUtils.getResourceTree(datasetList);
   const currentDataset = useCurrentDataset();
   const selectDataset = useSelectDataset();
+  if (currentDataset == null) selectDataset(sortedDatasetList?.[0] ?? null);
+
   const deleteDataset = useDeleteDataset();
   const refreshDatasetById = useRefreshDataset();
   return { sortedDatasetList, currentDataset, selectDataset, deleteDataset, refreshDatasetById };
--- src/views/DatasetManager/components/DatasetMetadata/DatasetMetadataHook.js
+++ src/views/DatasetManager/components/DatasetMetadata/DatasetMetadataHook.js
@@ -1,44 +1,9 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-// import { useCurrentDataset } from '../../../../state/hooks/DatasetHooks';
+import { useCurrentDataset } from '../../../../state/hooks/DatasetHooks';
 
 export const useDatasetMetadata = () => {
-  // const dataset = useCurrentDataset();
-
-  // TODO: replace hard-coded JSON below by "current dataset" from redux when available
-  const dataset = {
-    id: 'd-x10pzpjev43',
-    name: 'Brewery Aixo Mateix',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore ' +
-      'magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ' +
-      'commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat ' +
-      'nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ' +
-      'anim id est laborum.',
-    ownerId: '848be2c3-a28d-42f9-af18-89343426315b',
-    organizationId: 'O-gZYpnd27G7',
-    parentId: null,
-    twingraphId: 't-pyz9lgymrzy',
-    main: true,
-    creationDate: 1697443391520,
-    refreshDate: null,
-    sourceType: 'None',
-    source: null,
-    status: 'DRAFT',
-    queries: null,
-    tags: ['None', 'Brewery', 'Reference'],
-    connector: {
-      id: 'c-wdeg4orler5',
-      name: 'TwincacheConnector 0.4.1',
-      version: '0.4.1',
-      parametersValues: { TWIN_CACHE_NAME: 't-pyz9lgymrzy' },
-    },
-    fragmentsIds: null,
-    validatorId: null,
-    compatibility: null,
-    security: null,
-  };
-
+  const dataset = useCurrentDataset();
   return { dataset };
 };
