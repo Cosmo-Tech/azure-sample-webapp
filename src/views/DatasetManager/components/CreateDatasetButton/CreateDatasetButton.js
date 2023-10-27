9c96e0a525667a4d123e8675d03049da5bc2ce04
Elena Sasova <sassova.elena@gmail.com>
2023-10-27 14:57:26 +0200

    feat: [PROD-12492] add dataset creation wizard

--- src/views/DatasetManager/components/CreateDatasetButton/CreateDatasetButton.js
+++ src/views/DatasetManager/components/CreateDatasetButton/CreateDatasetButton.js
@@ -1,14 +1,23 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { IconButton } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
+import { CreateDatasetWizard } from './components/CreateDatasetWizard';
 
 export const CreateDatasetButton = () => {
+  const [isCreationWizardOpen, setIsCreationWizardOpen] = useState(false);
+  const handleCloseDialog = useCallback(() => {
+    setIsCreationWizardOpen(false);
+  }, [setIsCreationWizardOpen]);
+
   return (
-    <IconButton>
-      <AddIcon color="primary" />
-    </IconButton>
+    <div>
+      <IconButton onClick={() => setIsCreationWizardOpen(true)}>
+        <AddIcon color="primary" />
+      </IconButton>
+      <CreateDatasetWizard open={isCreationWizardOpen} closeDialog={handleCloseDialog} />
+    </div>
   );
 };
