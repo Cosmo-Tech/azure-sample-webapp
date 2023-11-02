1b017c70a06ddf216c77662b031002ac9a0bf908
Elena Sasova <sassova.elena@gmail.com>
2023-11-02 15:06:33 +0100

    feat: [PROD-12493] integrate datasetManager layout

--- src/views/DatasetManager/components/index.js
+++ src/views/DatasetManager/components/index.js
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 export { CreateDatasetButton } from './CreateDatasetButton';
-export { DatasetBrowser } from './DatasetBrowser';
 export { DatasetList } from './DatasetList';
 export { DatasetMetadata } from './DatasetMetadata';
 export { DatasetOverview } from './DatasetOverview';
