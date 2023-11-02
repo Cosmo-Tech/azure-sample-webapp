464b8a16d5fe4f049f851b91d263b35a6abeaf15
Elena Sasova <sassova.elena@gmail.com>
2023-11-02 15:06:33 +0100

    feat: [PROD-12493] integrate datasetManager layout

--- src/views/DatasetManager/DatasetManager.js
+++ src/views/DatasetManager/DatasetManager.js
@@ -2,20 +2,23 @@
 // Licensed under the MIT license.
 
 import React from 'react';
-import { Grid } from '@mui/material';
-import { DatasetBrowser, DatasetOverview } from './components';
+import { Card, Grid } from '@mui/material';
+import { DatasetList, DatasetMetadata, DatasetOverview } from './components';
 
 const DatasetManager = () => {
   return (
     <div data-cy="dataset-manager-view">
-      <Grid container spacing={0} sx={{ alignItems: 'stretch', justifyContent: 'flex-start' }}>
-        <Grid item xs={12} md={4}>
-          <DatasetBrowser />
+      <Card sx={{ m: 1, py: 3, px: 2 }}>
+        <Grid container spacing={0} sx={{ alignItems: 'stretch', justifyContent: 'flex-start' }}>
+          <Grid item xs={12} md={4}>
+            <DatasetList />
+            <DatasetMetadata />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <DatasetOverview />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={8}>
-          <DatasetOverview />
-        </Grid>
-      </Grid>
+      </Card>
     </div>
   );
 };
