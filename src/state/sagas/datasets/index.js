ca049c4c6a06cd733b0cff2da31a07d79c27a968
Nicolas Borde <nicolas.borde@cosmotech.com>
2023-11-14 20:34:02 +0100

    feat: [PROD-12042] implement dataset metadata update

--- src/state/sagas/datasets/index.js
+++ src/state/sagas/datasets/index.js
@@ -5,7 +5,8 @@ import { all, fork } from 'redux-saga/effects';
 import { findAllDatasetsData } from './FindAllDatasets';
 import { deleteDatasetSaga } from './DeleteDataset';
 import { createDatasetSaga } from './CreateDataset';
+import { updateDatasetSaga } from './UpdateDataset';
 
 export default function* datasetSaga() {
-  yield all([fork(findAllDatasetsData), fork(deleteDatasetSaga), fork(createDatasetSaga)]);
+  yield all([fork(findAllDatasetsData), fork(deleteDatasetSaga), fork(createDatasetSaga), fork(updateDatasetSaga)]);
 }
