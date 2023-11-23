614131e169439c045b15539d386ba4be01130fba
Nicolas Borde <nicolas.borde@cosmotech.com>
2023-11-23 16:15:16 +0100

    feat: fetch dataset overview queries results from API
    Co-authored-by: Tristan Huet <tristan.huet@cosmotech.com>

--- src/utils/WorkspacesUtils.js
+++ src/utils/WorkspacesUtils.js
@@ -26,6 +26,35 @@ const patchWorkspacesIfLocalConfigExists = async (originalWorkspaces) => {
   ArrayDictUtils.mergeArraysByElementsIds(originalWorkspaces, WORKSPACES);
 };
 
+const forgeDatasetManagerConfiguration = (config) => {
+  if (config == null) return;
+  const indicators = {
+    graphIndicators: config?.graphIndicators.map((kpi) => kpi.id),
+    categoriesKpis: [],
+  };
+  config?.categories.forEach((category) => category.kpis?.forEach((kpi) => indicators.categoriesKpis.push(kpi.id)));
+
+  const queriesMapping = {};
+  const addKpi = (kpi) => {
+    if (queriesMapping[kpi.queryId] === undefined) queriesMapping[kpi.queryId] = [kpi.id];
+    else queriesMapping[kpi.queryId].push(kpi.id);
+  };
+  config.graphIndicators.forEach(addKpi);
+  config.categories.forEach((category) => category.kpis?.forEach(addKpi));
+  return { indicators, queriesMapping };
+};
+
+const patchWorkspaceWithDatasetManagerConfiguration = (workspace) => {
+  try {
+    const config = forgeDatasetManagerConfiguration(workspace?.webApp?.options?.datasetManager);
+    workspace.indicators = config?.indicators;
+    workspace.queriesMapping = config?.queriesMapping;
+  } catch (error) {
+    console.warn('An error occurred while parsing the dataset manager queries. Data may be missing.');
+    console.error(error);
+  }
+};
+
 const addTranslationLabels = (workspace) => {
   try {
     TranslationUtils.addTranslationOfDatasetManagerLabels(workspace?.webApp?.options?.datasetManager ?? {});
@@ -39,4 +68,5 @@ export const WorkspacesUtils = {
   addTranslationLabels,
   patchWorkspacesIfLocalConfigExists,
   patchWorkspaceWithCurrentUserPermissions,
+  patchWorkspaceWithDatasetManagerConfiguration,
 };
