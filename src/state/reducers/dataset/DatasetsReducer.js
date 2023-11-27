d74ce6e27560f3b321d8865866e80d547d0862c2
Elena Sasova <sassova.elena@gmail.com>
2023-11-27 16:00:49 +0100

    refactor: refactor datasetReducer to constantly keep track of currently selected dataset index

---
+++ src/state/reducers/dataset/DatasetsReducer.js
@@ -0,0 +1,61 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import { createReducer } from '@reduxjs/toolkit';
+import { ResourceUtils } from '@cosmotech/core';
+import { STATUSES } from '../../commons/Constants';
+import { DATASET_ACTIONS_KEY } from '../../commons/DatasetConstants';
+import { DatasetsUtils } from '../../../utils';
+
+export const datasetInitialState = {
+  list: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+  selectedDatasetIndex: null,
+};
+
+export const datasetsReducer = createReducer(datasetInitialState, (builder) => {
+  builder
+    .addCase(DATASET_ACTIONS_KEY.GET_ALL_DATASETS, (state, action) => {
+      state.list.status = STATUSES.LOADING;
+      state.selectedDatasetIndex = null;
+    })
+    .addCase(DATASET_ACTIONS_KEY.SET_ALL_DATASETS, (state, action) => {
+      state.list.data = action.list;
+      state.list.status = action.status;
+    })
+    .addCase(DATASET_ACTIONS_KEY.ADD_DATASET, (state, action) => {
+      delete action.type;
+      state.list.data.push(action);
+    })
+    .addCase(DATASET_ACTIONS_KEY.SET_DATASET_SECURITY, (state, action) => {
+      state.list.data = state.list.data?.map((datasetData) => {
+        if (datasetData.id === action.datasetId) {
+          const datasetWithNewSecurity = { ...datasetData, security: action.security };
+          DatasetsUtils.patchDatasetWithCurrentUserPermissions(
+            datasetWithNewSecurity,
+            action.userEmail,
+            action.userId,
+            action.permissionsMapping
+          );
+          return { ...datasetData, security: datasetWithNewSecurity.security };
+        }
+        return datasetData;
+      });
+    })
+    .addCase(DATASET_ACTIONS_KEY.UPDATE_DATASET, (state, action) => {
+      const index = action.datasetIndex ?? state.list.data.findIndex((dataset) => dataset.id === action.datasetId);
+      state.list.data[index] = { ...state.list.data[index], ...action.datasetData };
+    })
+    .addCase(DATASET_ACTIONS_KEY.DELETE_DATASET, (state, action) => {
+      const index = state.list.data.findIndex((dataset) => dataset.id === action.datasetId);
+      state.list.data.splice(index, 1);
+    })
+    .addCase(DATASET_ACTIONS_KEY.SET_CURRENT_DATASET_INDEX, (state, action) => {
+      const datasetToSelect =
+        action.selectedDatasetId ||
+        ResourceUtils.getResourceTree(state.list.data?.filter((dataset) => dataset.main === true))?.[0]?.id;
+      state.selectedDatasetIndex = state.list.data.findIndex((dataset) => dataset.id === datasetToSelect);
+    });
+});
