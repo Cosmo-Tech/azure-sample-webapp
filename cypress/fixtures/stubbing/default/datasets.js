428814faec089e064041b812e15324613b267ba6
Tristan Huet <tristan.huet@cosmotech.com>
2023-11-28 18:22:57 +0100

    test: add selectors & test for datasets list

--- cypress/fixtures/stubbing/default/datasets.js
+++ cypress/fixtures/stubbing/default/datasets.js
@@ -1,7 +1,8 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-export const DEFAULT_DATASET = {
+// Legacy dataset with API v2.x structure
+export const LEGACY_DATASET_V2 = {
   id: 'D-stbdefault',
   name: 'Demo Brewery ADT reference',
   description: 'Brewery reference model in ADT',
@@ -16,6 +17,34 @@ export const DEFAULT_DATASET = {
   fragmentsIds: null,
   validatorId: null,
   compatibility: null,
+};
+
+export const DEFAULT_DATASET = {
+  organizationId: 'O-stbdorgztn',
+  parentId: null,
+  twingraphId: 't-stbdtwngrph',
+  main: false,
+  id: 'D-stbdefault',
+  name: 'Local file dataset',
+  description: 'Local file dataset description',
+  ownerId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+  tags: ['File', 'Brewery'],
+  sourceType: 'File',
+  source: { location: 'none', name: null, path: null, jobId: null },
+  status: 'READY',
+  connector: {
+    id: 'c-q1vjg10e5ej',
+    name: 'TwincacheConnector 0.4.1',
+    version: '0.4.1',
+    parametersValues: { TWIN_CACHE_NAME: 't-95j9q5jw17q' },
+  },
+  fragmentsIds: null,
+  validatorId: null,
+  compatibility: null,
+  creationDate: null,
+  refreshDate: null,
+  queries: null,
+  security: null,
 };
 
 export const ACTUAL_ADT_INSTANCE_DATASET = {
