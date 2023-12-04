29a1f521fc153865d25f0082964e1f11cc546372
Elena Sasova <sassova.elena@gmail.com>
2023-12-04 11:50:26 +0100

    feat: upload twingraph with fetch API

--- src/state/sagas/datasets/CreateDataset/CreateDataset.js
+++ src/state/sagas/datasets/CreateDataset/CreateDataset.js
@@ -1,11 +1,34 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import { put, takeEvery, call } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 import { t } from 'i18next';
+import { Auth } from '@cosmotech/core';
 import { Api } from '../../../../services/config/Api';
 import { DATASET_ACTIONS_KEY } from '../../../commons/DatasetConstants';
 import { dispatchSetApplicationErrorMessage } from '../../../dispatchers/app/ApplicationDispatcher';
+import { TWINGRAPH_STATUS } from '../../../../services/config/ApiConstants';
+
+const uploadZipWithFetchApi = async (organizationId, datasetId, file) => {
+  try {
+    const tokens = await Auth.acquireTokens();
+    const headers = {
+      Authorization: 'Bearer ' + tokens.accessToken,
+      'Content-Type': 'application/octet-stream',
+    };
+    return await fetch(`${Api.defaultBasePath}/organizations/${organizationId}/datasets/${datasetId}`, {
+      method: 'POST',
+      headers,
+      body: file,
+    });
+  } catch (error) {
+    console.error(error);
+    dispatchSetApplicationErrorMessage(
+      error,
+      t('commoncomponents.banner.twingraphNotCreated', 'A problem occurred during twingraph creation')
+    );
+  }
+};
 
 export function* createDataset(action) {
   const dataset = action.dataset;
@@ -23,12 +46,21 @@ export function* createDataset(action) {
       selectedDatasetId: data.id,
     });
 
-    if (dataset.sourceType !== 'None') {
+    if (!['None', 'File'].includes(dataset.sourceType)) {
       yield put({
         type: DATASET_ACTIONS_KEY.TRIGGER_SAGA_REFRESH_DATASET,
         organizationId,
         datasetId: data.id,
       });
+    } else if (dataset.sourceType === 'File') {
+      const response = yield call(uploadZipWithFetchApi, organizationId, data.id, dataset.file.file);
+      if (response?.status === 201) {
+        yield put({
+          type: DATASET_ACTIONS_KEY.UPDATE_DATASET,
+          datasetId: data.id,
+          datasetData: { status: TWINGRAPH_STATUS.READY },
+        });
+      }
     }
   } catch (error) {
     console.error(error);
