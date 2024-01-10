184897743d2192aa7d295168ad921a668963a3f6
Tristan Huet <tristan.huet@cosmotech.com>
2024-01-10 16:58:33 +0100

    fix: fix hidden item at the end of datasets list & make it more compact

--- src/views/DatasetManager/DatasetManager.js
+++ src/views/DatasetManager/DatasetManager.js
@@ -12,10 +12,10 @@ const DatasetManager = () => {
 
   return mainDatasets?.length > 0 ? (
     <div data-cy="dataset-manager-view" style={{ height: '100%', overflow: 'auto' }}>
-      <Card sx={{ m: 1, py: 3, px: 2, height: 'calc(100% - 16px)' }}>
+      <Card sx={{ m: 1, pt: 2, pb: 3, px: 2, height: 'calc(100% - 16px)' }}>
         <Grid container spacing={0} sx={{ alignItems: 'stretch', justifyContent: 'flex-start', height: '100%' }}>
           <Grid item xs={12} md={4} sx={{ height: '100%' }}>
-            <div style={{ height: '100%', overflow: 'auto', display: 'flex', flexFlow: 'column nowrap' }}>
+            <div style={{ height: '100%', display: 'flex', flexFlow: 'column nowrap' }}>
               <div style={{ flex: '1', minHeight: '300px', overflow: 'auto' }}>
                 <DatasetList />
               </div>
