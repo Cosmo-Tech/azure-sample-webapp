7251bf5ff77d8b688cbed507c5605530871c0e57
Tristan Huet <tristan.huet@cosmotech.com>
2024-01-17 17:31:48 +0100

    feat: [PROD-12930] add current user permissions in organization security in redux

--- src/services/config/accessControl/Permissions.js
+++ src/services/config/accessControl/Permissions.js
@@ -2,6 +2,21 @@
 // Licensed under the MIT license.
 
 export const ACL_PERMISSIONS = {
+  DATASET: {
+    READ: 'read',
+    READ_SECURITY: 'read_security',
+    WRITE: 'write',
+    DELETE: 'delete',
+    WRITE_SECURITY: 'write_security',
+  },
+  ORGANIZATION: {
+    READ: 'read',
+    READ_SECURITY: 'read_security',
+    CREATE_CHILDREN: 'create_children',
+    WRITE: 'write',
+    DELETE: 'delete',
+    WRITE_SECURITY: 'write_security',
+  },
   SCENARIO: {
     READ: 'read',
     READ_SECURITY: 'read_security',
