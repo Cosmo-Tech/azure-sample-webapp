ba08c078527dec5374f004fc9abeed0c41521840
Tristan Huet <tristan.huet@cosmotech.com>
2024-01-17 17:42:54 +0100

    fix: [PROD-12930] hide dataset creation button when lacking CREATE_CHILDREN permission

--- src/views/DatasetManager/components/DatasetList/DatasetListHook.js
+++ src/views/DatasetManager/components/DatasetList/DatasetListHook.js
@@ -8,13 +8,22 @@ import {
   useRefreshDataset,
   useSelectDataset,
 } from '../../../../state/hooks/DatasetHooks';
+import { useOrganizationData } from '../../../../state/hooks/OrganizationHooks';
 
 export const useDatasetList = () => {
+  const userPermissionsInCurrentOrganization = useOrganizationData()?.security?.currentUserPermissions ?? [];
   const datasets = useDatasets();
   const selectDataset = useSelectDataset();
   const currentDataset = useCurrentDataset();
 
   const deleteDataset = useDeleteDataset();
   const refreshDatasetById = useRefreshDataset();
-  return { datasets, currentDataset, selectDataset, deleteDataset, refreshDatasetById };
+  return {
+    userPermissionsInCurrentOrganization,
+    datasets,
+    currentDataset,
+    selectDataset,
+    deleteDataset,
+    refreshDatasetById,
+  };
 };
