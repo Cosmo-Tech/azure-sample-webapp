19a0067cd7b6e47234ec87ca439ff90bfa0a2b9d
Tristan Huet <tristan.huet@cosmotech.com>
2024-01-31 18:52:26 +0100

    test: add partial stubbing & interception of workspace-dataset link queries
    This commit does not actually implement the stubbing of datasets' field
    `linkedWorkspaceIdList` nor workspaces' field `linkedDatasetIdList`, it only
    adds the interception of queries to the /link endpoint when creating datasets
    with stubbing enabled.

--- cypress/fixtures/stubbing/default/workspaces.js
+++ cypress/fixtures/stubbing/default/workspaces.js
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 import { ROLES } from '../../../commons/constants/generic/TestConstants';
+import { DEFAULT_DATASETS_LIST } from './datasets';
 
 const defaultPowerBIReport = {
   title: { en: 'Scenario dashboard', fr: 'Rapport du scénario' },
@@ -52,6 +53,7 @@ export const WORKSPACE_EXAMPLE = {
   useDedicatedEventHubNamespace: true,
   sendScenarioMetadataToEventHub: true,
   security: { default: ROLES.SCENARIO.ADMIN, accessControlList: [] },
+  linkedDatasetIdList: DEFAULT_DATASETS_LIST.map((dataset) => dataset.id),
 };
 
 export const DEFAULT_WORKSPACE = WORKSPACE_EXAMPLE;
