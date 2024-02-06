90559ba86994d57dc3d7ee9a2e41ca72bd4696dd
Tristan Huet <tristan.huet@cosmotech.com>
2024-02-06 17:04:57 +0100

    fix: [PROD-13004] display error message when invalid dataset files are uploaded

--- src/views/DatasetManager/components/ReuploadFileDatasetButton/ReuploadFileDatasetButtonHook.js
+++ src/views/DatasetManager/components/ReuploadFileDatasetButton/ReuploadFileDatasetButtonHook.js
@@ -3,10 +3,12 @@
 
 import { useOrganizationId } from '../../../../state/hooks/OrganizationHooks';
 import { usePollTwingraphStatus, useUpdateDatasetInStore } from '../../../../state/hooks/DatasetHooks';
+import { useSetApplicationErrorMessage } from '../../../../state/hooks/ApplicationHooks';
 
 export const useReuploadFileDatasetButton = () => {
   const organizationId = useOrganizationId();
   const pollTwingraphStatus = usePollTwingraphStatus();
   const updateDatasetInStore = useUpdateDatasetInStore();
-  return { organizationId, pollTwingraphStatus, updateDatasetInStore };
+  const setApplicationErrorMessage = useSetApplicationErrorMessage();
+  return { organizationId, pollTwingraphStatus, setApplicationErrorMessage, updateDatasetInStore };
 };
