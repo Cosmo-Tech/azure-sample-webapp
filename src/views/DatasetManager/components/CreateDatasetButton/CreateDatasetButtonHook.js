c1ed3d4fe04cead0ef6b01c3b184605208985ae0
Tristan Huet <tristan.huet@cosmotech.com>
2024-03-27 11:59:19 +0100

    refactor: move generic function forgeRunnerParameters to SolutionUtils

--- src/views/DatasetManager/components/CreateDatasetButton/CreateDatasetButtonHook.js
+++ src/views/DatasetManager/components/CreateDatasetButton/CreateDatasetButtonHook.js
@@ -5,7 +5,7 @@ import { DATASET_SOURCE_TYPE } from '../../../../services/config/ApiConstants';
 import { useCreateDataset } from '../../../../state/hooks/DatasetHooks';
 import { useCreateRunner } from '../../../../state/hooks/RunnerHooks';
 import { useSolutionData } from '../../../../state/hooks/SolutionHooks';
-import { ArrayDictUtils } from '../../../../utils';
+import { ArrayDictUtils, SolutionsUtils } from '../../../../utils';
 
 export const useCreateDatasetOrRunner = () => {
   const createDataset = useCreateDataset();
@@ -14,25 +14,7 @@ export const useCreateDatasetOrRunner = () => {
 
   return useCallback(
     (values) => {
-      const getParametersForRunner = (values) => {
-        if (values == null) return [];
-        const solutionParameters = solutionData.parameters;
-
-        return Object.entries(values).map(([key, value]) => {
-          const solutionParameter = solutionParameters.find((param) => param.id === key);
-          const parameter = {
-            parameterId: key,
-            varType: solutionParameter?.varType,
-            value,
-          };
-          if (solutionParameter?.varType === '%DATASETID%')
-            parameter.connectorId = solutionParameter?.options?.connectorId;
-          return parameter;
-        });
-      };
-
       ArrayDictUtils.removeUndefinedValuesFromDict(values);
-
       const sourceType = values.sourceType;
       const dataset = { name: values.name, tags: values.tags, description: values.description, sourceType };
 
@@ -48,7 +30,7 @@ export const useCreateDatasetOrRunner = () => {
         createDataset(dataset);
       } else {
         const runner = { ...dataset };
-        runner.parametersValues = getParametersForRunner(values[sourceType]);
+        runner.parametersValues = SolutionsUtils.forgeRunnerParameters(solutionData, values[sourceType]);
         createRunner(runner);
       }
     },
