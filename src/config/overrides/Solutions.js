2ca3ae19aac1489f7af2933a22c10b0894f0ebd9
Tristan Huet <tristan.huet@cosmotech.com>
2024-04-10 20:12:36 +0200

    fix: [DO NOT MERGE] temporary solution patch for dynamic values

--- src/config/overrides/Solutions.js
+++ src/config/overrides/Solutions.js
@@ -4,4 +4,22 @@
 // Use the SOLUTIONS array below to override or add information to your solutions. This can be useful for development
 // purposes, but it is recommended to leave this array empty and use the API to update your Solution instead for
 // production environments.
-export const SOLUTIONS = [];
+export const SOLUTIONS = [
+  {
+    id: 'SOL-VkqXyNONQyB',
+    parameters: [
+      {
+        id: 'etl_param_subdataset_filter_is_thirsty',
+        varType: 'enum',
+        options: {
+          enumValues: undefined,
+          dynamicEnumValues: {
+            type: 'cypher',
+            query: 'MATCH(n:Customer) RETURN n.id as id',
+            resultKey: 'id',
+          },
+        },
+      },
+    ],
+  },
+];
