da0ec0a2811208a107f57b68166ce9143523777e
Tristan Huet <tristan.huet@cosmotech.com>
2024-04-24 17:10:25 +0200

    feat: enable dynamic values with MultiSelect component

---
+++ src/components/ScenarioParameters/components/ScenarioParametersInputs/GenericMultiSelect.js
@@ -0,0 +1,183 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { CircularProgress, Grid, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { MultiSelect } from '@cosmotech/ui';
+import { Api } from '../../../../services/config/Api';
+import { dispatchSetApplicationErrorMessage } from '../../../../state/dispatchers/app/ApplicationDispatcher';
+import { useOrganizationId } from '../../../../state/hooks/OrganizationHooks';
+import { ConfigUtils, TranslationUtils } from '../../../../utils';
+
+const useStyles = makeStyles((theme) => ({
+  error: {
+    color: theme.palette.error.main,
+  },
+}));
+
+export const GenericMultiSelect = ({
+  parameterData,
+  context,
+  parameterValue,
+  setParameterValue,
+  isDirty,
+  gridItemProps,
+}) => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const organizationId = useOrganizationId();
+  const isUnmounted = useRef(false);
+  useEffect(() => () => (isUnmounted.current = true), []);
+
+  // Possible value types for dynamicEnumValues:
+  // - 'undefined' when dynamic values are not enabled in the parameter configuration
+  // - 'null' when fetching data, a placeholder with a spinner will be displayed instead of the dropdown list
+  // - a list when the dynamic values have been retrieved sucessfully, these values will be shown in the dropdown list
+  // - a string when an error occured, this string should be the error message to display
+  const [dynamicEnumValues, setDynamicEnumValues] = useState(null);
+
+  useEffect(() => {
+    if (isUnmounted.current) return;
+    const dynamicSourceConfig = parameterData.options?.dynamicEnumValues;
+
+    const fetchDynamicEnumValues = async () => {
+      if (!dynamicSourceConfig) return;
+      if (context.targetDatasetId == null) {
+        console.error(`No dataset id forwarded to the enum parameter, can't fetch enum values dynamically.`);
+        return;
+      }
+
+      const query = { query: dynamicSourceConfig.query };
+      let data;
+      try {
+        data = await Api.Datasets.twingraphQuery(organizationId, context.targetDatasetId, query);
+      } catch (error) {
+        console.warn(`An error occurred when loading dynamic enum values of parameter "${parameterData.id}"`);
+        console.error(error);
+        const errorTitle = t(
+          'genericcomponent.enumInput.fetchingDynamicValuesError',
+          'Impossible to retrieve dynamic values from data source'
+        );
+        console.log(error);
+        dispatch(dispatchSetApplicationErrorMessage(error, errorTitle));
+        if (!isUnmounted.current) setDynamicEnumValues(errorTitle);
+        return;
+      }
+
+      const newDynamicEnumValues = data.data.map((item) => ({
+        key: item[dynamicSourceConfig.resultKey],
+        value: item[dynamicSourceConfig.resultKey],
+      }));
+      if (!isUnmounted.current) setDynamicEnumValues(newDynamicEnumValues);
+    };
+
+    setDynamicEnumValues(dynamicSourceConfig === undefined ? undefined : null);
+    fetchDynamicEnumValues();
+    // Only re-run this effect when the target dataset (e.g. parent dataset for sub-dataset creation), hence the
+    // disabled eslint warnings for missing hook dependencies
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context.targetDatasetId]);
+
+  const enumValues = useMemo(() => {
+    if (Array.isArray(dynamicEnumValues)) return dynamicEnumValues;
+
+    const rawEnumValues = ConfigUtils.getParameterAttribute(parameterData, 'enumValues') ?? [];
+    if (rawEnumValues.length === 0 && parameterData.options?.dynamicEnumValues == null) {
+      console.warn(
+        `Enum values are not defined for scenario parameter "${parameterData.id}".\n` +
+          'Please either provide an array in the "options.enumValues" field, or use "options.dynamicEnumValues" for ' +
+          'this parameter in the parameters configuration file.'
+      );
+    }
+
+    for (const enumValue of rawEnumValues) {
+      const valueTranslationKey = TranslationUtils.getParameterEnumValueTranslationKey(parameterData.id, enumValue.key);
+      const tooltipTranslationKey = TranslationUtils.getParameterEnumValueTooltipTranslationKey(
+        parameterData.id,
+        enumValue.key
+      );
+      enumValue.value = t(valueTranslationKey, enumValue.value);
+      enumValue.tooltip = t(tooltipTranslationKey, '');
+      delete enumValue.tooltipText;
+    }
+    return rawEnumValues;
+  }, [t, parameterData, dynamicEnumValues]);
+
+  useEffect(() => {
+    // Call setParameterValue when dynamic values are enabled to trigger an update of the form validity. This
+    // is required to automatically enable a form confirmation button when a default value is selected from
+    // dynamic values (e.g. in the dataset creation wizard). Yet, we don't want this behavior in the scenario
+    // parameters, because it tends to set the form state to "dirty" for enums without default values.
+    if (dynamicEnumValues != null && parameterValue == null && enumValues?.length > 0) {
+      setParameterValue([enumValues[0]?.key]);
+    }
+  }, [enumValues, dynamicEnumValues, parameterValue, setParameterValue]);
+
+  const loadingValuesPlaceholder = useMemo(
+    () => (
+      <>
+        <CircularProgress data-cy="fetching-dynamic-values-spinner" size="1rem" color="inherit" />
+        <Typography sx={{ px: 2 }}>
+          {t('genericcomponent.enumInput.fetchingDynamicValues', 'Fetching list of values...')}
+        </Typography>
+      </>
+    ),
+    [t]
+  );
+
+  const errorMessage = useMemo(
+    () =>
+      typeof dynamicEnumValues === 'string' ? (
+        <Typography sx={{ px: 2 }} className={classes.error}>
+          {dynamicEnumValues}
+        </Typography>
+      ) : null,
+    [dynamicEnumValues, classes.error]
+  );
+
+  const labels = useMemo(() => {
+    return {
+      label: t(TranslationUtils.getParameterTranslationKey(parameterData.id), parameterData.id),
+      noValues: t('genericcomponent.multiSelect.noValues', 'No selected values'),
+    };
+  }, [t, parameterData.id]);
+
+  return (
+    errorMessage ?? (
+      <Grid item xs={3} {...gridItemProps}>
+        <Grid container direction="row" alignItems="stretch">
+          {dynamicEnumValues === null ? (
+            loadingValuesPlaceholder
+          ) : (
+            <MultiSelect
+              id={parameterData.id}
+              labels={labels}
+              tooltipText={t(TranslationUtils.getParameterTooltipTranslationKey(parameterData.id), '')}
+              selectedKeys={Array.isArray(parameterValue) ? parameterValue : []}
+              onChange={setParameterValue}
+              disabled={!context.editMode}
+              options={enumValues}
+              isDirty={isDirty}
+            />
+          )}
+        </Grid>
+      </Grid>
+    )
+  );
+};
+
+GenericMultiSelect.propTypes = {
+  parameterData: PropTypes.object.isRequired,
+  context: PropTypes.object.isRequired,
+  parameterValue: PropTypes.any,
+  setParameterValue: PropTypes.func.isRequired,
+  isDirty: PropTypes.bool,
+  gridItemProps: PropTypes.object,
+};
+GenericMultiSelect.defaultProps = {
+  isDirty: false,
+};
