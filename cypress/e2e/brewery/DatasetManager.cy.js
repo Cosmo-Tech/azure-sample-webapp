c4bd4bcbc9a5eaf5f83e9f8c9efb2c00c5198ae4
Tristan Huet <tristan.huet@cosmotech.com>
2024-04-29 14:37:45 +0200

    refactor: move some dataset manager tests in a separate file

--- cypress/e2e/brewery/DatasetManager.cy.js
+++ cypress/e2e/brewery/DatasetManager.cy.js
@@ -5,65 +5,13 @@ import { stub } from '../../commons/services/stubbing';
 import {
   DATASETS,
   WORKSPACE,
-  WORKSPACE_WITHOUT_CONFIG,
   DATASETS_TO_FILTER,
   DATASETS_TO_REFRESH,
   ORGANIZATION_WITH_DEFAULT_ROLE_USER,
-  ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER,
 } from '../../fixtures/stubbing/DatasetManager';
 import { USER_EXAMPLE } from '../../fixtures/stubbing/default';
 
-const WORKSPACES = [WORKSPACE, WORKSPACE_WITHOUT_CONFIG];
-
-describe('Dataset manager view is optional', () => {
-  before(() => {
-    stub.start();
-    stub.setWorkspaces(WORKSPACES);
-  });
-  beforeEach(() => Login.login({ url: '/W-stbbdbrwryNoDM', workspaceId: 'W-stbbdbrwryNoDM' }));
-  after(stub.stop);
-
-  it('should not show the Dataset Manager tab when its configuration is not defined', () => {
-    DatasetManager.getDatasetManagerTab().should('not.exist');
-  });
-});
-
-describe('Viewer role in an empty dataset manager', () => {
-  before(() => {
-    stub.start();
-    stub.setOrganizations([ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER]);
-    stub.setWorkspaces(WORKSPACES);
-  });
-  beforeEach(() => Login.login({ url: '/W-stbbdbrwryWithDM', workspaceId: 'W-stbbdbrwryWithDM' }));
-  after(stub.stop);
-
-  it('cannot create datasets from the overview placeholder', () => {
-    DatasetManager.switchToDatasetManagerView();
-    DatasetManager.getDatasetCreationDialog().should('not.exist');
-    DatasetManager.getNoDatasetsPlaceholder().should('be.visible');
-    DatasetManager.getNoDatasetsPlaceholderViewerSubtitle().should('be.visible');
-    DatasetManager.getNoDatasetsPlaceholderUserSubtitle().should('not.exist');
-    DatasetManager.getCreateDatasetButton().should('not.exist');
-  });
-});
-
-describe('Viewer role in a non-empty dataset manager', () => {
-  before(() => {
-    stub.start();
-    stub.setOrganizations([ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER]);
-    stub.setWorkspaces(WORKSPACES);
-    stub.setDatasets([...DATASETS]);
-  });
-  beforeEach(() => Login.login({ url: '/W-stbbdbrwryWithDM', workspaceId: 'W-stbbdbrwryWithDM' }));
-  after(stub.stop);
-
-  it('cannot create datasets from the dataset list button', () => {
-    DatasetManager.switchToDatasetManagerView();
-    DatasetManager.getDatasetCreationDialog().should('not.exist');
-    DatasetManager.getCreateDatasetButton().should('not.exist');
-    DatasetManager.getNoDatasetsPlaceholder().should('not.exist');
-  });
-});
+const WORKSPACES = [WORKSPACE];
 
 describe('Dataset manager can be empty on start', () => {
   before(() => {
---
+++ cypress/e2e/brewery/DatasetManager_Restrictions.cy.js
@@ -0,0 +1,62 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import { Login, DatasetManager } from '../../commons/actions';
+import { stub } from '../../commons/services/stubbing';
+import {
+  DATASETS,
+  WORKSPACE,
+  WORKSPACE_WITHOUT_CONFIG,
+  ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER,
+} from '../../fixtures/stubbing/DatasetManager';
+
+const WORKSPACES = [WORKSPACE, WORKSPACE_WITHOUT_CONFIG];
+
+describe('Dataset manager view is optional', () => {
+  before(() => {
+    stub.start();
+    stub.setWorkspaces(WORKSPACES);
+  });
+  beforeEach(() => Login.login({ url: '/W-stbbdbrwryNoDM', workspaceId: 'W-stbbdbrwryNoDM' }));
+  after(stub.stop);
+
+  it('should not show the Dataset Manager tab when its configuration is not defined', () => {
+    DatasetManager.getDatasetManagerTab().should('not.exist');
+  });
+});
+
+describe('Viewer role in an empty dataset manager', () => {
+  before(() => {
+    stub.start();
+    stub.setOrganizations([ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER]);
+    stub.setWorkspaces(WORKSPACES);
+  });
+  beforeEach(() => Login.login({ url: '/W-stbbdbrwryWithDM', workspaceId: 'W-stbbdbrwryWithDM' }));
+  after(stub.stop);
+
+  it('cannot create datasets from the overview placeholder', () => {
+    DatasetManager.switchToDatasetManagerView();
+    DatasetManager.getDatasetCreationDialog().should('not.exist');
+    DatasetManager.getNoDatasetsPlaceholder().should('be.visible');
+    DatasetManager.getNoDatasetsPlaceholderViewerSubtitle().should('be.visible');
+    DatasetManager.getNoDatasetsPlaceholderUserSubtitle().should('not.exist');
+    DatasetManager.getCreateDatasetButton().should('not.exist');
+  });
+});
+
+describe('Viewer role in a non-empty dataset manager', () => {
+  before(() => {
+    stub.start();
+    stub.setOrganizations([ORGANIZATION_WITH_DEFAULT_ROLE_VIEWER]);
+    stub.setWorkspaces(WORKSPACES);
+    stub.setDatasets([...DATASETS]);
+  });
+  beforeEach(() => Login.login({ url: '/W-stbbdbrwryWithDM', workspaceId: 'W-stbbdbrwryWithDM' }));
+  after(stub.stop);
+
+  it('cannot create datasets from the dataset list button', () => {
+    DatasetManager.switchToDatasetManagerView();
+    DatasetManager.getDatasetCreationDialog().should('not.exist');
+    DatasetManager.getCreateDatasetButton().should('not.exist');
+    DatasetManager.getNoDatasetsPlaceholder().should('not.exist');
+  });
+});
