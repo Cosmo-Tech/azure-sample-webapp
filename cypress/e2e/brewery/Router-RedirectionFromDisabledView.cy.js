2c79fe1c7ac9c62713e618cd3f5bf4bd9917933b
Elena Sasova <sassova.elena@gmail.com>
2024-07-16 16:25:39 +0200

    test: add test for redirectFromDisabledView hook

---
+++ cypress/e2e/brewery/Router-RedirectionFromDisabledView.cy.js
@@ -0,0 +1,59 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import { DatasetManager, InstanceVisualization, Login } from '../../commons/actions';
+import { GENERIC_SELECTORS } from '../../commons/constants/generic/IdConstants';
+import { stub } from '../../commons/services/stubbing';
+import { routeUtils as route } from '../../commons/utils';
+import {
+  WORKSPACE_LIST_WITHOUT_DASHBOARDS,
+  WORKSPACE_WITHOUT_DASHBOARDS,
+} from '../../fixtures/stubbing/ScenarioViewDashboard/workspace';
+import { DEFAULT_SCENARIOS_LIST } from '../../fixtures/stubbing/default';
+
+const scenarioId = DEFAULT_SCENARIOS_LIST[0].id;
+
+describe('redirection from disabled view', () => {
+  before(() => {
+    stub.start();
+    stub.setWorkspaces(WORKSPACE_LIST_WITHOUT_DASHBOARDS);
+  });
+
+  beforeEach(() => {
+    Login.login({
+      url: `/${WORKSPACE_WITHOUT_DASHBOARDS.id}`,
+      workspaceId: WORKSPACE_WITHOUT_DASHBOARDS.id,
+      isPowerBiEnabled: false,
+    });
+  });
+
+  after(() => {
+    stub.stop();
+  });
+  it('can redirect from disabled Instance view to scenario view', () => {
+    route.browse({
+      url: `/${WORKSPACE_WITHOUT_DASHBOARDS.id}/instance`,
+      workspaceId: WORKSPACE_WITHOUT_DASHBOARDS.id,
+      isPowerBiEnabled: false,
+      expectedURL: `${WORKSPACE_WITHOUT_DASHBOARDS.id}/scenario/${scenarioId}`,
+    });
+    InstanceVisualization.getInstanceVisualizationViewTab().should('not.exist');
+  });
+  it('can redirect from disabled Dataset manager view to scenario view', () => {
+    route.browse({
+      url: `/${WORKSPACE_WITHOUT_DASHBOARDS.id}/datasetmanager`,
+      workspaceId: WORKSPACE_WITHOUT_DASHBOARDS.id,
+      isPowerBiEnabled: false,
+      expectedURL: `${WORKSPACE_WITHOUT_DASHBOARDS.id}/scenario/${scenarioId}`,
+    });
+    DatasetManager.getDatasetManagerTab().should('not.exist');
+  });
+  it('can redirect from dashboards to Scenario view when results display is disabled', () => {
+    route.browse({
+      url: `${WORKSPACE_WITHOUT_DASHBOARDS.id}/dashboards`,
+      workspaceId: WORKSPACE_WITHOUT_DASHBOARDS.id,
+      expectedURL: `${WORKSPACE_WITHOUT_DASHBOARDS.id}/scenario/${scenarioId}`,
+      isPowerBiEnabled: false,
+    });
+    cy.get(GENERIC_SELECTORS.dashboards.tabName).should('not.exist');
+  });
+});
