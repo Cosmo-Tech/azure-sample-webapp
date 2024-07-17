6b6d166118ff2372e38c509c0abbf727b6e96721
Nicolas Borde <nicolas.borde@cosmotech.com>
2024-07-17 15:47:05 +0200

    feat: [PROD-13559] add translation custom files features

--- src/services/config/i18next.js
+++ src/services/config/i18next.js
@@ -1,12 +1,15 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 import { initReactI18next } from 'react-i18next';
+import merge from 'deepmerge';
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
-import { FALLBACK_LANGUAGE, LANGUAGES } from './Languages';
+import { LANGUAGES, FALLBACK_LANGUAGE } from './Languages';
 
-const I18N_NAMESPACE = 'translation';
+const I18N_NAMESPACE = 'merged';
+const I18N_TRANSLATION_NAMESPACE = 'translation';
+const I18N_CUSTOM_NAMESPACE = 'custom';
 
 const langDetectorOptions = {
   order: ['localStorage', 'navigator'],
@@ -23,12 +26,28 @@ i18next
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: FALLBACK_LANGUAGE,
+    ns: [I18N_TRANSLATION_NAMESPACE, I18N_CUSTOM_NAMESPACE],
+    preload: Object.keys(LANGUAGES),
     supportedLngs: Object.keys(LANGUAGES),
+    fallbackLng: FALLBACK_LANGUAGE,
     detection: langDetectorOptions,
     backend: {
       loadPath: `${process.env?.PUBLIC_URL ?? ''}/locales/{{lng}}/{{ns}}.json`,
     },
   });
+
+i18next.on('initialized', function () {
+  i18next.setDefaultNamespace(I18N_NAMESPACE);
+});
+
+i18next.on('languageChanged', function (lng) {
+  const translationBundle = i18next.getResourceBundle(lng, I18N_TRANSLATION_NAMESPACE);
+  const customBundle = i18next.hasResourceBundle(lng, I18N_CUSTOM_NAMESPACE)
+    ? i18next.getResourceBundle(lng, I18N_CUSTOM_NAMESPACE)
+    : {};
+  const merged = merge(translationBundle, customBundle);
+
+  i18next.addResourceBundle(lng, I18N_NAMESPACE, merged, true, true);
+});
 
 export { i18next, I18N_NAMESPACE };
