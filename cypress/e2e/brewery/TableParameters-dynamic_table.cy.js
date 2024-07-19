ecea492bea0443e41c9ff12efb3741b981dc8ee7
Elena Sasova <sassova.elena@gmail.com>
2024-07-19 11:37:51 +0200

    test: [PROD-13674] cypress tests for saving table after a revert and on launch

--- cypress/e2e/brewery/TableParameters-dynamic_table.cy.js
+++ cypress/e2e/brewery/TableParameters-dynamic_table.cy.js
@@ -43,6 +43,13 @@ const twingraphQueryResponse = [
   },
 ];
 
+const runOptions = {
+  runDuration: 1000,
+  dataIngestionDuration: 1000,
+  finalStatus: 'Successful',
+  expectedPollsCount: 2,
+};
+
 describe('can use dataset data in editable table', () => {
   before(() => {
     stub.start();
@@ -80,6 +87,7 @@ describe('can use dataset data in editable table', () => {
     apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
     Scenarios.getScenarioViewTab(60).should('be.visible');
     ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.switchToEventsTab();
     BreweryParameters.getEventsRevertTableButton().should('not.exist');
     BreweryParameters.switchToCustomersTab();
     BreweryParameters.getCustomersTableGrid().should('exist');
@@ -88,22 +96,107 @@ describe('can use dataset data in editable table', () => {
     BreweryParameters.revertCustomersTable(twingraphQueryResponse);
     ScenarioParameters.getSaveButton().should('not.exist');
   });
-  it('can fetch data from dataset and save table as dataset part, then revert data', () => {
+  it('can fetch data from dataset, edit it, save and revert without reloading', () => {
     apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
     Scenarios.getScenarioViewTab(60).should('be.visible');
     ScenarioParameters.expandParametersAccordion();
     BreweryParameters.switchToCustomersTab();
     BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.getCustomersRevertTableButton().should('exist');
     BreweryParameters.editCustomersTableStringCell('name', 0, 'Client').should('have.text', 'Client');
     ScenarioParameters.save({ datasetsEvents: [{ id: 'd-stbddtspr1', securityChanges: { default: 'admin' } }] });
-    BreweryParameters.switchToEventsTab();
+    BreweryParameters.revertCustomersTable(twingraphQueryResponse);
+    ScenarioParameters.getSaveButton().should('exist');
+  });
+  it('can fetch data from dataset and save table as dataset part, then revert data after reloading scenario', () => {
+    apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
+    Scenarios.getScenarioViewTab(60).should('be.visible');
     ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[1].name, DEFAULT_SCENARIOS_LIST[1].id);
-    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[0].name, DEFAULT_SCENARIOS_LIST[0].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.editCustomersTableStringCell('name', 0, 'Client').should('have.text', 'Client');
+    ScenarioParameters.save({ datasetsEvents: [{ id: 'd-stbddtspr2', securityChanges: { default: 'admin' } }] });
+    BreweryParameters.switchToEventsTab();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[2].name, DEFAULT_SCENARIOS_LIST[2].id);
     apiUtils.interceptDownloadWorkspaceFile();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[1].name, DEFAULT_SCENARIOS_LIST[1].id);
     BreweryParameters.switchToCustomersTab();
     BreweryParameters.getCustomersTableGrid().should('exist');
     BreweryParameters.getCustomersTableCell('name', 0).should('have.text', 'Client');
     BreweryParameters.revertCustomersTable(twingraphQueryResponse);
     ScenarioParameters.getSaveButton().should('exist');
+    ScenarioParameters.save({ datasetsEvents: [{ id: 'd-stbddtspr3', securityChanges: { default: 'admin' } }] });
+    BreweryParameters.switchToEventsTab();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[2].name, DEFAULT_SCENARIOS_LIST[2].id);
+    apiUtils.interceptDownloadWorkspaceFile();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[1].name, DEFAULT_SCENARIOS_LIST[1].id);
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.getCustomersTableCell('name', 0).should('have.text', 'Customer3');
+  });
+  it('can fetch data from dataset and save table on first save', () => {
+    apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[2].name, DEFAULT_SCENARIOS_LIST[2].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.switchToBasicTypesTab();
+    BreweryParameters.getAdditionalSeatsInput().click().clear().type('10');
+    ScenarioParameters.save({ datasetsEvents: [{ id: 'd-stbddtspr4', securityChanges: { default: 'admin' } }] });
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[3].name, DEFAULT_SCENARIOS_LIST[3].id);
+    apiUtils.interceptDownloadWorkspaceFile();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[2].name, DEFAULT_SCENARIOS_LIST[2].id);
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.getCustomersTableCell('name', 0).should('have.text', 'Customer3');
+  });
+  it('can fetch data from dataset and save table on first launch', () => {
+    apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[3].name, DEFAULT_SCENARIOS_LIST[3].id);
+    ScenarioParameters.expandParametersAccordion();
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.switchToEventsTab();
+    ScenarioParameters.launch({
+      saveAndLaunch: true,
+      datasetsEvents: [{ id: 'd-stbddtspr5', securityChanges: { default: 'admin' } }],
+      runOptions,
+    });
+    ScenarioParameters.waitForScenarioRunEnd();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[4].name, DEFAULT_SCENARIOS_LIST[4].id);
+    apiUtils.interceptDownloadWorkspaceFile();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[3].name, DEFAULT_SCENARIOS_LIST[3].id);
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.getCustomersTableCell('name', 0).should('have.text', 'Customer3');
+  });
+  it('can fetch data from dataset and save table on second launch', () => {
+    Scenarios.getScenarioViewTab(60).should('be.visible');
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[4].name, DEFAULT_SCENARIOS_LIST[4].id);
+    ScenarioParameters.launch({
+      saveAndLaunch: true,
+      runOptions,
+    });
+    ScenarioParameters.waitForScenarioRunEnd();
+    ScenarioParameters.expandParametersAccordion();
+    apiUtils.interceptPostDatasetTwingraphQuery(twingraphQueryResponse, false);
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    ScenarioParameters.launch({
+      saveAndLaunch: true,
+      datasetsEvents: [{ id: 'd-stbddtspr6', securityChanges: { default: 'admin' } }],
+      runOptions,
+    });
+    ScenarioParameters.waitForScenarioRunEnd();
+    BreweryParameters.switchToEventsTab();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[0].name, DEFAULT_SCENARIOS_LIST[0].id);
+    apiUtils.interceptDownloadWorkspaceFile();
+    ScenarioSelector.selectScenario(DEFAULT_SCENARIOS_LIST[4].name, DEFAULT_SCENARIOS_LIST[4].id);
+    BreweryParameters.switchToCustomersTab();
+    BreweryParameters.getCustomersTableGrid().should('exist');
+    BreweryParameters.getCustomersTableCell('name', 0).should('have.text', 'Customer3');
   });
 });
