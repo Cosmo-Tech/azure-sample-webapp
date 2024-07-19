53af4697811bb9dc4d5a98351e3afeef41ad0e41
Elena Sasova <sassova.elena@gmail.com>
2024-07-19 16:24:20 +0200

    fix: fix solution reset before landing on workspace view

--- src/components/AppBar/components/hooks/WorkspaceInfoHook.js
+++ src/components/AppBar/components/hooks/WorkspaceInfoHook.js
@@ -1,11 +1,9 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import { useResetCurrentSolution } from '../../../../state/hooks/SolutionHooks';
 import { useWorkspaceData, useWorkspacesList } from '../../../../state/hooks/WorkspaceHooks';
 
 export const useWorkspaceInfo = () => {
   const currentWorkspaceData = useWorkspaceData();
   const workspacesList = useWorkspacesList();
-  const resetCurrentSolution = useResetCurrentSolution();
-  return { currentWorkspaceData, workspacesList, resetCurrentSolution };
+  return { currentWorkspaceData, workspacesList };
 };
