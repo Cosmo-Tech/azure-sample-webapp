ed92097d7655bb9cf979204969b89e027333b37a
Elena Sasova <sassova.elena@gmail.com>
2024-09-11 15:54:11 +0200

    fix: [PROD-12316] add backdrop in scenario manager
    Co-authored-by: Nicolas Borde <nicolas.borde@cosmotech.com>
    Co-authored-by: Tristan Huet <tristan.huet@cosmotech.com>

---
+++ src/components/LoadingBackdrop/LoadingBackdrop.js
@@ -0,0 +1,31 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { Backdrop, CircularProgress, Stack, Typography } from '@mui/material';
+import { STATUSES } from '../../state/commons/Constants';
+
+const LoadingBackdrop = ({ status }) => {
+  const { t } = useTranslation();
+  const showBackdrop = status === STATUSES.LOADING || status === STATUSES.SAVING;
+
+  return (
+    <Backdrop data-cy="scenario-backdrop" open={showBackdrop} style={{ zIndex: '10000' }}>
+      <Stack spacing={2} alignItems="center">
+        <CircularProgress data-cy="scenario-loading-spinner" color="inherit" />
+        {status === STATUSES.SAVING && (
+          <Typography data-cy="scenario-backdrop-saving-text" variant="h4">
+            {t('genericcomponent.text.scenario.saving', 'Saving your data')}
+          </Typography>
+        )}
+      </Stack>
+    </Backdrop>
+  );
+};
+
+LoadingBackdrop.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+export default LoadingBackdrop;
