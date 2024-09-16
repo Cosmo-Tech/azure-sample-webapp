d47a62b772560351d8ed131bbe4a3a011f36406c
Tristan Huet <tristan.huet@cosmotech.com>
2024-09-16 17:39:54 +0200

    fix: fix parsing of runner logs (API v4 breaking change)

--- src/services/runnerRun/RunnerRunService.js
+++ src/services/runnerRun/RunnerRunService.js
@@ -9,19 +9,8 @@ import { Api } from '../config/Api';
 async function downloadLogsFile(organizationId, workspaceId, runnerId, lastRunId) {
   try {
     const fileName = `${runnerId}_${lastRunId}_logs.txt`;
-    const { data } = await Api.RunnerRuns.getRunLogs(organizationId, workspaceId, runnerId, lastRunId, {
-      responseType: 'text',
-    });
-
-    const logsParse = JSON.parse(data);
-    const jsonLogs = JSON.parse(logsParse.containers.csmorchestrator.logs);
-    const mainContainerLogs = (jsonLogs.data.result ?? []).filter((result) => result.stream.container === 'main');
-    const parsedLogs = mainContainerLogs
-      .map((result) => result.values)
-      .flat()
-      .sort((a, b) => a[0] - b[0])
-      .map((row) => row[1])
-      .join('\n');
+    const { data } = await Api.RunnerRuns.getRunLogs(organizationId, workspaceId, runnerId, lastRunId);
+    const parsedLogs = data?.logs?.map((logItem) => logItem.line)?.join('\n') ?? '';
 
     FileBlobUtils.downloadFileFromData(parsedLogs, fileName);
   } catch (error) {
