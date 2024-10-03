5006ff682739cb87a2ba0149d9a213e9615bf05d
Tristan Huet <tristan.huet@cosmotech.com>
2024-10-03 15:07:47 +0200

    fix: [FIXUP] fix missing public url in get-embed-info query url

--- src/services/powerbi/PowerBIService.js
+++ src/services/powerbi/PowerBIService.js
@@ -50,7 +50,7 @@ const getPowerBIDataWithServiceAccount = async (powerBIWorkspaceId, reportsIds) 
   const headers = await getAuthenticationHeaders(false); // Do not accept API key as token for Azure Functions
   return clientApi
     .post(
-      GET_EMBED_INFO_URL,
+      `${process.env?.PUBLIC_URL}${GET_EMBED_INFO_URL}`,
       { reports: reportsIds, workspaceId: powerBIWorkspaceId },
       { headers: { 'csm-authorization': headers.Authorization } }
     )
