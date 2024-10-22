68137031f5f9adcc1931b357e464c1a5727625a6
Elena Sasova <sassova.elena@gmail.com>
2024-10-22 14:09:49 +0200

    refactor: add path to http-proxy-middleware v3

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:7071',
+      target: 'http://127.0.0.1:7071/api',
       secure: false,
       changeOrigin: true,
     })
