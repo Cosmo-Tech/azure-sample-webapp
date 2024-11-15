fc1d1e23150092e7e996d6d323d795e946f8f3c9
Elena Sasova <sassova.elena@gmail.com>
2024-11-15 11:17:19 +0100

    refactor: use eslint flat config

--- src/theme/overrides/index.js
+++ src/theme/overrides/index.js
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 import MuiAppBar from './MuiAppBar';
 
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
   MuiAppBar,
 };
