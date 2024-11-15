e00f43e043330dd21f229ab82e8a0e7865b0d4a3
Tristan Huet <tristan.huet@cosmotech.com>
2024-11-15 15:03:52 +0100

    fix: minor changes in patching script & doc improvements

--- scripts/patch_webapp_server/patch_and_start_server.sh
+++ scripts/patch_webapp_server/patch_and_start_server.sh
@@ -5,10 +5,10 @@ WORKING_DIR="/tmp/webapp"
 BUILD_DIR="$WORKING_DIR/build"
 
 echo "Copying webapp build folder to $BUILD_DIR..."
-cp -r build $BUILD_DIR/
+cp -r build $WORKING_DIR/
 
 
-echo "Starting script patch_and_start_server.sh..."
+echo "Running script patch_webapp_server/main.py..."
 pushd patch_webapp_server
   python3 main.py --csp azure,custom,default -i ../patch_config/ -o $BUILD_DIR
 popd
