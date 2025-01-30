31b4195dc05caf648bf2b102b2fef0d53e704122
Tristan Huet <tristan.huet@cosmotech.com>
2025-01-30 10:01:54 +0100

    fix: [FIXUP] fix powerBI spinner behavior

--- src/state/powerBi/reducers.js
+++ src/state/powerBi/reducers.js
@@ -19,11 +19,13 @@ const powerBiSlice = createSlice({
   reducers: {
     setEmbedInfo: (state, action) => {
       const { data, status, error } = action.payload;
-      state.data.accessToken = data?.accessToken; // TODO: remove from redux
-      state.data.reportsInfo = data?.reportsInfo; // TODO: rename ?
-      state.data.expiry = data?.expiry; // TODO: remove from redux
-      state.error = error;
-      state.status = status;
+      if (data) {
+        state.data.accessToken = data?.accessToken; // TODO: remove from redux
+        state.data.reportsInfo = data?.reportsInfo; // TODO: rename ?
+        state.data.expiry = data?.expiry; // TODO: remove from redux
+      }
+      if (error) state.error = error;
+      if (status) state.status = status;
     },
     setReportConfig: (state, action) => {
       state.data.reportsConfig = action.payload;
@@ -34,5 +36,6 @@ const powerBiSlice = createSlice({
     },
   },
 });
+
 export const { setEmbedInfo, setReportConfig, clearEmbedInfo } = powerBiSlice.actions;
 export default powerBiSlice.reducer;
