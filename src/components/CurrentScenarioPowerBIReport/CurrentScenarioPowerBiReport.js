2587f712abbcd7570d114d97ad3f9a8cecd451be
Tristan Huet <tristan.huet@cosmotech.com>
2025-01-30 10:36:41 +0100

    fix: [FIXUP] style PBI files

--- src/components/CurrentScenarioPowerBIReport/CurrentScenarioPowerBiReport.js
+++ src/components/CurrentScenarioPowerBIReport/CurrentScenarioPowerBiReport.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
 
 const CurrentScenarioPowerBiReport = ({
   alwaysShowReports,
-  isParentLoading=false,
+  isParentLoading = false,
   reportConfiguration,
   iframeRatio,
   index,
