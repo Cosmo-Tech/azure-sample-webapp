212567b6f042320382ed8fd1f7750632b1fa4054
Tristan Huet <tristan.huet@cosmotech.com>
2025-02-10 17:16:45 +0100

    fix: [PROD-14144] graph indicators not displayed if categories aren't defined in dataset manager
    Changes copied from commit ed7ce526 on branch release/7.0
    
    Co-authored-by: Elena Sasova <sassova.elena@gmail.com>

--- src/state/sagas/datasets/QueryDatasetTwingraph/QueryDatasetTwingraph.js
+++ src/state/sagas/datasets/QueryDatasetTwingraph/QueryDatasetTwingraph.js
@@ -50,8 +50,8 @@ function* startAllDatasetTwingraphQueries(action) {
   yield all(
     queries.map((query, index) => {
       if (
-        !categories.some((category) => (category?.kpis ?? []).some((kpi) => kpi?.queryId === query.id)) &&
-        !graphIndicators.some((indicator) => indicator?.queryId === query.id)
+        !categories?.some((category) => (category?.kpis ?? [])?.some((kpi) => kpi?.queryId === query.id)) &&
+        !graphIndicators?.some((indicator) => indicator?.queryId === query.id)
       )
         return null;
       if (index !== 0) delay(TWINGRAPH_QUERIES_DELAY);
