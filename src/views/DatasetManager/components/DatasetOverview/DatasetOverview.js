69184b3265c6c3f5fec3da2e30575c3df3d399e7
Tristan Huet <tristan.huet@cosmotech.com>
2025-02-11 14:45:49 +0100

    fix: [WIP]

--- src/views/DatasetManager/components/DatasetOverview/DatasetOverview.js
+++ src/views/DatasetManager/components/DatasetOverview/DatasetOverview.js
@@ -1,13 +1,17 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 import React, { useMemo } from 'react';
-import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Edit as EditIcon } from '@mui/icons-material';
+import { Card, CardContent, CardHeader, Grid, IconButton, Stack, Typography } from '@mui/material';
+import { FadingTooltip } from '@cosmotech/ui';
 import { INGESTION_STATUS } from '../../../../services/config/ApiConstants';
 import { useDatasetOverview } from './DatasetOverviewHook';
 import { CategoryAccordion, DatasetOverviewPlaceholder, GraphIndicator } from './components';
 import DatasetActions from './components/DatasetActions/DatasetActions';
 
 export const DatasetOverview = () => {
+  const { t } = useTranslation();
   const { categories, graphIndicators, queriesResults, datasetIngestionStatus, dataset, datasetName } =
     useDatasetOverview();
 
@@ -24,6 +28,23 @@ export const DatasetOverview = () => {
     });
   }, [graphIndicators, queriesResults]);
 
+  const datasetCardTitle = useMemo(() => {
+    const canRenameScenario = true; // FIXME
+    const startEdition = () => {}; // FIXME
+    return (
+      <Stack direction="row">
+        <Typography variant="h5">{datasetName}</Typography>
+        {canRenameScenario && (
+          <FadingTooltip title={t('commoncomponents.datasetmanager.overview.actions.renameDatasetTooltip', 'Rename')}>
+            <IconButton data-cy="rename-dataset-button" aria-label="rename dataset" size="small" onClick={startEdition}>
+              <EditIcon fontSize="18px" color="appbar.contrastTextSoft" />
+            </IconButton>
+          </FadingTooltip>
+        )}
+      </Stack>
+    );
+  }, [datasetName, t]);
+
   return (
     <Card
       elevation={0}
@@ -32,7 +53,8 @@ export const DatasetOverview = () => {
     >
       <CardHeader
         data-cy="dataset-name"
-        title={datasetName}
+        disableTypography={true}
+        title={datasetCardTitle}
         action={<DatasetActions dataset={dataset}></DatasetActions>}
       ></CardHeader>
       <CardContent>
