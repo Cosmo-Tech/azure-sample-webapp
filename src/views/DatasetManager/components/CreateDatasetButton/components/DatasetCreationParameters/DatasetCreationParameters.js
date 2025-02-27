3a472bc68c3f9551dab7d65bcd44b287e988e3f7
Elena Sasova <sassova.elena@gmail.com>
2025-02-27 14:58:48 +0100

    test: [PROD-14182] add cypress tests to check ETL parameters edition

--- src/views/DatasetManager/components/CreateDatasetButton/components/DatasetCreationParameters/DatasetCreationParameters.js
+++ src/views/DatasetManager/components/CreateDatasetButton/components/DatasetCreationParameters/DatasetCreationParameters.js
@@ -14,7 +14,7 @@ import { useDatasetCreationParameters } from './DatasetCreationParametersHook';
 
 const clone = rfdc();
 
-export const DatasetCreationParameters = ({ dataSourceRunTemplates, parentDataset, selectedRunner }) => {
+export const DatasetCreationParameters = ({ dataSourceRunTemplates, parentDataset, selectedRunner = {} }) => {
   const { t } = useTranslation();
   const { resetField } = useFormContext();
   const { datasourceParameterHelpers, getDataSourceTypeEnumValues, getUploadFileLabels, getDefaultFileTypeFilter } =
@@ -232,7 +232,4 @@ DatasetCreationParameters.propTypes = {
   dataSourceRunTemplates: PropTypes.object.isRequired,
   parentDataset: PropTypes.object,
   selectedRunner: PropTypes.object,
-};
-DatasetCreationParameters.defaultProps = {
-  selectedRunner: {},
 };
