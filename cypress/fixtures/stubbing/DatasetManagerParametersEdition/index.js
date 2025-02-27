c612ba13d00628f77b5b2148a008f7e2c0e114c1
Elena Sasova <sassova.elena@gmail.com>
2025-02-27 14:58:48 +0100

    test: [PROD-14182] add cypress tests to check ETL parameters edition

---
+++ cypress/fixtures/stubbing/DatasetManagerParametersEdition/index.js
@@ -0,0 +1,7 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+export { DATASETS } from './datasets';
+export { RUNNERS } from './runners';
+export { WORKSPACE } from './workspaces';
+export { SOLUTION } from './solutions';
