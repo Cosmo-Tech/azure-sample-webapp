d8b67961bc1608e82b9e49c9c4a21b15b436c1f5
Tristan Huet <tristan.huet@cosmotech.com>
2025-05-14 17:52:27 +0200

    fix: [WIP] add production resources & operations

--- src/views/Simulation/components/SceneContainer/pixiUtils.js
+++ src/views/Simulation/components/SceneContainer/pixiUtils.js
@@ -41,10 +41,10 @@ export const createApp = (appRef, canvasRef, containerRef, instance, theme, togg
   };
 };
 
-const makeStockTexture = (appRef) => {
+const createTexture = (appRef, options) => {
   const graphics = new PIXI.Graphics();
-  graphics.beginFill('#003d00');
-  graphics.lineStyle(0.75, '#ffffff');
+  graphics.beginFill(options.fillColor);
+  graphics.lineStyle(0.75, options.lineColor);
   graphics.moveTo(1, 1);
   graphics.lineTo(8 - 1, 1);
   graphics.lineTo(8 - 1, 8 - 1);
@@ -55,10 +55,11 @@ const makeStockTexture = (appRef) => {
 };
 
 export const renderElements = (appRef, containerRef, instance, toggleInspectorDrawer) => {
-  const stocks = [];
-  const stockTexture = makeStockTexture(appRef);
   const width = containerRef.current.clientWidth;
   const height = containerRef.current.clientHeight;
+
+  const stocks = [];
+  const stockTexture = createTexture(appRef, { fillColor: '#003d00', lineColor: '#ffffff' });
   for (const [index, stock] of instance.stocks.entries()) {
     const sprite = new PIXI.Sprite(stockTexture);
     sprite.x = Math.random() * width;
@@ -67,9 +68,47 @@ export const renderElements = (appRef, containerRef, instance, toggleInspectorDr
     sprite.eventMode = 'static';
     sprite.buttonMode = true;
     sprite.scale.set((stock.initialStock / 500.0) * 0.5 + 0.8);
-    // sprite.scale.set(Math.random() * 0.5 + 0.8);
+
     sprite.on('click', toggleInspectorDrawer);
     stocks.push(sprite);
     appRef.current.stage.addChild(sprite);
   }
+
+  const productionResources = [];
+  const productionResourceTexture = createTexture(appRef, { fillColor: '#3d0000', lineColor: '#ff0000' });
+  for (const [index] of instance.production_resources.entries()) {
+    const sprite = new PIXI.Sprite(productionResourceTexture);
+    sprite.x = Math.random() * width;
+    sprite.y = Math.random() * height;
+    sprite.index = index;
+    sprite.eventMode = 'static';
+    sprite.buttonMode = true;
+    sprite.scale.set(Math.random() * 0.7 + 1.3);
+
+    sprite.anchor.x = 0.5;
+    sprite.anchor.y = 0.5;
+    sprite.rotation = Math.PI / 4;
+
+    sprite.on('click', toggleInspectorDrawer);
+    productionResources.push(sprite);
+    appRef.current.stage.addChild(sprite);
+  }
+
+  const productionOperations = [];
+  const productionOperationTexture = createTexture(appRef, { fillColor: '#00003d', lineColor: '#4444ff' });
+  for (const [index] of instance.production_operations.entries()) {
+    const sprite = new PIXI.Sprite(productionOperationTexture);
+    sprite.x = Math.random() * width;
+    sprite.y = Math.random() * height;
+    sprite.index = index;
+    sprite.eventMode = 'static';
+    sprite.buttonMode = true;
+    sprite.scale.set(Math.random() * 0.7 + 1.3);
+
+    sprite.on('click', toggleInspectorDrawer);
+    productionOperations.push(sprite);
+    appRef.current.stage.addChild(sprite);
+  }
+
+  return { stocks, productionResources };
 };
