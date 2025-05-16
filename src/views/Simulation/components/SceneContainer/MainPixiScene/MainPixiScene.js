2aae9b07d0983f97c14f8cdb3557377fcceb8a97
Nicolas Borde <nicolas.borde@cosmotech.com>
2025-05-16 15:47:35 +0200

    feat: [PROD-1443] first draf

---
+++ src/views/Simulation/components/SceneContainer/MainPixiScene/MainPixiScene.js
@@ -0,0 +1,60 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import React, { useEffect, useRef } from 'react';
+import { Application, Assets, Sprite, Graphics } from 'pixi.js';
+
+const mainLoop = async () => {
+  // Create a PixiJS application.
+  const app = new Application();
+
+  // Intialize the application.
+  await app.init({ background: '#1099bb', resizeTo: window });
+
+  // Then adding the application's canvas to the DOM body.
+  document.body.appendChild(app.canvas);
+
+  // Load the bunny texture.
+  const texture = await Assets.load('../assets/frog.png');
+
+  // Create a new Sprite from an image path.
+  const bunny = new Sprite(texture);
+
+  // Add to stage.
+  app.stage.addChild(bunny);
+
+  // Center the sprite's anchor point.
+  bunny.anchor.set(0.5);
+
+  // Move the sprite to the center of the screen.
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
+
+  // Add an animation loop callback to the application's ticker.
+  app.ticker.add((time) => {
+    /**
+     * Just for fun, let's rotate mr rabbit a little.
+     * Time is a Ticker object which holds time related data.
+     * Here we use deltaTime, which is the time elapsed between the frame callbacks
+     * to create frame-independent transformation. Keeping the speed consistent.
+     */
+    bunny.rotation += 0.1 * time.deltaTime;
+  });
+};
+
+const MainPixiScene = () => {
+  const pixiContainer = useRef(null);
+  const scoreRef = useRef(0);
+
+  useEffect(() => {
+    mainLoop();
+  }, []);
+
+  return (
+    <div>
+      <div ref={pixiContainer} />
+      <div>Score: {scoreRef.current}</div>
+    </div>
+  );
+};
+
+export default MainPixiScene;
