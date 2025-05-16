8b8ba5c8fccf432866318b329ee36a69961cf599
Tristan Huet <tristan.huet@cosmotech.com>
2025-05-16 11:54:03 +0200

    fix: [WIP]

--- src/views/Simulation/Simulation.js
+++ src/views/Simulation/Simulation.js
@@ -1,29 +1,35 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Box, Grid } from '@mui/material';
 import { InspectorDrawer, Kpis, SceneContainer, TopBar } from './components';
 
 const Simulation = () => {
+  const inspectorDrawerParentContainerRef = useRef(null);
   const [isInspectorDrawerOpen, setIsInspectorDrawerOpen] = useState(false);
   // const openInspectorDrawer = () => setIsInspectorDrawerOpen(true);
   const closeInspectorDrawer = () => setIsInspectorDrawerOpen(false);
   const toggleInspectorDrawer = () => setIsInspectorDrawerOpen((previousValue) => !previousValue);
 
   return (
-    <Box sx={{ flexGrow: 1, height: '100%', width: '100%' }}>
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, height: '100%', width: '100%', overflowX: 'clip' }}
+    >
+      <div style={{ width: isInspectorDrawerOpen ? '100%' : '0px' }}></div>
+      <TopBar />
       <Grid container wrap="nowrap" direction="column" gap={1} style={{ height: '100%' }}>
         <Grid item xs>
-          <TopBar />
-        </Grid>
-        <Grid item xs>
-          <div>
-            <InspectorDrawer open={isInspectorDrawerOpen} onClose={closeInspectorDrawer} />
-            <Kpis />
-          </div>
+          <Kpis />
         </Grid>
         <Grid item xs={12}>
-          <SceneContainer toggleInspectorDrawer={toggleInspectorDrawer} />
+          <div id="drawer-container" style={{ position: 'relative', height: '100%' }}>
+            <SceneContainer toggleInspectorDrawer={toggleInspectorDrawer} />
+            <InspectorDrawer
+              open={isInspectorDrawerOpen}
+              onClose={closeInspectorDrawer}
+              containerRef={inspectorDrawerParentContainerRef}
+            />
+          </div>
         </Grid>
       </Grid>
     </Box>
--- src/views/Simulation/components/InspectorDrawer/InspectorDrawer.js
+++ src/views/Simulation/components/InspectorDrawer/InspectorDrawer.js
@@ -1,12 +1,27 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Drawer } from '@mui/material';
 
-const InspectorDrawer = ({ open = false, onClose, data }) => {
+const InspectorDrawer = ({ data, onClose, open = true }) => {
+  // Work-around to prevent animation glitch on first time the drawer is opened
+  const [, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
   return (
-    <Drawer anchor="right" open={open} onClose={onClose}>
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      PaperProps={{ style: { position: 'absolute' } }}
+      BackdropProps={{ style: { position: 'absolute' } }}
+      ModalProps={{
+        container: document.getElementById('drawer-container'),
+        style: { position: 'absolute' },
+        keepMounted: true,
+      }}
+    >
       <div style={{ height: '100%', backgroundColor: '#232323', textAlign: 'center', padding: '8px' }}>
         inspector drawer placeholder
       </div>
