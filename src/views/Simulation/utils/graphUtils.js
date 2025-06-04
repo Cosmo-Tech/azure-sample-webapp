67fcfd98963e2c91826e9a7dd75d4e0256dd08cf
Tristan Huet <tristan.huet@cosmotech.com>
2025-06-04 11:20:39 +0200

    fix: [PROD-14406] aggregate production resources with resource operations in graph

--- src/views/Simulation/utils/graphUtils.js
+++ src/views/Simulation/utils/graphUtils.js
@@ -16,28 +16,55 @@ const forgeElementData = (element, keysToHide = []) => {
   return data;
 };
 
-const getGraphLinks = (instance, nodes) => {
-  const links = [];
-  Object.entries(instance).forEach(([type, elements]) => {
-    if (['transports', 'input', 'output'].includes(type) === false) return;
-    elements.forEach((link) => {
-      const sourceId = link.src;
-      const targetId = link.dest;
-      const source = nodes.find((node) => node.id === sourceId);
-      const target = nodes.find((node) => node.id === targetId);
-      if (source == null) {
-        console.warn(`Cannot find link source "${sourceId}"`);
-        return;
-      }
-      if (target == null) {
-        console.warn(`Cannot find link target "${targetId}"`);
-        return;
-      }
-      links.push({ type, source, target, data: forgeElementData(link) });
-    });
-  });
+const forgeLink = (nodes, link, type, sourceId, targetId) => {
+  const source = nodes.find((node) => node.id === sourceId);
+  const target = nodes.find((node) => node.id === targetId);
+  if (source == null) return console.warn(`Transports: cannot find link source "${sourceId}"`);
+  if (target == null) return console.warn(`Transports: cannot find link target "${targetId}"`);
+  return { type, source, target, data: forgeElementData(link) };
+};
 
+const getTransportLinks = (instance, nodes) => {
+  const links = [];
+  instance.transports.forEach((linkMetadata) => {
+    const link = forgeLink(nodes, linkMetadata, 'transports', linkMetadata.src, linkMetadata.dest);
+    if (link) links.push(link);
+  });
   return links;
+};
+
+const getInputLinks = (instance, nodes) => {
+  const links = [];
+  instance.input.forEach((input) => {
+    const target = instance.production_operations.find((el) => el.id === input.dest);
+    if (!target) return console.warn(`Input: cannot find target Operation with id "${input.dest}"`);
+
+    const targetResourceId = instance.compounds.find((rel) => rel.child === target.id)?.parent;
+    if (!targetResourceId) return console.warn(`Input: cannot find compound link with child id "${target.id}"`);
+
+    const link = forgeLink(nodes, input, 'input', input.src, targetResourceId);
+    if (link) links.push(link);
+  });
+  return links;
+};
+
+const getOutputLinks = (instance, nodes) => {
+  const links = [];
+  instance.output.forEach((output) => {
+    const source = instance.production_operations.find((el) => el.id === output.src);
+    if (!source) return console.warn(`Output: cannot find source Operation with id "${output.src}"`);
+
+    const sourceResourceId = instance.compounds.find((rel) => rel.child === source.id)?.parent;
+    if (!sourceResourceId) return console.warn(`Output: cannot find compound link with child id "${source.id}"`);
+
+    const link = forgeLink(nodes, output, 'output', sourceResourceId, output.dest);
+    if (link) links.push(link);
+  });
+  return links;
+};
+
+const getGraphLinks = (instance, nodes) => {
+  return [...getTransportLinks(instance, nodes), ...getInputLinks(instance, nodes), ...getOutputLinks(instance, nodes)];
 };
 
 export const getGraphFromInstance = (instance) => {
@@ -51,13 +78,21 @@ export const getGraphFromInstance = (instance) => {
     };
   };
 
-  const stocks = instance.stocks.map((stock) => createNode(stock, 'stock'));
-  const productionResources = instance.production_resources.map((stock) => createNode(stock, 'productionResource'));
-  const productionOperations = instance.production_operations.map((stock) => createNode(stock, 'productionOperation'));
+  const createSubElement = (subElement, type) => {
+    return {
+      id: subElement.id,
+      type,
+      data: forgeElementData(subElement, ['id', 'latitude', 'longitude']),
+    };
+  };
 
-  const nodes = [...stocks, ...productionResources, ...productionOperations];
+  const stocks = instance.stocks.map((el) => createNode(el, 'stock'));
+  const productionResources = instance.production_resources.map((el) => createNode(el, 'productionResource'));
+  const operations = instance.production_operations.map((el) => createSubElement(el, 'productionOperation'));
+
+  const nodes = [...stocks, ...productionResources];
   const links = getGraphLinks(instance, nodes);
-  return { nodes, links };
+  return { nodes, operations, links };
 };
 
 export const resetGraphLayout = (graphRef, width, height) => {
