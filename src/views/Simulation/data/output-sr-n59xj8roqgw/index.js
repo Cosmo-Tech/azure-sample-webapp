c2f896728ed01910b532c52483ec0b03a6719d51
Tristan Huet <tristan.huet@cosmotech.com>
2025-07-10 18:55:52 +0200

    chore: add data for new scenarios

---
+++ src/views/Simulation/data/output-sr-n59xj8roqgw/index.js
@@ -0,0 +1,15 @@
+import bottlenecks from './bottlenecks.json';
+import configuration from './configuration.json';
+import instance from './graph.json';
+import kpis from './kpis.json';
+import shortages from './shortages.json';
+import stockDemands from './stock_demands.json';
+
+export const SCENARIO_DATA = {
+  bottlenecks,
+  configuration,
+  instance,
+  kpis,
+  shortages,
+  stockDemands,
+};
