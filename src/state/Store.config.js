5c7470334836aee0246a603f31a0822c64a59ce4
Tristan Huet <tristan.huet@cosmotech.com>
2025-08-12 18:51:31 +0200

    build: replace deprecated dependency package redux-devtools-extension by @redux-devtools/extension

--- src/state/Store.config.js
+++ src/state/Store.config.js
@@ -1,7 +1,7 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas/rootSaga';
