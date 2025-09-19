2787f69121acea1b1488748c11e092d13eb5034a
Tristan Huet <tristan.huet@cosmotech.com>
2025-09-19 15:27:48 +0200

    build: remove dev dependencies that are no longer necessary

--- config-overrides.js
+++ config-overrides.js
@@ -44,13 +44,7 @@ function addCspHtmlWebpackPlugin(config) {
 function addFallback(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
-    crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify'),
-    url: require.resolve('url'),
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
