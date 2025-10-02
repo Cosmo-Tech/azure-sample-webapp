22a026526dcbe3d051200a85db282df4387ab40e
Tristan Huet <tristan.huet@cosmotech.com>
2025-10-02 15:28:47 +0200

    chore: remove @redux-devtools/extension dependency

--- src/state/Store.config.js
+++ src/state/Store.config.js
@@ -1,14 +1,13 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = applyMiddleware(sagaMiddleware);
-const applicationStore = createStore(rootReducer, composeWithDevTools(middleware));
+const applicationStore = createStore(rootReducer, middleware);
 sagaMiddleware.run(rootSaga);
 
 export default applicationStore;
