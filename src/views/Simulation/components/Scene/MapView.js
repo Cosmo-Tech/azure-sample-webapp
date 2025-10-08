487d8a708abba34d7c9498631f0c9dde37f0b068
arianber7 <arian.berisha.ext@cosmotech.com>
2025-10-08 09:48:07 +0200

    feat: [PROD-14709] add chip with incident details on hovering
    feat: [PROD-14709] add chip with incident details on hovering

--- src/views/Simulation/components/Scene/MapView.js
+++ src/views/Simulation/components/Scene/MapView.js
@@ -1,13 +1,20 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Minimap } from './Minimap';
+import { IncidentChip } from './components/IncidentChip';
 
 const MapView = () => {
   const mapCanvasRef = useRef(null);
   const mapAppRef = useRef(null);
   const minimapCanvasRef = useRef(null);
   const sceneContainerRef = useRef(null);
+  // eslint-disable-next-line no-unused-vars
+  const [hoveredIncident, setHoveredIncident] = useState({
+    visible: false,
+    position: { x: 0, y: 0 },
+    data: { bottlenecks: 0, shortages: 0 },
+  });
 
   useEffect(() => {
     if (!mapCanvasRef.current) return;
@@ -24,6 +31,7 @@ const MapView = () => {
   return (
     <>
       <div ref={mapCanvasRef} style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }} />
+      <IncidentChip position={hoveredIncident.position} data={hoveredIncident.data} visible={hoveredIncident.visible} />
       <Minimap ref={minimapCanvasRef} sceneContainerRef={sceneContainerRef} />
     </>
   );
