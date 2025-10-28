61a82c1e82fb786365f114ce1d0084d861ebae22
Tristan Huet <tristan.huet@cosmotech.com>
2025-10-28 14:18:33 +0100

    fix: update regex for cosmo resource ids to allow underscores

--- api/common/check.js
+++ api/common/check.js
@@ -5,9 +5,9 @@ const isString = (value) => typeof value === 'string' || value instanceof String
 
 const isValidCosmoResourceId = (value) => {
   if (!isString(value)) return false;
-  // Vague length and regex checks on purpose to match strings with the pattern "wxyz-0a1b2c3de..."
+  // Vague length and regex checks on purpose to match strings with the pattern "wxyz-0a1b2c3de_..."
   if (value.length < 1 || value.length > 20) return false;
-  return /^[a-z]+-[a-z0-9]+$/.test(value);
+  return /^[a-z]+-[a-z0-9_]+$/.test(value);
 };
 
 module.exports = {
