26404c975aa2b2ff642090fc51f7160363643eec
Tristan Huet <tristan.huet@cosmotech.com>
2025-10-29 17:02:07 +0100

    feat: [PROD-14523] add new native data source 'File Upload'

--- src/state/datasets/sagas/CreateDataset.js
+++ src/state/datasets/sagas/CreateDataset.js
@@ -1,107 +1,93 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 import { t } from 'i18next';
-import { call, put, takeEvery, select } from 'redux-saga/effects';
+import { all, call, put, takeEvery, select } from 'redux-saga/effects';
 import { Api } from '../../../services/config/Api';
-import { DATASET_PERMISSIONS_MAPPING, INGESTION_STATUS } from '../../../services/config/ApiConstants';
+import { DATASET_PERMISSIONS_MAPPING } from '../../../services/config/ApiConstants';
 import { DatasetsUtils } from '../../../utils/DatasetsUtils';
 import { setApplicationErrorMessage } from '../../app/reducers';
-import { linkToDataset } from '../../workspaces/reducers';
 import { DATASET_ACTIONS_KEY } from '../constants';
-import { addDataset, selectDataset, updateDataset } from '../reducers';
+import { addDataset, selectDataset } from '../reducers';
 
-const getUserName = (state) => state.auth.userName;
+// TODO: use the additionalData field when it's available to store the creator name
+// const getUserName = (state) => state.auth.userName;
 const getUserEmail = (state) => state.auth.userEmail;
 const getWorkspaceId = (state) => state.workspace.current?.data?.id;
+const getOrganizationId = (state) => state.organization.current?.data?.id;
 
-export function* createDataset(action) {
-  const dataset = action.dataset;
-  const organizationId = action.organizationId;
-  const ownerName = yield select(getUserName);
-  const userEmail = yield select(getUserEmail);
+export function* postEmptyDataset(dataset) {
+  // TODO: use the additionalData field when it's available to store the creator name
+  // (createInfo.userId contains the user email and is not editable)
+  // const ownerName = yield select(getUserName);
+  const organizationId = yield select(getOrganizationId);
   const workspaceId = yield select(getWorkspaceId);
 
+  const datasetWithoutParts = { ...dataset, parts: [] };
+  const { data: emptyDataset } = yield call(
+    Api.Datasets.createDataset,
+    organizationId,
+    workspaceId,
+    datasetWithoutParts
+  );
+  return emptyDataset;
+}
+
+export function* postDatasetPart(datasetId, datasetPart, file) {
+  const organizationId = yield select(getOrganizationId);
+  const workspaceId = yield select(getWorkspaceId);
+  const { data } = yield call(
+    Api.Datasets.createDatasetPart,
+    organizationId,
+    workspaceId,
+    datasetId,
+    file,
+    datasetPart
+  );
+  return data;
+}
+
+export function* postDatasetParts(dataset, files = []) {
+  if (files.length !== dataset.parts.length)
+    throw new Error('The number of files does not match the number of expected dataset parts');
+
+  const calls = dataset.parts.map((part, index) => call(postDatasetPart, dataset.id, part, files[index]));
+  return yield all(calls);
+}
+
+export function* postDatasetAndDatasetParts(dataset, files) {
+  if (!dataset.parts) dataset.parts = [];
+  const emptyDataset = yield postEmptyDataset(dataset);
+  dataset.id = emptyDataset.id;
+  const createdParts = yield postDatasetParts(dataset, files);
+  dataset.parts = dataset.parts
+    .map((part) => createdParts.find((createdPart) => createdPart.name === part.name))
+    .filter((part) => part !== undefined);
+
+  // TODO: fetch dataset again after parts have been uploaded? (this might be necessary to have up-to-date metadata)
+
+  return dataset;
+}
+
+export function* createDataset({ dataset, files, shouldSelectDataset }) {
+  let createdDataset;
   try {
-    const datasetWithAuthor = {
-      ...dataset,
-      ownerName,
-      security: { default: 'none', accessControlList: [{ id: userEmail, role: 'admin' }] },
-    };
+    createdDataset = yield postDatasetAndDatasetParts(dataset, files);
 
-    const { data } = yield call(Api.Datasets.createDataset, organizationId, workspaceId, datasetWithAuthor);
-    DatasetsUtils.patchDatasetWithCurrentUserPermissions(data, userEmail, DATASET_PERMISSIONS_MAPPING);
+    const userEmail = yield select(getUserEmail);
+    DatasetsUtils.patchDatasetWithCurrentUserPermissions(createdDataset, userEmail, DATASET_PERMISSIONS_MAPPING);
 
-    try {
-      // FIXME: links between datasets and workspaces no longer exist, remove this feature
-      // yield call(Api.Datasets.linkWorkspace, organizationId, data.id, workspaceId);
-    } catch (error) {
-      console.error(error);
-      yield put(
-        setApplicationErrorMessage({
-          error,
-          errorMessage: t(
-            'commoncomponents.banner.datasetNotLinked',
-            'An error occurred when trying to link the created dataset ({{datasetId}}) to the workspace.' +
-              'Please try to create a new dataset and contact your administrator if the problem persists.',
-            {
-              datasetId: data.id,
-            }
-          ),
-        })
-      );
-    }
+    // TODO: manage status?
+    // ingestionStatus: dataset.sourceType !== 'None' ? INGESTION_STATUS.PENDING : INGESTION_STATUS.NONE,
+    yield put(addDataset(createdDataset));
 
-    yield put(
-      addDataset({
-        ...data,
-        ownerName,
-        ingestionStatus: dataset.sourceType !== 'None' ? INGESTION_STATUS.PENDING : INGESTION_STATUS.NONE,
-      })
-    );
-
-    yield put(linkToDataset({ datasetId: data.id, workspaceId }));
-    yield put(selectDataset({ selectedDatasetId: data.id }));
-
-    if (!['None', 'File', 'ETL'].includes(dataset.sourceType)) {
-      yield put({ type: DATASET_ACTIONS_KEY.REFRESH_DATASET, organizationId, datasetId: data.id });
-    } else if (dataset.sourceType === 'File') {
-      const response = yield call(
-        DatasetsUtils.uploadZipWithFetchApi,
-        organizationId,
-        workspaceId,
-        data.id,
-        dataset.file.file
-      );
-      if (response?.ok) {
-        yield put({ type: DATASET_ACTIONS_KEY.START_TWINGRAPH_STATUS_POLLING, datasetId: data.id, organizationId });
-      } else {
-        let error = null;
-        if (response?.status === 400) {
-          error = yield new Response(response.body).json();
-          console.error(error);
-        }
-
-        yield put(
-          setApplicationErrorMessage({
-            error,
-            errorMessage: t(
-              'commoncomponents.banner.datasetCreationFileUploadFailed',
-              'The file upload for dataset creation has failed'
-            ),
-          })
-        );
-
-        yield put(updateDataset({ datasetId: data.id, datasetData: { ingestionStatus: INGESTION_STATUS.ERROR } }));
-      }
-    } else if (dataset.sourceType === 'ETL') return data.id;
+    if (shouldSelectDataset) yield put(selectDataset({ selectedDatasetId: createdDataset.id }));
   } catch (error) {
     console.error(error);
-    yield put(
-      setApplicationErrorMessage({
-        error,
-        errorMessage: t('commoncomponents.banner.datasetNotCreated', "Dataset hasn't been created"),
-      })
+    const errorMessage = t(
+      'commoncomponents.banner.errorDuringDatasetCreation',
+      'Something went wrong during the dataset creation'
     );
+    yield put(setApplicationErrorMessage({ error, errorMessage }));
   }
 }
 
