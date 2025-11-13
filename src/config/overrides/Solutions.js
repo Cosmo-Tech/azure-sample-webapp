64330ef93539513f3b8e5798479ff8cb6c4c73d5
Tristan Huet <tristan.huet@cosmotech.com>
2025-11-13 15:28:26 +0100

    fix: [DO NOT MERGE] temporary local config override, do not merge this commit

--- src/config/overrides/Solutions.js
+++ src/config/overrides/Solutions.js
@@ -4,4 +4,18 @@
 // Use the SOLUTIONS array below to override or add information to your solutions. This can be useful for development
 // purposes, but it is recommended to leave this array empty and use the API to update your Solution instead for
 // production environments.
-export const SOLUTIONS = [];
+export const SOLUTIONS = [
+  {
+    id: 'sol-414m4e1q72em8',
+    parameters: [
+      { id: 'initial_stock_dataset', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'example_dataset_part_1', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'example_dataset_part_2', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'example_dataset_part_3', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'customers', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'events', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'initial_state', varType: '%DATASET_PART_ID_FILE%' },
+      { id: 'etl_param_bar_instance', varType: '%DATASET_PART_ID_FILE%' },
+    ],
+  },
+];
