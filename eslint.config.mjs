a7c83f4b348a8f85eb254c6ec77870238ca4f994
arianber7 <arian.berisha.ext@cosmotech.com>
2025-11-26 16:22:04 +0100

    Update yarn cache after adding Storyshots
    feat: eslint fix the
    
    feat: eslint fix the

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-import storybook from "eslint-plugin-storybook";
-
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import { fixupPluginRules } from '@eslint/compat';
@@ -8,6 +6,7 @@ import js from '@eslint/js';
 import cypress from 'eslint-plugin-cypress';
 import jest from 'eslint-plugin-jest';
 import prettier from 'eslint-plugin-prettier';
+import storybook from 'eslint-plugin-storybook';
 import globals from 'globals';
 import neostandard from 'neostandard';
 import path from 'node:path';
@@ -22,74 +21,83 @@ const compat = new FlatCompat({
 });
 const neostandardConfig = neostandard({ semi: true, noStyle: true });
 
-export default [{
-  ignores: [
-    '**/build',
-    '**/dist',
-    '**/.docz',
-    '**/.github',
-    '**/node_modules',
-    'cypress/commons/services/stubbing.js',
-    'cypress/reports',
-    'scripts/migration/src/*/templates',
-  ],
-}, ...neostandardConfig, ...compat.extends('plugin:react/recommended', 'prettier', 'plugin:prettier/recommended', 'plugin:jest/recommended'), cypress.configs.recommended, cypress.configs.globals, {
-  plugins: {
-    react,
-    'react-hooks': fixupPluginRules(reactHooks),
-    cypress,
-    prettier,
-    jest,
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.es2021,
-      ...jest.environments.globals.globals,
-    },
-
-    ecmaVersion: 12,
-    sourceType: 'module',
-
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  },
-
-  settings: {
-    react: {
-      version: '18.3',
-    },
-  },
-
-  rules: {
-    'no-constant-binary-expression': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'cypress/assertion-before-screenshot': 'warn',
-    'cypress/no-force': 'error',
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    semi: [2, 'always'],
-
-    'max-len': [
-      'error',
-      {
-        code: 120,
-      },
+export default [
+  {
+    ignores: [
+      '**/build',
+      '**/dist',
+      '**/.docz',
+      '**/.github',
+      '**/node_modules',
+      'cypress/commons/services/stubbing.js',
+      'cypress/reports',
+      'scripts/migration/src/*/templates',
     ],
   },
-}, {
-  files: ['cypress/**/*.js'],
-  rules: {
-    'jest/expect-expect': 0,
-    'jest/valid-describe-callback': 0,
-    'jest/valid-expect': 0,
-    'jest/valid-expect-in-promise': 0,
+  ...neostandardConfig,
+  ...compat.extends('plugin:react/recommended', 'prettier', 'plugin:prettier/recommended', 'plugin:jest/recommended'),
+  cypress.configs.recommended,
+  cypress.configs.globals,
+  {
+    plugins: {
+      react,
+      'react-hooks': fixupPluginRules(reactHooks),
+      cypress,
+      prettier,
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...jest.environments.globals.globals,
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: '18.3',
+      },
+    },
+
+    rules: {
+      'no-constant-binary-expression': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'cypress/assertion-before-screenshot': 'warn',
+      'cypress/no-force': 'error',
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      semi: [2, 'always'],
+
+      'max-len': [
+        'error',
+        {
+          code: 120,
+        },
+      ],
+    },
   },
-}, ...storybook.configs["flat/recommended"]];
+  {
+    files: ['cypress/**/*.js'],
+    rules: {
+      'jest/expect-expect': 0,
+      'jest/valid-describe-callback': 0,
+      'jest/valid-expect': 0,
+      'jest/valid-expect-in-promise': 0,
+    },
+  },
+  ...storybook.configs['flat/recommended'],
+];
