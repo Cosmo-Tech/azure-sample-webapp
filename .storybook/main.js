21242aea9651968c275da40a1c1d551c2737d938
arianber7 <arian.berisha.ext@cosmotech.com>
2025-11-26 16:22:04 +0100

    Update yarn cache after adding Storyshots
    feat: eslint fix the
    
    feat: eslint fix the

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,42 +1,28 @@
 const config = {
-  stories: [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-  ],
+  addons: ['@storybook/preset-create-react-app', '@storybook/addon-docs', '@storybook/addon-onboarding'],
 
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
 
   docs: {
-    autodocs: true,  
+    autodocs: true,
   },
 
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
 
   webpackFinal: async (config) => {
     // 🚫 Remove any ESLint loaders injected by CRA preset
     config.module.rules = config.module.rules.filter(
-      (rule) =>
-        !(
-          rule.use &&
-          rule.use.some((u) =>
-            u.loader?.includes("eslint")
-          )
-        )
+      (rule) => !(rule.use && rule.use.some((u) => u.loader?.includes('eslint')))
     );
 
     // 🚫 Remove ESLint plugin created by CRA (this is the real problem)
     config.plugins = config.plugins.filter(
-      (plugin) =>
-        !(plugin.constructor && plugin.constructor.name === "ESLintWebpackPlugin")
+      (plugin) => !(plugin.constructor && plugin.constructor.name === 'ESLintWebpackPlugin')
     );
 
     return config;
