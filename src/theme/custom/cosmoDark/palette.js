0610119e058dc56e2c222f4e1e01059b6b56e49d
arianber7 <arian.berisha.ext@cosmotech.com>
2025-12-03 11:16:11 +0100

    feat: [PROD-14973] update palette theme, use new colors

--- src/theme/custom/cosmoDark/palette.js
+++ src/theme/custom/cosmoDark/palette.js
@@ -4,64 +4,68 @@
 export default {
   type: 'dark',
   primary: {
-    main: '#a4bfe4',
-    light: '#d6f2ff',
-    dark: '#748fb2',
-    contrastText: '#000000',
+    main: '#FFB039',
   },
   secondary: {
-    main: '#ffe26b',
-    dark: '#c9b03a',
-    light: '#ffff9c',
-    contrastText: '#000000',
+    main: '#292F33',
   },
-  error: {
-    main: '#e57373',
-    dark: '#A05050',
-    light: '#EA8F8F',
-    contrastText: '#000000',
+  tertiary: {
+    main: '#F36121',
   },
-  warning: {
-    main: '#ffa726',
-    dark: '#f57c00',
-    light: '#ffb74d',
-    contrastText: '#000000',
+  neutral: {
+    neutral01: {
+      main: '#47525F',
+    },
+    neutral02: {
+      main: '#68788A',
+    },
+    neutral03: {
+      main: '#919EAF',
+    },
+    neutral04: {
+      main: '#FFFFFF',
+    },
+    neutral05: {
+      main: '#E9EEF5',
+    },
+    neutral06: {
+      main: 'rgba(204, 204, 204, 0.72)',
+    },
+    neutral07: {
+      main: 'rgba(170, 170, 170, 0.72)',
+    },
+    neutral08: {
+      main: '#DEECD0',
+    },
   },
-  info: {
-    main: '#67B8E3',
-    dark: '#48809E',
-    light: '#85C6E8',
-    contrastText: '#000000',
+  background: {
+    background01: {
+      main: '#F1F5F9',
+    },
+    background02: {
+      main: '#D9DDE2',
+    },
   },
-  success: {
-    main: '#66bb6a',
-    dark: '#388e3c',
-    light: '#81c784',
-    contrastText: '#000000',
+  status: {
+    error: {
+      main: '#D32F2F',
+      background: '#ECD0D0',
+    },
+    warning: {
+      main: '#8A8768',
+      background: '#ECDEC7',
+    },
+    success: {
+      main: '#738A68',
+      background: '#DDECD0',
+    },
+    information: {
+      main: '#516388',
+      background: '#D0D9EC',
+    },
   },
   microsoft: {
-    main: '#2F2F2F',
-    contrastText: '#FFFFFF',
-  },
-  appbar: {
-    main: '#121212',
-    contrastText: '#FFFFFF',
-    contrastTextSoft: 'rgba(255, 255, 255, 0.7)',
-  },
-  login: {
-    main: '#2F363B',
-  },
-  navigation: {
-    background: '#1F252A',
-    border: '#2F363B',
-    text: '#FFFFFF',
-    hoverBg: '#2A3139',
-    mutedBg: '#252C34',
-    activeBg: '#FFFFFF',
-    activeText: '#1F252A',
-    icon: '#FFFFFF',
-    menuBackground: '#2A3139',
-    scrollbarThumb: 'rgba(255, 255, 255, 0.35)',
-    scrollbarThumbHover: 'rgba(255, 255, 255, 0.5)',
+    main: '#FFFFFF',
+    contrastText: '#5E5E5E',
   },
 };
