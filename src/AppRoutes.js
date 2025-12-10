0c92dfd02e21caee6c1a6a95e9b690d396b6730c
Nicolas Borde <nicolas.borde@cosmotech.com>
2025-12-10 11:11:14 +0100

    fix: fix default route redirection issue

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -41,7 +41,7 @@ const AppRoutes = () => {
           path=":workspaceId"
           element={
             <UserStatusGate>
-              <Navigate to="scenario" replace />
+              <Navigate to="scenarios" replace />
             </UserStatusGate>
           }
         />
