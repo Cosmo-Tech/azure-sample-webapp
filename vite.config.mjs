5ea9e0d1edb5eec9bcaf697579e4bc30deabc793
Tristan Huet <tristan.huet@cosmotech.com>
2025-12-15 18:24:27 +0100

    build: add CSP configuration setup for vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,24 @@
 // Licensed under the MIT license.
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import csp from 'vite-plugin-csp-guard';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { ENABLE_CSP_IN_DEV_MODE, CSP_CONFIG_POLICY } from './config-overrides.js';
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    csp({
+      dev: {
+        run: ENABLE_CSP_IN_DEV_MODE ?? true, // Run the plugin in dev mode
+      },
+      policy: CSP_CONFIG_POLICY,
+      build: {
+        sri: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       src: '/src',
