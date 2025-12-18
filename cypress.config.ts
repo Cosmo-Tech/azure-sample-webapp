ce17608445a1673abafb087af0a80d9da4ae243b
Tristan Huet <tristan.huet@cosmotech.com>
2025-12-18 14:57:35 +0100

    chore: disable modifyObstructiveCode in cypress config to allow usage of built local webapp

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   responseTimeout: 60000,
   viewportWidth: 1920,
   viewportHeight: 966,
+  modifyObstructiveCode: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
