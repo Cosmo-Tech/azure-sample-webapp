f639b3cf3bd0b043138788febf607e8a7156c095
Tristan Huet <tristan.huet@cosmotech.com>
2026-01-28 18:24:06 +0100

    fix: [PROD-15412] support file parameters for ETL runners in dataset creation

--- src/state/runner/sagas/CreateETLRunnerAndDataset.js
+++ src/state/runner/sagas/CreateETLRunnerAndDataset.js
@@ -3,9 +3,10 @@
 import { t } from 'i18next';
 import { put, takeEvery, call, select } from 'redux-saga/effects';
 import { Api } from '../../../services/config/Api';
+import DatasetService from '../../../services/dataset/DatasetService';
 import { ApiUtils, ConfigUtils, DatasetsUtils, RunnersUtils } from '../../../utils';
 import { setApplicationErrorMessage } from '../../app/reducers';
-import { addDataset } from '../../datasets/reducers';
+import { addDataset, addOrUpdateDatasetPart } from '../../datasets/reducers';
 import { createDataset } from '../../datasets/sagas/CreateDataset';
 import { RUNNER_ACTIONS_KEY } from '../constants';
 import { addEtlRunner, updateEtlRunner } from '../reducers';
@@ -14,117 +15,120 @@ const getUserEmail = (state) => state.auth.userEmail;
 const getUserName = (state) => state.auth.userName;
 const getSolution = (state) => state.solution?.current?.data;
 
-function* uploadFileParameter(parameter, organizationId, workspaceId) {
-  try {
-    const file = parameter.value.file;
+function* createETLRunner(action) {
+  const organizationId = action.organizationId;
+  const workspaceId = action.workspaceId;
+  const runner = action.runner;
+  const userEmail = yield select(getUserEmail);
+  const ownerName = yield select(getUserName);
+  const solution = yield select(getSolution);
+
+  runner.solutionId = solution.id;
+  // Add custom security with userEmail to support service accounts in ACL (do not remove, this is NOT managed by the
+  // back-end)
+  runner.security = { default: 'none', accessControlList: [{ id: userEmail, role: 'admin' }] };
+  runner.parametersValues = ApiUtils.formatParametersForApi(runner.parametersValues).parametersValues;
+  RunnersUtils.setRunnerOptions(runner, { ownerName });
+
+  const { data: createdRunner } = yield call(Api.Runners.createRunner, organizationId, workspaceId, runner);
+
+  // When creating a new runner, some dataset parameters can be initialized by the back-end (e.g. runner inheritance,
+  // workspace solution default values, ...). If the new runner already has dataset parts, we must store them in redux
+  const runnerDatasetParameters = createdRunner.datasets.parameters;
+  if (Array.isArray(runnerDatasetParameters) && runnerDatasetParameters.length > 0) {
+    const datasetId = createdRunner.datasets.parameter;
+    const { data: runnerDataset } = yield call(DatasetService.findDatasetById, organizationId, workspaceId, datasetId);
+    yield put(addDataset(runnerDataset));
+
+    for (const datasetPart of runnerDatasetParameters) {
+      yield put(addOrUpdateDatasetPart({ datasetId, datasetPart }));
+    }
+  }
+
+  const formattedParametersValues = ApiUtils.formatParametersFromApi(createdRunner.parametersValues);
+  yield put(addEtlRunner({ runner: { ...createdRunner, parametersValues: formattedParametersValues } }));
+
+  return createdRunner;
+}
+
+function* createRunnerDatasetParts(action, createdRunner, datasetPartParameters) {
+  const runnerParameterDatasetId = createdRunner.datasets?.parameter;
+  const organizationId = action.organizationId;
+  const workspaceId = action.workspaceId;
+
+  const createdDatasetParts = [];
+  for (const parameter of datasetPartParameters) {
     const parameterId = parameter.parameterId;
-
-    const datasetPart = {
-      name: parameterId,
-      description: parameter.description,
-      sourceType: 'None',
-      tags: ['dataset_part'],
-      main: false,
-    };
-    const { data } = yield call(Api.Datasets.createDataset, organizationId, workspaceId, datasetPart);
-    const datasetId = data.id;
-    const datasetLocation = DatasetsUtils.buildDatasetLocation(datasetId, file.name);
-    const newSource = data.source ?? {};
-    newSource.location = datasetLocation;
-    const updatedDatasetPart = { ...datasetPart, source: newSource };
-
-    const { data: updatedDataset } = yield call(
-      Api.Datasets.updateDataset,
+    const fileParameter = parameter.value;
+    const file = fileParameter.value;
+    const datasetPartToCreate = { name: parameterId, sourceName: file?.name };
+    const createdDatasetPart = yield call(
+      DatasetService.createDatasetPart,
       organizationId,
       workspaceId,
-      datasetId,
-      updatedDatasetPart
+      runnerParameterDatasetId,
+      datasetPartToCreate,
+      file
     );
-    // FIXME: workspace files no longer exist, replace by new dataset+part structure
-    // yield call(Api.Workspaces.uploadWorkspaceFile, organizationId, workspaceId, file, true, datasetLocation);
-    yield put(addDataset({ ...updatedDataset }));
 
-    return datasetId;
-  } catch (error) {
-    console.error(error);
-    yield put(
-      setApplicationErrorMessage({
-        error,
-        errorMessage: t(
-          'commoncomponents.banner.runnerFileNotUploaded',
-          `Runner file {runnerFile} hasn't been uploaded`,
-          {
-            runnerFile: parameter.value.file,
-          }
-        ),
-      })
-    );
-    throw new Error(error);
+    const runnerId = createdRunner.id;
+    addOrUpdateDatasetPart({ datasetId: runnerParameterDatasetId, datasetPart: createdDatasetPart, runnerId });
+    createdDatasetParts.push(createdDatasetPart);
   }
+}
+
+function* forgeAndCreateDataset(action, createdRunner, parentDatasetId) {
+  const ownerName = yield select(getUserName);
+  const runnerId = createdRunner.id;
+
+  const dataset = {
+    name: createdRunner.name,
+    description: createdRunner.description,
+    tags: createdRunner.tags,
+    additionalData: {
+      webapp: { runnerId, sourceType: 'ETL', ownerName, visible: { datasetManager: true, scenarioCreation: true } },
+    },
+    // TODO: should we set dataset.security to support service accounts? (same as we're doing for runner.security)
+  };
+
+  if (parentDatasetId != null) DatasetsUtils.setDatasetOptions(dataset, { parentId: parentDatasetId });
+  return yield call(createDataset, { dataset, shouldSelectDataset: true });
 }
 
 export function* createETLRunnerAndDataset(action) {
   try {
     const organizationId = action.organizationId;
     const workspaceId = action.workspaceId;
-    const runner = action.runner;
-    const userEmail = yield select(getUserEmail);
-    const ownerName = yield select(getUserName);
-    const solution = yield select(getSolution);
 
-    runner.solutionId = solution.id;
-    // Add custom security with userEmail to support service accounts in ACL (do not remove, this is NOT managed by the
-    // back-end)
-    runner.security = { default: 'none', accessControlList: [{ id: userEmail, role: 'admin' }] };
-    RunnersUtils.setRunnerOptions(runner, { ownerName });
-
-    for (const parameter of runner.parametersValues) {
-      if (ConfigUtils.isFileParameter(parameter)) {
-        const datasetId = yield call(uploadFileParameter, parameter, organizationId, workspaceId);
-        parameter.value = datasetId;
-      }
+    const datasetPartParameters = [];
+    if (action?.runner?.parametersValues != null) {
+      const filteredParameterValues = [];
+      action.runner.parametersValues.forEach((parameter) => {
+        if (ConfigUtils.isDatasetPartVarType(parameter?.varType)) datasetPartParameters.push(parameter);
+        else filteredParameterValues.push(parameter);
+      });
+      action.runner.parametersValues = filteredParameterValues;
     }
 
-    const { data: createdRunner } = yield call(Api.Runners.createRunner, organizationId, workspaceId, {
-      ...runner,
-      parametersValues: ApiUtils.formatParametersForApi(runner.parametersValues).parametersValues,
-    });
-    const runnerId = createdRunner.id;
-    yield put(
-      addEtlRunner({
-        runner: {
-          ...createdRunner,
-          parametersValues: ApiUtils.formatParametersFromApi(createdRunner.parametersValues),
-        },
-      })
-    );
-
-    const dataset = {
-      name: runner.name,
-      description: runner.description,
-      tags: runner.tags,
-      additionalData: {
-        webapp: { runnerId, sourceType: 'ETL', ownerName, visible: { datasetManager: true, scenarioCreation: true } },
-      },
-      source: { location: workspaceId, name: runnerId },
-    };
     // When creating subdatasets, the runner provided to the createRunner saga contains the id of the **parent dataset**
     // in datasets.bases
-    const baseDatasets = createdRunner.datasets?.bases ?? [];
-    if (baseDatasets.length > 0) DatasetsUtils.setDatasetOptions(dataset, { parentId: baseDatasets[0] });
-    const createdDataset = yield call(createDataset, { dataset, shouldSelectDataset: true });
-    const datasetId = createdDataset.id;
+    const baseDatasets = action?.runner?.datasetList ?? [];
+    const parentDatasetId = baseDatasets?.[0];
+
+    const createdRunner = yield call(createETLRunner, action);
+    yield call(createRunnerDatasetParts, action, createdRunner, datasetPartParameters);
+    const createdDataset = yield call(forgeAndCreateDataset, action, createdRunner, parentDatasetId);
 
     // First entry of datasetList must be the "ETL" dataset, to which we add the additional datasets (e.g. the parent
     // dataset when creating subdatasets)
-    const datasetList = [datasetId].concat(baseDatasets);
-    const patchedRunner = { runTemplateId: runner.runTemplateId, datasetList };
+    const runnerUpdatePayload = { datasetList: [createdDataset.id].concat(baseDatasets) };
+    const runnerId = createdRunner.id;
     const { data: updatedRunner } = yield call(
       Api.Runners.updateRunner,
       organizationId,
       workspaceId,
-      runnerId,
-      patchedRunner
+      createdRunner.id,
+      runnerUpdatePayload
     );
     yield put(
       updateEtlRunner({
