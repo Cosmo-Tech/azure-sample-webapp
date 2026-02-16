ba0c5c241acf29fc2b70acf21eee1d94e0559014
Tristan Huet <tristan.huet@cosmotech.com>
2026-02-16 16:57:11 +0100

    test: fix cypress test ResultsDashboards-DisplayDisabled

--- cypress/fixtures/stubbing/ScenarioViewDashboard/workspace.js
+++ cypress/fixtures/stubbing/ScenarioViewDashboard/workspace.js
@@ -1,12 +1,12 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-import { WORKSPACE_EXAMPLE } from '../default';
+import { DEFAULT_WORKSPACE } from '../default';
 
 export const WORKSPACE_WITHOUT_DASHBOARDS = {
-  ...WORKSPACE_EXAMPLE,
+  ...DEFAULT_WORKSPACE,
   id: 'w-stbwrknodsh',
   key: 'breweryNoDashboards',
   additionalData: { webapp: null },
 };
 
-export const WORKSPACE_LIST_WITHOUT_DASHBOARDS = [WORKSPACE_EXAMPLE, WORKSPACE_WITHOUT_DASHBOARDS];
+export const WORKSPACE_LIST_WITHOUT_DASHBOARDS = [WORKSPACE_WITHOUT_DASHBOARDS];
