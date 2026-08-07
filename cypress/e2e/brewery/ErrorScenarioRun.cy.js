61c88549524c3dfdf855e9c85583bce21f4dc5a3
Tristan Huet <tristan.huet@cosmotech.com>
2026-08-07 18:05:07 +0200

    test: fix interceptions of stubbed test ErrorScenarioRun

--- cypress/e2e/brewery/ErrorScenarioRun.cy.js
+++ cypress/e2e/brewery/ErrorScenarioRun.cy.js
@@ -3,21 +3,16 @@
 import { Scenarios, ScenarioParameters, ErrorBanner, Login } from '../../commons/actions';
 import { API_REGEX } from '../../commons/constants/generic/TestConstants';
 import { stub } from '../../commons/services/stubbing';
+import { apiUtils as api } from '../../commons/utils';
 
 describe('Displaying error banner on run scenario fail', () => {
-  before(() => {
-    stub.start();
-  });
-
-  beforeEach(() => {
-    Login.login();
-  });
-
-  after(() => {
-    stub.stop();
-  });
+  before(() => stub.start());
+  beforeEach(() => Login.login());
+  after(() => stub.stop());
 
   it('can display error banner and dismiss it', () => {
+    const updateRunAlias = api.interceptUpdateSimulationRunner();
+    const startRunAlias = 'startRunAlias';
     cy.intercept('POST', API_REGEX.START_RUNNER, {
       statusCode: 400,
       body: {
@@ -25,10 +20,12 @@ describe('Displaying error banner on run scenario fail', () => {
         status: 400,
         detail: `<error placeholder>`,
       },
-    });
+    }).as(startRunAlias);
 
-    // Do not use "launch" action to be able to use our custom 'intercept'
+    // Do not use ScenarioParameters.launch() action to be able to return an error immediately
     ScenarioParameters.getLaunchButton(180).should('not.be.disabled').click();
+    api.waitAliases([updateRunAlias, startRunAlias]);
+
     ErrorBanner.checkAnDismissErrorBanner();
     Scenarios.getDashboardAccordionLogsDownloadButton().should('not.exist');
   });
