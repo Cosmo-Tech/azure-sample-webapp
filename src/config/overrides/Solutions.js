1f22ff198ec3221b0dc429ceb9699f195438e620
Tristan Huet <tristan.huet@cosmotech.com>
2026-08-18 18:00:15 +0200

    chore: [DO NOT MERGE] custom config override for local dev

--- src/config/overrides/Solutions.js
+++ src/config/overrides/Solutions.js
@@ -4,4 +4,29 @@
 // Use the SOLUTIONS array below to override or add information to your solutions. This can be useful for development
 // purposes, but it is recommended to leave this array empty and use the API to update your Solution instead for
 // production environments.
-export const SOLUTIONS = [];
+export const SOLUTIONS = [
+  {
+    id: 'sol-414m4e1q72em8',
+    runTemplates: [
+      {
+        id: 'etl_mock_parameters',
+        labels: {
+          fr: "Run template d'ETL avec paramètres basiques fictifs",
+          en: 'ETL run template with mock basic types parameters',
+        },
+        tags: ['datasource'],
+        name: 'ETL run template with mock basic types parameters',
+        description: 'ETL run template with mock basic types parameters',
+        parameterGroups: [
+          'basic_types',
+          'dataset_parts',
+          'extra_dataset_part',
+          'customers',
+          'events',
+          'additional_parameters',
+          'initial_state',
+        ],
+      },
+    ],
+  },
+];
--- src/config/overrides/Workspaces.js
+++ src/config/overrides/Workspaces.js
@@ -4,4 +4,21 @@
 // Use the WORKSPACES array below to override or add information to your workspaces. This can be useful for development
 // purposes, but it is recommended to leave this array empty and use the API to update your Workspace instead for
 // production environments.
-export const WORKSPACES = [];
+export const WORKSPACES = [
+  {
+    id: 'w-0qq1yk8k42161',
+    additionalData: {
+      webapp: {
+        datasetManager: {
+          datasourceFilter: [
+            'etl_mock_parameters',
+            'etl_instance_generator',
+            'FileUploadToDataset',
+            'etl_zip2db',
+            'None',
+          ],
+        },
+      },
+    },
+  },
+];
