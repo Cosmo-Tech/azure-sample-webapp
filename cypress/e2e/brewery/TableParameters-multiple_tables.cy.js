d592a01c8318058480a6f73c630123ccb8290248
Tristan Huet <tristan.huet@cosmotech.com>
2026-09-17 16:50:55 +0200

    test: add cypress test TableParameters-multiple_tables

---
+++ cypress/e2e/brewery/TableParameters-multiple_tables.cy.js
@@ -0,0 +1,149 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import { Login } from '../../commons/actions/brewery';
+import { ScenarioParameters, ScenarioSelector, TableParameters } from '../../commons/actions/generic';
+import { stub } from '../../commons/services/stubbing';
+import { apiUtils } from '../../commons/utils';
+import {
+  CHILD_BASE_DATASET,
+  CHILD_EDITABLE_TABLE_PART_ID,
+  CHILD_RUNNER,
+  PARENT_BASE_DATASET,
+  PARENT_EDITABLE_TABLE_PART_ID,
+  PARENT_RUNNER,
+  ROOT_RUNNER,
+  SOLUTION_WITH_TWO_TABLES,
+} from '../../fixtures/stubbing/TableParameters-multiple_tables';
+import { DEFAULT_RUNNER_BASE_DATASET, DEFAULT_WORKSPACE } from '../../fixtures/stubbing/default';
+
+const PARENT_DYNAMIC_TABLE_QUERY_RESPONSE = 'product,stock\nWidgetParent,1';
+const PARENT_EDITABLE_TABLE_CSV = 'item,quantity\nParentItem,0';
+const CHILD_DYNAMIC_TABLE_QUERY_RESPONSE = 'product,stock\nWidgetChild,42';
+const CHILD_EDITABLE_TABLE_CSV = 'item,quantity\nChildItem,7';
+
+const getDynamicTable = () => cy.get('[data-cy=table-dynamic_table]');
+const getEditableTable = () => cy.get('[data-cy=table-editable_table]');
+
+// TODO: we shouldn't need to intercept these queries so many times, it may be cause only by the cypress test
+// behavior (the page refresh during login) but it might also be caused by the webapp (too many renders? no lock
+// on the dynamic queries?)
+const TABLE_QUERY_INTERCEPTION_COUNT = 3;
+
+describe('Table parameters with multiple tables in one group, opened directly on a child scenario', () => {
+  before(() => {
+    stub.start();
+    stub.setSolutions([SOLUTION_WITH_TWO_TABLES]);
+    stub.setDatasets([DEFAULT_RUNNER_BASE_DATASET, PARENT_BASE_DATASET, CHILD_BASE_DATASET]);
+    stub.setRunners([ROOT_RUNNER, PARENT_RUNNER, CHILD_RUNNER]);
+    stub.addDatasetPartFile(CHILD_EDITABLE_TABLE_PART_ID, CHILD_EDITABLE_TABLE_CSV);
+  });
+
+  after(() => stub.stop());
+
+  it('displays the child scenario data when the webapp is opened directly on it via URL', () => {
+    apiUtils.interceptPostDatasetQuery(
+      {},
+      (req) => {
+        throw new Error('Query to parent scenario must not be called');
+      },
+      1,
+      {
+        datasetId: PARENT_BASE_DATASET.id,
+        datasetPartId: CHILD_BASE_DATASET.parts[0].id, // Only intercept mismatch queries with parent & child ids
+      }
+    );
+    apiUtils.interceptDownloadDatasetPart({
+      datasetId: PARENT_RUNNER.datasets.parameter,
+      datasetPartId: CHILD_EDITABLE_TABLE_PART_ID, // Only intercept mismatch queries with parent & child ids
+      validateRequest: (req) => {
+        throw new Error('Query to parent scenario must not be called');
+      },
+    });
+
+    let hasDatasetPostQueryBeenCalled = false;
+    apiUtils.interceptPostDatasetQuery(
+      CHILD_DYNAMIC_TABLE_QUERY_RESPONSE,
+      (req) => (hasDatasetPostQueryBeenCalled = true),
+      TABLE_QUERY_INTERCEPTION_COUNT,
+      {
+        datasetId: CHILD_BASE_DATASET.id,
+        datasetPartId: CHILD_BASE_DATASET.parts[0].id,
+      }
+    );
+    let hasDatasetPartDownloadBeenCalled = false;
+    apiUtils.interceptDownloadDatasetPart({
+      datasetPartId: CHILD_EDITABLE_TABLE_PART_ID,
+      fileContent: CHILD_EDITABLE_TABLE_CSV,
+      validateRequest: (req) => {
+        hasDatasetPartDownloadBeenCalled = true;
+        expect(req.url).to.include(CHILD_RUNNER.datasets.parameter);
+      },
+      times: TABLE_QUERY_INTERCEPTION_COUNT,
+    });
+
+    Login.login({
+      url: `${DEFAULT_WORKSPACE.id}/scenario/${CHILD_RUNNER.id}`,
+      scenarioId: CHILD_RUNNER.id,
+    });
+
+    cy.then(() => expect(hasDatasetPostQueryBeenCalled).to.be.true);
+    cy.then(() => expect(hasDatasetPartDownloadBeenCalled).to.be.true);
+
+    ScenarioParameters.expandParametersAccordion();
+    TableParameters.getLoadingSpinner(getDynamicTable()).should('not.exist');
+    TableParameters.getCell(getDynamicTable(), 'product', 0).should('have.text', 'WidgetChild');
+    TableParameters.getCell(getDynamicTable(), 'stock', 0).should('have.text', '42');
+
+    TableParameters.getLoadingSpinner(getEditableTable()).should('not.exist');
+    TableParameters.getCell(getEditableTable(), 'item', 0).should('have.text', 'ChildItem');
+    TableParameters.getCell(getEditableTable(), 'quantity', 0).should('have.text', '7');
+
+    let hasParentDatasetPostQueryBeenCalled = false;
+    apiUtils.interceptPostDatasetQuery(
+      PARENT_DYNAMIC_TABLE_QUERY_RESPONSE,
+      (req) => (hasParentDatasetPostQueryBeenCalled = true),
+      1,
+      {
+        datasetId: PARENT_BASE_DATASET.id,
+        datasetPartId: PARENT_BASE_DATASET.parts[0].id,
+      }
+    );
+
+    let hasParentDatasetPartDownloadBeenCalled = false;
+    apiUtils.interceptDownloadDatasetPart({
+      datasetPartId: PARENT_EDITABLE_TABLE_PART_ID,
+      fileContent: PARENT_EDITABLE_TABLE_CSV,
+      validateRequest: (req) => {
+        hasParentDatasetPartDownloadBeenCalled = true;
+        expect(req.url).to.include(PARENT_RUNNER.datasets.parameter);
+      },
+      times: 1,
+    });
+
+    // Switch to parent scenario & check tables
+    ScenarioSelector.selectScenario(PARENT_RUNNER.name, PARENT_RUNNER.id);
+
+    TableParameters.getLoadingSpinner(getDynamicTable()).should('not.exist');
+    TableParameters.getCell(getDynamicTable(), 'product', 0).should('have.text', 'WidgetParent');
+    TableParameters.getCell(getDynamicTable(), 'stock', 0).should('have.text', '1');
+
+    TableParameters.getLoadingSpinner(getEditableTable()).should('not.exist');
+    TableParameters.getCell(getEditableTable(), 'item', 0).should('have.text', 'ParentItem');
+    TableParameters.getCell(getEditableTable(), 'quantity', 0).should('have.text', '0');
+
+    cy.then(() => expect(hasParentDatasetPostQueryBeenCalled).to.be.true);
+    cy.then(() => expect(hasParentDatasetPartDownloadBeenCalled).to.be.true);
+
+    // Switch to root scenario & switch back to child scenario
+    ScenarioSelector.selectScenario(ROOT_RUNNER.name, ROOT_RUNNER.id);
+    ScenarioSelector.selectScenario(CHILD_RUNNER.name, CHILD_RUNNER.id);
+
+    TableParameters.getLoadingSpinner(getDynamicTable()).should('not.exist');
+    TableParameters.getCell(getDynamicTable(), 'product', 0).should('have.text', 'WidgetChild');
+    TableParameters.getCell(getDynamicTable(), 'stock', 0).should('have.text', '42');
+
+    TableParameters.getLoadingSpinner(getEditableTable()).should('not.exist');
+    TableParameters.getCell(getEditableTable(), 'item', 0).should('have.text', 'ChildItem');
+    TableParameters.getCell(getEditableTable(), 'quantity', 0).should('have.text', '7');
+  });
+});
---
+++ cypress/fixtures/stubbing/TableParameters-multiple_tables.js
@@ -0,0 +1,148 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+import {
+  DEFAULT_RUNNER_BASE_DATASET,
+  DEFAULT_SIMULATION_RUNNER,
+  DEFAULT_SOLUTION,
+  NO_PARAMETERS_RUN_TEMPLATE,
+} from './default';
+
+const TWO_TABLES_GROUP = {
+  id: 'two_tables_group',
+  parameters: ['dynamic_table', 'editable_table'],
+};
+
+const DYNAMIC_TABLE_PARAM = {
+  id: 'dynamic_table',
+  varType: '%DATASET_PART_ID_FILE%',
+  additionalData: {
+    subType: 'TABLE',
+    dynamicValues: { datasetPartName: 'dynamic_data' },
+    columns: [
+      { field: 'product', type: ['string', 'nonEditable'] },
+      {
+        field: 'stock',
+        type: ['int', 'nonEditable'],
+      },
+    ],
+  },
+};
+
+const EDITABLE_TABLE_PARAM = {
+  id: 'editable_table',
+  varType: '%DATASET_PART_ID_FILE%',
+  additionalData: {
+    subType: 'TABLE',
+    canChangeRowsNumber: true,
+    columns: [
+      { field: 'item', type: ['string'] },
+      {
+        field: 'quantity',
+        type: ['int'],
+      },
+    ],
+  },
+};
+
+const CUSTOM_RUN_TEMPLATE = {
+  id: 'sim_two_tables_inheritance',
+  name: 'Run template with two table parameters',
+  tags: ['sim_two_tables_inheritance'],
+  parameterGroups: [TWO_TABLES_GROUP.id],
+};
+
+export const SOLUTION_WITH_TWO_TABLES = {
+  ...DEFAULT_SOLUTION,
+  runTemplates: [NO_PARAMETERS_RUN_TEMPLATE, CUSTOM_RUN_TEMPLATE],
+  parameters: [DYNAMIC_TABLE_PARAM, EDITABLE_TABLE_PARAM],
+  parameterGroups: [TWO_TABLES_GROUP],
+};
+
+export const ROOT_RUNNER = {
+  ...DEFAULT_SIMULATION_RUNNER,
+  id: 'r-twoTablesRoot',
+  name: 'Cypress - Two tables root scenario',
+  runTemplateId: NO_PARAMETERS_RUN_TEMPLATE.id,
+  runTemplateName: NO_PARAMETERS_RUN_TEMPLATE.name,
+  parentId: null,
+  rootId: null,
+};
+
+export const PARENT_BASE_DATASET = {
+  ...DEFAULT_RUNNER_BASE_DATASET,
+  id: 'd-parentBaseDataset',
+  parts: [
+    {
+      id: 'dp-parentDynamicPart',
+      name: 'dynamic_data',
+      type: 'DB',
+      organizationId: DEFAULT_RUNNER_BASE_DATASET.organizationId,
+      workspaceId: DEFAULT_RUNNER_BASE_DATASET.workspaceId,
+      datasetId: 'd-parentBaseDataset',
+    },
+  ],
+};
+
+export const CHILD_BASE_DATASET = {
+  ...DEFAULT_RUNNER_BASE_DATASET,
+  id: 'd-childBaseDataset',
+  parts: [
+    {
+      id: 'dp-childDynamicPart',
+      name: 'dynamic_data',
+      type: 'DB',
+      organizationId: DEFAULT_RUNNER_BASE_DATASET.organizationId,
+      workspaceId: DEFAULT_RUNNER_BASE_DATASET.workspaceId,
+      datasetId: 'd-childBaseDataset',
+    },
+  ],
+};
+
+export const PARENT_RUNNER_PARAMETER_DATASET_ID = 'd-parentRunnerParameterDataset';
+export const CHILD_RUNNER_PARAMETER_DATASET_ID = 'd-childRunnerParameterDataset';
+export const PARENT_EDITABLE_TABLE_PART_ID = 'dp-parentEditableTable';
+export const CHILD_EDITABLE_TABLE_PART_ID = 'dp-childEditableTable';
+
+export const PARENT_RUNNER = {
+  ...DEFAULT_SIMULATION_RUNNER,
+  id: 'r-twoTablesParent',
+  name: 'Cypress - Two tables parent scenario',
+  runTemplateId: CUSTOM_RUN_TEMPLATE.id,
+  runTemplateName: CUSTOM_RUN_TEMPLATE.name,
+  datasets: {
+    bases: [PARENT_BASE_DATASET.id],
+    parameter: PARENT_RUNNER_PARAMETER_DATASET_ID,
+    parameters: [
+      {
+        id: PARENT_EDITABLE_TABLE_PART_ID,
+        name: EDITABLE_TABLE_PARAM.id,
+        datasetId: PARENT_RUNNER_PARAMETER_DATASET_ID,
+        sourceName: 'editable_table.csv',
+      },
+    ],
+  },
+  parentId: ROOT_RUNNER.id,
+  rootId: ROOT_RUNNER.id,
+};
+
+export const CHILD_RUNNER = {
+  ...DEFAULT_SIMULATION_RUNNER,
+  id: 'r-twoTablesChild',
+  name: 'Cypress - Two tables child scenario',
+  runTemplateId: CUSTOM_RUN_TEMPLATE.id,
+  runTemplateName: CUSTOM_RUN_TEMPLATE.name,
+  datasets: {
+    bases: [CHILD_BASE_DATASET.id],
+    parameter: CHILD_RUNNER_PARAMETER_DATASET_ID,
+    parameters: [
+      {
+        id: CHILD_EDITABLE_TABLE_PART_ID,
+        name: EDITABLE_TABLE_PARAM.id,
+        datasetId: CHILD_RUNNER_PARAMETER_DATASET_ID,
+        sourceName: 'editable_table.csv',
+      },
+    ],
+  },
+  parentId: PARENT_RUNNER.id,
+  rootId: ROOT_RUNNER.id,
+};
