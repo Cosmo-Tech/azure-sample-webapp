f68178f8c423eabc73322dec944a7abe187a2cb1
Tristan Huet <tristan.huet@cosmotech.com>
2026-09-25 16:05:49 +0200

    chore: prepare release v7.4.0-vanilla

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,7 @@ export const CSP_CONFIG_POLICY = {
     "'self'",
 
     // Example of source for a custom on-premises cluster
-    'https://aks-dev-joy.azure.platform.cosmotech.com',
+    // 'https://kubernetes.cosmotech.com',
 
     // Examples of sources for Azure login and PowerBI
     // 'api.powerbi.com',
@@ -24,10 +24,10 @@ export const CSP_CONFIG_POLICY = {
     'blob:',
 
     // Example of source for a custom on-premises cluster
-    'https://superset-aks-dev-joy.azure.platform.cosmotech.com',
+    // 'https://superset-kubernetes.cosmotech.com',
 
     // Examples of sources for Azure login and PowerBI
-    'https://app.powerbi.com',
+    // 'https://app.powerbi.com',
     // 'https://login.microsoftonline.com'
   ],
   'manifest-src': ["'self'"],
